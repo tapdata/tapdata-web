e5ff10d86c500f2471318304583692f62813f835
唐晓敏 <bassie@tapdata.io>
2020-03-10 15:18:06 +0800

    集群管理添加服务

---
+++ src/view/clusterManagement/component/addServe.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="addServe">
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="name" prop="name">
+        <el-input v-model.number="ruleForm.name"></el-input>
+      </el-form-item>
+
+      <el-form-item label="command" prop="command">
+        <el-input v-model="ruleForm.command" ></el-input>
+      </el-form-item>
+
+      <el-form-item label="arguements" prop="arguements">
+        <el-input v-model="ruleForm.arguements"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  name: "AddServe",
+  data(){
+    return {
+      ruleForm :{
+        name: '',
+        command: '',
+        arguements: ''
+      }
+    }
+  }
+}
+</script>
