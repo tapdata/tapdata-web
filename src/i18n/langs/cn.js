f3164a258ef39c93b0d1bb49e75c140d7f01fd5a
唐晓敏 <bassie@tapdata.io>
2020-03-20 20:47:08 +0800

    监控图标数据对接

--- src/i18n/langs/cn.js
+++ src/i18n/langs/cn.js
@@ -42,7 +42,7 @@ const cn = {
   },
   dataFlow: {
     taskName: '任务名称',
-    Founder: '创建人',
+    creatdor: '创建人',
     creationTime: '创建时间',
     state: '状态',
     executionTime: '本次执行时间',
@@ -56,6 +56,14 @@ const cn = {
     min: "分",
     hour: "时",
     day: "日",
+    input: '输入',
+    output: '输出',
+    totalInput:'总输入',
+    totalOutput:'总输出',
+    replicate: "数据同步差距",
+    throughputpop:'平均每秒源端数据采集的速度以及目标端写入的速度，数值越大越好',
+    transtime_pop: '传输耗时：除源节点外，事件处理完的时间减去事件的发生时间。 节点间统计：事件从进入节点到输出到所消耗的时间 任务流统计：所有节点耗时相加，数值越小越好',
+    replicate_pop: '源库和目标库数据最后更新时间的差距，数值越小越好'
   },
   ...zhLocale
 };
--- src/view/job/components/echartHead.vue
+++ src/view/job/components/echartHead.vue
@@ -2,6 +2,7 @@
   <div class="echartHead">
     <h2>{{title}}</h2>
     <el-popover
+      v-if="isIput"
       placement="top-start"
       width="300"
       popper-class ="echartsTitle_popover"
@@ -10,19 +11,19 @@
       <span class="icon iconfont icon-tishi1" slot="reference"></span>
     </el-popover>
     <div class="rightOpt fr">
-      <el-radio-group v-model="num" size="mini" @change="changeRadio" v-if="isScreeing">
-        <el-radio-button label="bars">{{$t("dataFlow.rowCount")}}</el-radio-button>
-        <el-radio-button label="kb">KB</el-radio-button>
+      <el-radio-group v-model="num" size="mini" :class="selectColor" @change="changeRadio" v-if="isScreeing">
+        <el-radio-button label="flow">{{$t("dataFlow.rowCount")}}</el-radio-button>
+        <el-radio-button label="stage">KB</el-radio-button>
       </el-radio-group>
-      <el-radio-group v-model="speed" size="mini" @change="changeSpeed" v-if="isSpeed">
-        <el-radio-button label="qps">QBS</el-radio-button>
+      <el-radio-group v-model="speed" size="mini" :class="selectColor" @change="changeSpeed" v-if="isSpeed">
+        <el-radio-button label="qps">QPS</el-radio-button>
         <el-radio-button label="kbs">KB/S</el-radio-button>
       </el-radio-group>
-      <el-radio-group v-model="time" size="mini" @change="changeTime" v-if="isIput">
-        <el-radio-button label="ss">{{$t("dataFlow.second")}}</el-radio-button>
-        <el-radio-button label="mm">{{$t("dataFlow.min")}}</el-radio-button>
-        <el-radio-button label="hh">{{$t("dataFlow.hour")}}</el-radio-button>
-        <el-radio-button label="dd">{{$t("dataFlow.day")}}</el-radio-button>
+      <el-radio-group v-model="time" size="mini" :class="selectColor" @change="changeTime" v-if="isIput">
+        <el-radio-button label="second">{{$t("dataFlow.second")}}</el-radio-button>
+        <el-radio-button label="min">{{$t("dataFlow.min")}}</el-radio-button>
+        <el-radio-button label="hour">{{$t("dataFlow.hour")}}</el-radio-button>
+        <el-radio-button label="day">{{$t("dataFlow.day")}}</el-radio-button>
       </el-radio-group>
     </div>
   </div>
@@ -37,15 +38,15 @@ export default {
     return {
       title: '',
       tip: '',
-      num: 'bars',
+      num: 'flow',
       speed: 'qps',
-      time: 'ss',
+      time: 'second',
       rowCount: null,
       kbs: null,
       isScreeing: false,
       isIput: false,
       isSpeed: false,
-      
+      selectColor: ''
     };
   },
   mounted() {
@@ -55,9 +56,19 @@ export default {
       this.isScreeing = this.data.isScreeing;
       this.isIput = this.data.isIput
       this.isSpeed = this.data.isSpeed
+
       this.$emit("twoRadio",this.num,this.data.type);
-      this.$emit("getSpeed",this.speed,this.data.type);
+      this.$emit("getSpeed",this.speed,this.time);
       this.$emit("getTime",this.time,this.data.type);
+      if(this.data.type ==="screening") {
+        this.selectColor = 'screeningColor'
+      } else if (this.data.type ==="inputOutput") {
+        this.selectColor = 'putColor'
+      } else if (this.data.type ==="transf") {
+        this.selectColor = 'transfColor'
+      } else if (this.data.type ==="replicate") {
+        this.selectColor = 'replicateColor'
+      }
     })
   },
   methods: {
@@ -65,10 +76,11 @@ export default {
       this.$emit("twoRadio",val,this.data.type);
     },
     changeSpeed (val) {
-      this.$emit("getSpeed",val,this.data.type);
+      this.$emit("getSpeed",val,this.time);
     },
     changeTime(val) {
       this.$emit("getTime",val,this.data.type);
+      this.$emit("getSpeed",this.speed,val);
     }
   }
 };
@@ -114,6 +126,27 @@ export default {
 .echartHead {
   .el-radio-group {
     padding-left: 20px;
+  }
+  .screeningColor,.putColor {
+    .el-radio-button__orig-radio:checked+.el-radio-button__inner {
+      background-color: #48b6e2;
+      border-color: #48b6e2;
+      box-shadow: -1px 0 0 0 #48B6E2;
+    }
+  }
+  .transfColor {
+    .el-radio-button__orig-radio:checked+.el-radio-button__inner {
+      background-color: #fb8e00;
+      border-color: #fb8e00;
+      box-shadow: -1px 0 0 0 #fb8e00;
+    }
+  }
+  .replicateColor {
+    .el-radio-button__orig-radio:checked+.el-radio-button__inner {
+      background-color: #f56c6c;
+      border-color: #f56c6c;
+      box-shadow: -1px 0 0 0 #f56c6c;
+    }
   }
 }
 
