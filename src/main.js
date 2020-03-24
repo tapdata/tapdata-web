c007e7079aeb9849d4f273c64ec82bdb99716df7
唐晓敏 <bassie@tapdata.io>
2020-03-24 14:52:24 +0800

    debug日志

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import moment from 'moment';  //时间格式化
 import store from './vuex';   // 引入全局数据控制
 import axios from 'axios';
 import VueCookie from 'vue-cookie';
@@ -14,6 +15,8 @@ import './theme/index.css';
 
 Vue.config.productionTip = false;
 Vue.use(VueCookie);
+
+Vue.prototype.$moment = moment;
 
 // Vue.prototype.i18n = window.jQuery.i18n
 new Vue({
