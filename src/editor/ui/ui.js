107b787af64242133d41156bd7ae153e17f90fda
fannie <fannie@tapdata.io>
2020-05-20 12:31:50 +0800

    任务编排-任务名称-回车提交

--- src/editor/ui/ui.js
+++ src/editor/ui/ui.js
@@ -22,9 +22,10 @@ export default class UI extends Component {
 			<div class="e-header">
 				<div class="e-action-bar"><a class="e-action-back iconfont icon-biaotongbu"></a></div>
 				<div class="e-title">
-					<input value="新任务未命名" class="ui-input" id="taskNameInput" type="text"  maxlength="24" "/>
+					<input value="新任务未命名" class="ui-input" id="taskNameInput" type="text"  maxlength="24"/>
 				</div>
-				<i class='el-icon-edit el-icon'></i>
+				<i class='el-icon-edit el-icon' id="edit"></i>
+				<i class='iconfont icon-baocun el-icon' id="submit" style="display: none;color:##48B6E2" ></i>
 				<div class="e-toolbar-container">
 					<div class="graph-toolbar"></div>
 					<div class="action-toolbar"></div>
@@ -41,6 +42,35 @@ export default class UI extends Component {
 		if( this.opts.actionBarEl ){
 			this.getActionBarEl().append(this.opts.actionBarEl);
 		}
+		this.el.find('input.ui-input').on('click', function(){
+      $('#taskNameInput').focus();
+      $('#edit').css('display','none');
+      $('#submit').css('display','inline-block');
+      var input = document.getElementById("taskNameInput");
+      input.setSelectionRange(0, -1);
+    });
+    this.el.find('i.el-icon-edit').on('click',function(){
+      $('#taskNameInput').focus();
+      $('#edit').css('display','none');
+      $('#submit').css('display','inline-block');
+      var input = document.getElementById("taskNameInput");
+      input.setSelectionRange(0, -1);
+    });
+
+    this.el.find('input.ui-input').on('keyup', function(event){
+      if (event.keyCode == "13") {
+        $('#taskNameInput').blur();
+        $('#submit').css('display','none');
+        $('#edit').css('display','inline-block');
+      }
+    });
+
+    this.el.find('input.ui-input').on('blur', function(){
+      $('#submit').css('display','none');
+      $('#edit').css('display','inline-block');
+      var input = document.getElementById("taskNameInput");
+      input.setSelectionRange(0, 0);
+    });
 	}
 	getName(){
 		return this.el.find('.e-title #taskNameInput').val();
@@ -48,6 +78,7 @@ export default class UI extends Component {
 	setName(name){
 		return this.el.find('.e-title #taskNameInput').val(name);
 	}
+
 	// getBackButtonEl(){
 	// 	return this.el.find('.e-header .e-action-back');
 	// }
