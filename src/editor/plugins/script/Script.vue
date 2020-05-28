86493471ea6fdddb3c9ce0c3d5a94cfce3bbe6ca
fannie <fannie@tapdata.io>
2020-05-28 11:28:04 +0800

    提交一次eslint格式化后的文件

--- src/editor/plugins/script/Script.vue
+++ src/editor/plugins/script/Script.vue
@@ -25,10 +25,14 @@
 					</el-select>
 				</el-form-item>
 
-				<el-form-item :required="true" :label="$t('editor.cell.processor.script.form.script.label')" size="mini">
-			<JsEditor :code.sync="model.script"></JsEditor>
-			<!--			<el-input type="textarea" rows="10" v-model="model.script"></el-input>-->
-		</el-form-item>
+				<el-form-item
+					:required="true"
+					:label="$t('editor.cell.processor.script.form.script.label')"
+					size="mini"
+				>
+					<JsEditor :code.sync="model.script"></JsEditor>
+					<!--			<el-input type="textarea" rows="10" v-model="model.script"></el-input>-->
+				</el-form-item>
 			</el-form>
 		</div>
 	</div>
