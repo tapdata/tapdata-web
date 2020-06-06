8a23f87e1eda8c91e1d337d1999916f0c7a9fceb
fannie <fannie@tapdata.io>
2020-06-06 18:01:20 +0800

    fix some bug

--- src/editor/plugins/script/Script.vue
+++ src/editor/plugins/script/Script.vue
@@ -45,7 +45,7 @@
 					<JsEditor :code.sync="model.script" ref="jsEditor" :width.sync="width"></JsEditor>
 				</el-form-item>
 			</el-form>
-			<el-button class="btn-debug" type="primary" size="mini" :loading="sending" @click="showDebug">
+			<el-button class="btn-debug" type="primary" size="mini" :loading="!!sending" @click="showDebug">
 				{{ $t("editor.cell.processor.script.debug_button_label") }}
 			</el-button>
 		</div>
