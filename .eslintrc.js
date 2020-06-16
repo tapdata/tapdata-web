19f973359fc55937d4b5607248d0678f2d2ff934
Payne <payne@tapdata.io>
2020-06-16 21:45:34 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@
 module.exports = {
 	root: true,
 	parserOptions: {
-		parser: "babel-eslint",
+		parser: 'babel-eslint',
 		ecmaFeatures: true
 	},
 	env: {
@@ -14,36 +14,36 @@ module.exports = {
 	extends: [
 		// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
 		// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-		"plugin:vue/essential",
+		'plugin:vue/essential',
 		// https://github.com/standard/standard/blob/master/docs/RULES-en.md
-		"plugin:prettier/recommended",
-		"eslint:recommended"
+		'eslint:recommended',
+		'plugin:prettier/recommended'
 	],
 	// required to lint *.vue files
-	plugins: ["vue"],
+	plugins: ['vue'],
 	// add your custom rules here
 	rules: {
-		"prettier/prettier": [
-			"error",
+		'prettier/prettier': [
+			'error',
 			{
 				singleQuote: true,
-				trailingComma: "none",
+				trailingComma: 'none',
 				bracketSpacing: true,
 				jsxBracketSameLine: true
 			}
 		],
 		// allow async-await
-		"generator-star-spacing": "off",
-		"no-new": "off",
-		"one-var": "off",
-		"no-control-regex": "off",
-		"standard/computed-property-even-spacing": 0,
+		'generator-star-spacing': 'off',
+		'no-new': 'off',
+		'one-var': 'off',
+		'no-control-regex': 'off',
+		'standard/computed-property-even-spacing': 0,
 		// allow debugger during development
-		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		//'quotes': [1, 'single', 'backtick', 'double'],
-		"no-console": ["error", { allow: ["warn"] }],
-		"no-multiple-empty-lines": ["warn", { max: 2, maxEOF: 1 }],
-		semi: [1, "always"],
-		"no-undef": ["error"]
+		'no-console': ['error', { allow: ['warn'] }],
+		'no-multiple-empty-lines': ['warn', { max: 2, maxEOF: 1 }],
+		semi: [1, 'always'],
+		'no-undef': ['error']
 	}
 };
