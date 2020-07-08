d25d38f85d2f866bd0cb8f033ce92eb2b5457ed3
Payne <payne@tapdata.io>
2020-07-08 10:52:45 +0800

    fix: 重构databaseAttribute.vue，删除掉一些不必要的watch，优化了整个数据结构

--- src/editor/plugins/database/Database.js
+++ src/editor/plugins/database/Database.js
@@ -3,17 +3,17 @@
  * @date 3/4/20
  * @description
  */
-import { options } from "../../lib/rappid/config";
-import DatabaseAttribute from "./DatabaseAttribute";
-import i18n from "../../../i18n/i18n";
-import { FORM_DATA_KEY } from "../../constants";
+import { options } from '../../lib/rappid/config';
+import DatabaseAttribute from './DatabaseAttribute';
+import i18n from '../../../i18n/i18n';
+import { FORM_DATA_KEY } from '../../constants';
 
 export const databaseConfig = {
 	/**
 	 * the name of the subtype class.
 	 *
 	 */
-	type: "app.Database",
+	type: 'app.Database',
 
 	/**
 	 * define shape
@@ -24,7 +24,7 @@ export const databaseConfig = {
 		/**
 		 * extends exists shape
 		 */
-		extends: "app.BaseElement",
+		extends: 'app.BaseElement',
 
 		/**
 		 * object that contains properties to be assigned to every constructed instance of the subtype.
@@ -54,17 +54,17 @@ export const databaseConfig = {
 		defaultInstanceProperties: {
 			attrs: {
 				image: {
-					xlinkHref: "static/editor/o-DB.svg"
+					xlinkHref: 'static/editor/o-DB.svg'
 				},
 				label: {
-					text: i18n.t("editor.cell.data_node.database.name")
+					text: i18n.t('editor.cell.data_node.database.name')
 				}
 			},
 			[FORM_DATA_KEY]: {
-				connectionId: "",
+				connectionId: '',
 				includeTables: [],
 				dropTable: false,
-				type: "database"
+				type: 'database'
 			}
 		},
 		/**
@@ -88,8 +88,8 @@ export const databaseConfig = {
 		prototypeProperties: {
 			portLabelMarkup: [
 				{
-					tagName: "text",
-					selector: "portLabel"
+					tagName: 'text',
+					selector: 'portLabel'
 				}
 			],
 			isDataNode() {
@@ -103,10 +103,10 @@ export const databaseConfig = {
 			 */
 			validate: function(data) {
 				data = data || this.getFormData();
-				let name = this.attr("label/text");
-				if (!data) throw new Error(`${name}: ${i18n.t("editor.cell.validate.none_setting")}`);
+				let name = this.attr('label/text');
+				if (!data) throw new Error(`${name}: ${i18n.t('editor.cell.validate.none_setting')}`);
 				if (!data.connectionId)
-					throw new Error(`${name}: ${i18n.t("editor.cell.data_node.database.none_database")}`);
+					throw new Error(`${name}: ${i18n.t('editor.cell.data_node.database.none_database')}`);
 				return true;
 			},
 
@@ -116,7 +116,7 @@ export const databaseConfig = {
 			 * @return {boolean}
 			 */
 			allowTarget(targetCell) {
-				return ["app.Database"].includes(targetCell.get("type"));
+				return ['app.Database'].includes(targetCell.get('type'));
 			},
 
 			/**
@@ -125,7 +125,7 @@ export const databaseConfig = {
 			 * @return {boolean}
 			 */
 			allowSource(sourceCell) {
-				return ["app.Database"].includes(sourceCell.get("type"));
+				return ['app.Database'].includes(sourceCell.get('type'));
 			}
 		}
 		/**
@@ -144,82 +144,82 @@ export const databaseConfig = {
 			attrs: {
 				label: {
 					text: {
-						type: "content-editable",
-						label: "Text",
-						group: "text",
+						type: 'content-editable',
+						label: 'Text',
+						group: 'text',
 						index: 1
 					},
 					fontSize: {
-						type: "range",
+						type: 'range',
 						min: 5,
 						max: 80,
-						unit: "px",
-						label: "Font size",
-						group: "text",
-						when: { ne: { "attrs/label/text": "" } },
+						unit: 'px',
+						label: 'Font size',
+						group: 'text',
+						when: { ne: { 'attrs/label/text': '' } },
 						index: 2
 					},
 					fontFamily: {
-						type: "select-box",
+						type: 'select-box',
 						options: options.fontFamily,
-						label: "Font family",
-						group: "text",
-						when: { ne: { "attrs/label/text": "" } },
+						label: 'Font family',
+						group: 'text',
+						when: { ne: { 'attrs/label/text': '' } },
 						index: 3
 					},
 					fontWeight: {
-						type: "select-box",
+						type: 'select-box',
 						options: options.fontWeight,
-						label: "Font thickness",
-						group: "text",
-						when: { ne: { "attrs/label/text": "" } },
+						label: 'Font thickness',
+						group: 'text',
+						when: { ne: { 'attrs/label/text': '' } },
 						index: 4
 					},
 					fill: {
-						type: "color-palette",
+						type: 'color-palette',
 						options: options.colorPalette,
-						label: "Fill",
-						group: "text",
-						when: { ne: { "attrs/label/text": "" } },
+						label: 'Fill',
+						group: 'text',
+						when: { ne: { 'attrs/label/text': '' } },
 						index: 5
 					}
 				},
 				body: {
 					fill: {
-						type: "color-palette",
+						type: 'color-palette',
 						options: options.colorPalette,
-						label: "Fill",
-						group: "presentation",
+						label: 'Fill',
+						group: 'presentation',
 						index: 1
 					},
 					stroke: {
-						type: "color-palette",
+						type: 'color-palette',
 						options: options.colorPalette,
-						label: "Outline",
-						group: "presentation",
+						label: 'Outline',
+						group: 'presentation',
 						index: 2
 					},
 					strokeWidth: {
-						type: "range",
+						type: 'range',
 						min: 0,
 						max: 30,
 						step: 1,
 						defaultValue: 1,
-						unit: "px",
-						label: "Outline thickness",
-						group: "presentation",
-						when: { ne: { "attrs/body/stroke": "transparent" } },
+						unit: 'px',
+						label: 'Outline thickness',
+						group: 'presentation',
+						when: { ne: { 'attrs/body/stroke': 'transparent' } },
 						index: 3
 					},
 					strokeDasharray: {
-						type: "select-box",
+						type: 'select-box',
 						options: options.strokeStyle,
-						label: "Outline style",
-						group: "presentation",
+						label: 'Outline style',
+						group: 'presentation',
 						when: {
 							and: [
-								{ ne: { "attrs/body/stroke": "transparent" } },
-								{ ne: { "attrs/body/strokeWidth": 0 } }
+								{ ne: { 'attrs/body/stroke': 'transparent' } },
+								{ ne: { 'attrs/body/strokeWidth': 0 } }
 							]
 						},
 						index: 4
@@ -229,11 +229,11 @@ export const databaseConfig = {
 		},
 		groups: {
 			presentation: {
-				label: "Presentation",
+				label: 'Presentation',
 				index: 1
 			},
 			text: {
-				label: "Text",
+				label: 'Text',
 				index: 2
 			}
 		}
@@ -247,7 +247,7 @@ export const databaseConfig = {
 		/**
 		 * 左侧列表的分组名称，默认有：数据节点:data; 处理节点：process；标准图形：standard
 		 */
-		group: "data",
+		group: 'data',
 		/**
 		 * 界面显示的分组名称
 		 */
@@ -256,35 +256,35 @@ export const databaseConfig = {
 		size: { width: 5, height: 4 },
 		attrs: {
 			root: {
-				dataTooltip: i18n.t("editor.cell.data_node.database.tip"),
-				dataTooltipPosition: "left",
-				dataTooltipPositionSelector: ".joint-stencil"
+				dataTooltip: i18n.t('editor.cell.data_node.database.tip'),
+				dataTooltipPosition: 'left',
+				dataTooltipPositionSelector: '.joint-stencil'
 			},
 			body: {
 				rx: 2,
 				ry: 2,
-				stroke: "#fff",
-				fill: "#fff",
+				stroke: '#fff',
+				fill: '#fff',
 				strokeWidth: 0,
-				strokeDasharray: "0"
+				strokeDasharray: '0'
 			},
 			image: {
-				xlinkHref: "static/editor/database2.svg",
-				refWidth: "60%",
-				refHeight: "60%",
-				refX: "2%",
-				refY: "0%"
+				xlinkHref: 'static/editor/database2.svg',
+				refWidth: '60%',
+				refHeight: '60%',
+				refX: '2%',
+				refY: '0%'
 			},
 			label: {
-				text: i18n.t("editor.cell.data_node.database.name"),
-				textAnchor: "middle",
-				fill: "#666",
-				fontFamily: "Roboto Condensed",
-				fontWeight: "Normal",
+				text: i18n.t('editor.cell.data_node.database.name'),
+				textAnchor: 'middle',
+				fill: '#666',
+				fontFamily: 'Roboto Condensed',
+				fontWeight: 'Normal',
 				fontSize: 10,
 				strokeWidth: 0,
-				refX: "75%",
-				refY: "40%",
+				refX: '75%',
+				refY: '40%',
 				x: -32,
 				y: 27
 			}
