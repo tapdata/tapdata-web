aef3d20d03be66aee6edf569d841d0553ddf37f6
Payne <payne@tapdata.io>
2020-07-18 17:06:54 +0800

    fix some bug

--- src/editor/plugins/collection/CollectionAttribute.vue
+++ src/editor/plugins/collection/CollectionAttribute.vue
@@ -90,7 +90,8 @@
 								? $t('editor.cell.data_node.collection.form.fieldFilter.placeholderKeep')
 								: $t('editor.cell.data_node.collection.form.fieldFilter.placeholderDelete')
 						"
-						@removeTag="removeTag"
+						@input="handleFilterChange()"
+						@removeTag="handleFilterChange"
 					></PrimaryKeyInput>
 				</el-form-item>
 				<el-form-item
@@ -268,11 +269,6 @@ export default {
 					}
 				}
 			}
-		},
-		'model.fieldFilter': {
-			handler() {
-				this.handleFilterChange();
-			}
 		}
 	},
 	data() {
@@ -386,50 +382,55 @@ export default {
 				if (f) fn(f);
 			});
 		},
-		removeTag() {
-			// let fieldNameArr = this.model.fieldFilter.split(',');
-			// let fieldFilter = this.getFieldData(fieldNameArr);
-			// fieldNameArr.splice(
-			// 	fieldNameArr.findIndex(f => f.field_name === tagName),
-			// 	1
-			// );
-			// this.model.fieldFilter = fieldNameArr.join(',');
-			// fieldFilter.forEach(f => {
-			// 	if (f.parent === tagName) {
-			// 		this.removeTag(f.field_name);
-			// 	}
-			// });
-		},
-		handleFilterChange() {
+		handleFilterChange(tagName) {
+			if (this.model.fieldFilterType === 'retainedField') {
+				this.checkFieldChild(tagName);
+			}
+
 			//根据类型判断 fieldFilterType 不过滤字段Keep all fields、保留字段Retained field、删除字段Delete field。默认显示：不过滤字段。
 			let fieldFilter = this.model.fieldFilter ? this.model.fieldFilter.split(',') : [];
-			if (fieldFilter.length === 0) {
-				return;
-			}
+
+			let fieldList = this.getFieldData(fieldFilter);
 			if (this.model.fieldFilterType === 'retainedField') {
-				this.handleRetainedField(this.getFieldData(fieldFilter));
+				this.handleRetainedField(fieldList);
 			} else if (this.model.fieldFilterType === 'deleteField') {
-				this.handleDeleteField(this.getFieldData(fieldFilter));
+				this.handleDeleteField(fieldList);
 			}
 			this.$emit('schemaChange', _.cloneDeep(this.defaultSchema));
+		},
+
+		checkFieldChild(name) {
+			let fields = this.model.fieldFilter.split(',');
+			let fieldFilter = this.getFieldData(fields);
+			if (name) {
+				let index = fields.findIndex(f => f === name);
+				fields.splice(index, 1);
+				this.model.fieldFilter = fields.join(',');
+			}
+			for (let i = 0; i < fieldFilter.length; i++) {
+				const f = fieldFilter[i];
+				if (name && f.parent === name) {
+					this.checkFieldChild(f.field_name);
+				} else if (!name && f.parent && !fieldFilter.find(item => item.field_name === f.parent)) {
+					this.model.fieldFilter += ',' + f.parent;
+					this.checkFieldChild();
+					break;
+				}
+			}
 		},
 		handleRetainedField(fieldFilter) {
 			this.model.operations = [];
 
 			fieldFilter.forEach(f => {
-				if (f.parent && !fieldFilter.find(item => item.field_name === f.parent)) {
-					this.model.fieldFilter += ',' + f.parent;
-				} else {
-					let self = this;
-					let ops = self.model.operations.filter(v => v.op === 'RETAINED' && v.id === f.id);
-					let op;
-					if (ops.length === 0) {
-						op = Object.assign(_.cloneDeep(RETAINED_OPS_TPL), {
-							id: f.id,
-							field: f.field_name
-						});
-						self.model.operations.push(op);
-					}
+				let self = this;
+				let ops = self.model.operations.filter(v => v.op === 'RETAINED' && v.id === f.id);
+				let op;
+				if (ops.length === 0) {
+					op = Object.assign(_.cloneDeep(RETAINED_OPS_TPL), {
+						id: f.id,
+						field: f.field_name
+					});
+					self.model.operations.push(op);
 				}
 			});
 		},
