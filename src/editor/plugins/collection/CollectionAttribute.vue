e4056bec6d2d97d03cfd71163d136f28240f762a
Payne <payne@tapdata.io>
2020-07-20 11:15:32 +0800

    fix some bug

--- src/editor/plugins/collection/CollectionAttribute.vue
+++ src/editor/plugins/collection/CollectionAttribute.vue
@@ -406,8 +406,10 @@ export default {
 			let fieldFilter = this.getFieldData(fields);
 			if (name) {
 				let index = fields.findIndex(f => f === name);
-				fields.splice(index, 1);
-				this.model.fieldFilter = fields.join(',');
+				if (index >= 0) {
+					fields.splice(index, 1);
+					this.model.fieldFilter = fields.join(',');
+				}
 			}
 			for (let i = 0; i < fieldFilter.length; i++) {
 				const f = fieldFilter[i];
@@ -439,8 +441,10 @@ export default {
 		getFieldData(fieldFilter) {
 			let currentFiled = [];
 			fieldFilter.forEach(f => {
-				let op = this.defaultSchema.fields.find(item => item.field_name === f);
-				currentFiled.push(op);
+				if (f && f.length) {
+					let op = this.defaultSchema.fields.find(item => item.field_name === f);
+					currentFiled.push(op);
+				}
 			});
 			return currentFiled;
 		},
