956271e76724609473bb7d83bed4bc540f0d3b26
Payne <payne@tapdata.io>
2020-07-31 14:28:11 +0800

    fix: 修复老项目部分路由在新框架不能跳转问题以及老项目路由跳转外部框架路由不变化的问题

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,207 @@ const view = path => () => import(`../view/${path}`);
 
 Vue.use(Router);
 
+const childRoutes = [
+	{
+		path: '/dashboard',
+		name: 'dashboard',
+		component: view('dashboard/Dashboard'),
+		meta: { requiresAuth: true }
+	},
+	{
+		path: '/connections',
+		name: 'connections',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/connections' }
+	},
+	{
+		path: '/connection',
+		name: 'connection',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/connection' }
+	},
+	{
+		path: '/connection/:id',
+		name: 'editConnection',
+		component: view('ExternalLink'),
+		meta: { url: '/old/index.html#/connection' }
+	},
+	{
+		path: '/metadataDefinition',
+		name: 'metadataDefinition',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/metadataDefinition' }
+	},
+	{
+		path: '/metadataInstances/:id',
+		name: 'metadataInstances',
+		component: view('ExternalLink')
+	},
+	{
+		path: '/dataQuality',
+		name: 'dataQuality',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/dataQuality' }
+	},
+	{
+		path: '/dataQuality/:id',
+		name: 'dataQualityDetail',
+		component: view('ExternalLink')
+	},
+	{
+		path: '/ttl',
+		name: 'timeToLive',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/ttl' }
+	},
+	{
+		path: '/dataRules',
+		name: 'dataRules',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/dataRules' }
+	},
+	{
+		path: '/dictionary',
+		name: 'dictionary',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/dictionary' }
+	},
+	{
+		path: '/modules',
+		name: 'modules',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/modules' }
+	},
+	{
+		path: '/module/:id',
+		name: 'editModule',
+		component: view('ExternalLink')
+	},
+	{
+		path: '/dataExplorer',
+		name: 'dataExplorer',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/dataExplorer' }
+	},
+	{
+		path: '/apiDocAndTest',
+		name: 'apiDocAndTest',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/apiDocAndTest' }
+	},
+	{
+		path: '/apiAnalysis',
+		name: 'apiAnalysis',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/apiAnalysis' }
+	},
+	{
+		path: '/applications',
+		name: 'applications',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/applications' }
+	},
+	{
+		path: '/apiServers',
+		name: 'apiServers',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/apiServers' }
+	},
+	{
+		path: '/dataCollect',
+		name: 'dataCollect',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/dataCollect' }
+	},
+	{
+		path: '/tasks',
+		name: 'tasks',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/tasks' }
+	},
+	{
+		path: '/agentdownload',
+		name: 'agentdownload',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/agentdownload' }
+	},
+	{
+		path: '/agents',
+		name: 'agents',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/agents' }
+	},
+	{
+		path: '/serversOversee',
+		name: 'serversOversee',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/serversOversee' }
+	},
+	{
+		path: '/users',
+		name: 'users',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/users' }
+	},
+	{
+		path: '/journal',
+		name: 'journal',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/journal' }
+	},
+	{
+		path: '/roles',
+		name: 'roles',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/roles' }
+	},
+	{
+		path: '/role',
+		name: 'role',
+		component: view('ExternalLink')
+	},
+	{
+		path: '/role/:id',
+		name: 'editRole',
+		component: view('ExternalLink')
+	},
+	{
+		path: '/settings',
+		name: 'settings',
+		component: view('ExternalLink'),
+		meta: { requiresAuth: true, url: '/old/index.html#/settings' }
+	},
+	/*-----------------------------------------------------------------*/
+	{
+		path: '/clusterManagement',
+		name: 'clusterManagement',
+		component: view('clusterManagement/layout'),
+		meta: { requiresAuth: true }
+	},
+	{
+		path: '/dataFlows',
+		name: 'dataFlows',
+		component: view('task/DataFlows'),
+		meta: { requiresAuth: true }
+	},
+	{
+		path: '/dataMap',
+		name: 'dataMap',
+		component: view('dataMap/DataMap'),
+		meta: { requiresAuth: true }
+	},
+	{
+		path: '/upload',
+		name: 'upload',
+		component: view('Upload')
+	},
+	{
+		path: '/apiInfo',
+		name: 'apiInfo',
+		component: view('job/apiInfo')
+	}
+];
+
 const router = new Router({
 	routes: [
 		{
@@ -25,175 +226,7 @@ const router = new Router({
 			name: 'layout',
 			redirect: 'dashboard',
 			component: view('Layout'),
-			children: [
-				{
-					path: '/dashboard',
-					name: 'dashboard',
-					component: view('dashboard/Dashboard'),
-					meta: { requiresAuth: true }
-				},
-				{
-					path: '/connections',
-					name: 'connections',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/connections' }
-				},
-				{
-					path: '/connection',
-					name: 'connection',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/connection' }
-				},
-				{
-					path: '/metadataDefinition',
-					name: 'metadataDefinition',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/metadataDefinition' }
-				},
-				{
-					path: '/dataQuality',
-					name: 'dataQuality',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/dataQuality' }
-				},
-				{
-					path: '/ttl',
-					name: 'timeToLive',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/ttl' }
-				},
-				{
-					path: '/dataRules',
-					name: 'dataRules',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/dataRules' }
-				},
-				{
-					path: '/dictionary',
-					name: 'dictionary',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/dictionary' }
-				},
-				{
-					path: '/modules',
-					name: 'modules',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/modules' }
-				},
-				{
-					path: '/dataExplorer',
-					name: 'dataExplorer',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/dataExplorer' }
-				},
-				{
-					path: '/apiDocAndTest',
-					name: 'apiDocAndTest',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/apiDocAndTest' }
-				},
-				{
-					path: '/apiAnalysis',
-					name: 'apiAnalysis',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/apiAnalysis' }
-				},
-				{
-					path: '/applications',
-					name: 'applications',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/applications' }
-				},
-				{
-					path: '/apiServers',
-					name: 'apiServers',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/apiServers' }
-				},
-				{
-					path: '/dataCollect',
-					name: 'dataCollect',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/dataCollect' }
-				},
-				{
-					path: '/tasks',
-					name: 'tasks',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/tasks' }
-				},
-				{
-					path: '/agentdownload',
-					name: 'agentdownload',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/agentdownload' }
-				},
-				{
-					path: '/agents',
-					name: 'agents',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/agents' }
-				},
-				{
-					path: '/serversOversee',
-					name: 'serversOversee',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/serversOversee' }
-				},
-				{
-					path: '/users',
-					name: 'users',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/users' }
-				},
-				{
-					path: '/journal',
-					name: 'journal',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/journal' }
-				},
-				{
-					path: '/roles',
-					name: 'roles',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/roles' }
-				},
-				{
-					path: '/settings',
-					name: 'settings',
-					component: view('ExternalLink'),
-					meta: { requiresAuth: true, url: '/old/index.html#/settings' }
-				},
-				/*-----------------------------------------------------------------*/
-				{
-					path: '/clusterManagement',
-					name: 'clusterManagement',
-					component: view('clusterManagement/layout'),
-					meta: { requiresAuth: true }
-				},
-				{
-					path: '/dataFlows',
-					name: 'dataFlows',
-					component: view('task/DataFlows'),
-					meta: { requiresAuth: true }
-				},
-				{
-					path: '/dataMap',
-					name: 'dataMap',
-					component: view('dataMap/DataMap'),
-					meta: { requiresAuth: true }
-				},
-				{
-					path: '/upload',
-					name: 'upload',
-					component: view('Upload')
-				},
-				{
-					path: '/apiInfo',
-					name: 'apiInfo',
-					component: view('job/apiInfo')
-				}
-			]
+			children: childRoutes
 		}
 	]
 });
@@ -279,3 +312,4 @@ router.beforeEach(async (to, from, next) => {
 });
 
 export default router;
+export { childRoutes };
