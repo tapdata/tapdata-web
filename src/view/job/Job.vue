4760ac55ed712c79f87113b196fac8b09887d0d7
Payne <payne@tapdata.io>
2020-08-15 14:58:24 +0800

    fix: 修复多个账号同时操作一个dataflow时，状态变更后设置页面仍然可以编辑的问题

--- src/view/job/Job.vue
+++ src/view/job/Job.vue
@@ -1106,7 +1106,7 @@ export default {
 		 */
 		showSetting() {
 			log('Job.showSetting');
-			this.editor.showSetting(this.$route.query.isMoniting == 'true');
+			this.editor.showSetting(!this.editable);
 		},
 
 		/**
