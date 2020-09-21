b242c688e2b4805d1ccc116b61cafcfa1b72120f
许智清 <zhiqingxu@tapdata.io>
2020-09-21 10:40:03 +0800

    table flow

--- src/view/task/TableFlows.vue
+++ src/view/task/TableFlows.vue
@@ -1,228 +1,228 @@
 <template>
-	<el-container class="table-flows-wrap">
-		<div class="panel-left" v-if="formData.panelFlag">
-			<metaData v-on:nodeClick="nodeClick"></metaData>
+	<div style="height: 100%;">
+		<div style=" border-bottom: 1px solid #e2e2e7;text-align:center">
+			<div style="border-top: 2px solid gray; float:left; width:180px; height: 40px; background-color:#f5f5f5; ">
+				<span style="position: relative; top:20%;">任务视图</span>
+			</div>
+			<div
+				style="border-top: 2px solid #48b6e2; border-left: 1px solid #e2e2e7;  border-right: 1px solid #e2e2e7;float:left; width:180px; height: 40px;"
+			>
+				<span style="position: relative; top:20%;">表视图</span>
+			</div>
+			<div style="clear:both;"></div>
 		</div>
-		<el-container>
-			<el-header height="auto" style="padding-top: 20px;">
-				<el-form class="search-bar" size="mini" :inline="true" :model="searchParams">
-					<el-form-item>
-						<i class="iconfont icon-xiangshangzhanhang"></i>
-						<span>{{ formData.panelFlag ? $t('dataFlow.closeSetting') : $t('dataFlow.openPanel') }}</span>
-					</el-form-item>
-					<el-form-item>
-						<el-input
-							clearable
-							style="width: 300px;"
-							v-model="searchParams.keyword"
-							:placeholder="$t('dataFlow.searchPlaceholder')"
-							@change="screenFn"
-						></el-input>
-					</el-form-item>
-					<el-form-item>
-						<el-select
-							v-model="formData.status"
-							size="mini"
-							clearable
-							:placeholder="$t('dataFlow.taskStatusPlaceholder')"
-							style="width:160px"
-							@change="screenFn"
-						>
-							<el-option
-								v-for="item in options"
-								:key="item.value"
-								:label="item.label"
-								:value="item.value"
-							></el-option>
-						</el-select>
-					</el-form-item>
-					<el-form-item>
-						<el-select
-							v-model="formData.way"
-							size="mini"
-							clearable
-							:placeholder="$t('dataFlow.taskSettingPlaceholder')"
-							style="width:160px"
-							@change="screenFn"
-						>
-							<el-option
-								v-for="item in optionsKey"
-								:key="item.value"
-								:label="item.label"
-								:value="item.value"
-							></el-option>
-						</el-select>
-					</el-form-item>
-					<el-form-item>
-						<el-select
-							v-model="formData.executionStatus"
-							size="mini"
-							clearable
-							:placeholder="$t('dataFlow.executionStatus')"
-							style="width:160px"
-							@change="screenFn"
-						>
-							<el-option
-								v-for="opt in ['initializing', 'cdc', 'initialized']"
-								:key="opt"
-								:label="$t('dataFlow.status.' + opt)"
-								:value="opt"
-							></el-option>
-						</el-select>
-					</el-form-item>
+		<el-container class="table-flows-wrap">
+			<div class="panel-left" v-if="formData.panelFlag">
+				<metaData v-on:nodeClick="nodeClick"></metaData>
+			</div>
+			<el-container>
+				<el-header height="auto" style="padding-top: 20px;">
+					<el-form class="search-bar" size="mini" :inline="true">
+						<el-form-item>
+							<i class="iconfont icon-xiangshangzhanhang"></i>
+							<span>{{
+								formData.panelFlag ? $t('dataFlow.closeSetting') : $t('dataFlow.openPanel')
+							}}</span>
+						</el-form-item>
+						<el-form-item>
+							<el-input
+								clearable
+								style="width: 300px;"
+								v-model="formData.keyword"
+								:placeholder="$t('dataFlow.searchPlaceholder')"
+								@change="screenFn"
+							></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-select
+								v-model="formData.status"
+								size="mini"
+								clearable
+								:placeholder="$t('dataFlow.taskStatusPlaceholder')"
+								style="width:160px"
+								@change="screenFn"
+							>
+								<el-option
+									v-for="item in options"
+									:key="item.value"
+									:label="item.label"
+									:value="item.value"
+								></el-option>
+							</el-select>
+						</el-form-item>
+						<el-form-item>
+							<el-select
+								v-model="formData.way"
+								size="mini"
+								clearable
+								:placeholder="$t('dataFlow.taskSettingPlaceholder')"
+								style="width:160px"
+								@change="screenFn"
+							>
+								<el-option
+									v-for="item in optionsKey"
+									:key="item.value"
+									:label="item.label"
+									:value="item.value"
+								></el-option>
+							</el-select>
+						</el-form-item>
+						<el-form-item>
+							<el-select
+								v-model="formData.executionStatus"
+								size="mini"
+								clearable
+								:placeholder="$t('dataFlow.executionStatus')"
+								style="width:160px"
+								@change="screenFn"
+							>
+								<el-option
+									v-for="opt in ['initializing', 'cdc', 'initialized']"
+									:key="opt"
+									:label="$t('dataFlow.status.' + opt)"
+									:value="opt"
+								></el-option>
+							</el-select>
+						</el-form-item>
 
-					<el-form-item>
-						<el-button style="padding: 7px;" icon="el-icon-refresh-right" @click="reset()"></el-button>
-					</el-form-item>
-					<el-form-item style="margin-right: 0;flex:1;text-align:right;">
-						<el-button>
-							<i class="iconfont icon-jiekoufuwu"></i>
-							<span>批量校验</span>
-						</el-button>
-					</el-form-item>
-				</el-form>
-			</el-header>
-			<el-main class="main">
-				<div class="table">
-					<el-table
-						:data="page.data"
-						height="100%"
-						style="width: 100%"
-						v-loading="loading"
-						:row-class-name="rowClassHandler"
-						@sort-change="sortHandler"
-						@selection-change="selectHandler"
+						<el-form-item>
+							<el-button style="padding: 7px;" icon="el-icon-refresh-right" @click="reset()"></el-button>
+						</el-form-item>
+						<el-form-item style="margin-right: 0;flex:1;text-align:right;">
+							<el-button>
+								<i class="iconfont icon-jiekoufuwu"></i>
+								<span>批量校验</span>
+							</el-button>
+						</el-form-item>
+					</el-form>
+				</el-header>
+				<el-main class="main">
+					<div class="table">
+						<el-table
+							:data="page.data"
+							height="100%"
+							style="width: 100%"
+							v-loading="loading"
+							:row-class-name="rowClassHandler"
+							@sort-change="sortHandler"
+							@selection-change="selectHandler"
+						>
+							<el-table-column type="selection" width="44" align="center"></el-table-column>
+							<el-table-column sortable="custom" label="源表/目标表">
+								<template slot-scope="scope">
+									<div class="table-item">
+										<div class="table-source">[S] {{ scope.row.stages.name }}</div>
+										<div class="from-db">{{ scope.row.databaseName }}</div>
+										<div v-for="item in scope.row.outf" :key="item.name">
+											<div class="table-target">[T] {{ item.name }}</div>
+											<div class="from-db">{{ item.databaseName }}</div>
+										</div>
+									</div>
+								</template>
+							</el-table-column>
+							<el-table-column label="所在任务/执行时间" width="150" align="center">
+								<template slot-scope="scope">
+									<div class="table-item">
+										<div>{{ scope.row.name }}</div>
+										<div class="dark-color">{{ scope.row.startTime }}</div>
+									</div>
+								</template>
+							</el-table-column>
+							<el-table-column sortable="custom" label="状态" width="120">
+								<template slot-scope="scope">
+									<img
+										v-if="scope.row.status == 'running'"
+										style="width: 12px;height: 15px;vertical-align: middle;"
+										src="../../../static/editor/running-blue.svg"
+									/>
+									<span class="primary-color" v-show="scope.row.name">{{
+										scope.row.statusLabel
+									}}</span>
+								</template>
+							</el-table-column>
+							<el-table-column sortable="custom" label="阶段" width="120">
+								<template slot-scope="scope">
+									<span>
+										初始化中 (<span class="dark-color">{{ scope.row.num }}%</span>)
+									</span>
+								</template>
+							</el-table-column>
+							<el-table-column label="输出/输入" width="120">
+								<template slot-scope="scope">
+									<div class="table-item">
+										<div>
+											<span class="dark-color">[输出]</span>&nbsp;
+											<span>{{ scope.row.out }}</span>
+										</div>
+										<div>
+											<span class="dark-color">[输入]</span>&nbsp;
+											<span>{scope.row.in}}</span>
+										</div>
+									</div>
+								</template>
+							</el-table-column>
+							<el-table-column sortable="custom" label="速度(条/s)" width="120" align="center">
+								<template slot-scope="scope">
+									<span>{{ scope.row.out }}</span>
+								</template>
+							</el-table-column>
+							<el-table-column lsortable="custom" label="行数对比" width="150" align="center">
+								<template slot-scope="scope">
+									<div class="table-item">
+										<div>{{ scope.row.in }}/{{ scope.row.out }}</div>
+										<div class="dark-color">2020-02-21 12:22:33</div>
+									</div>
+								</template>
+							</el-table-column>
+							<el-table-column sortable="custom" label="传输状况" width="120" align="center">
+								<template slot-scope="scope">
+									<div v-if="scope.row.out < 1000">
+										<el-tag size="mini" type="success">Normal</el-tag>
+									</div>
+									<div>
+										<el-tag size="mini" type="warning">Warning</el-tag>
+									</div>
+									<div>
+										<el-tag size="mini" type="danger">Error</el-tag>
+									</div>
+								</template>
+							</el-table-column>
+							<el-table-column width="120" align="center">
+								<template>
+									<el-link size="mini" type="primary">行数检查</el-link><br />
+									<el-link size="mini" type="primary">忽略异常</el-link>
+								</template>
+							</el-table-column>
+						</el-table>
+					</div>
+					<el-pagination
+						background
+						class="pagination"
+						:current-page.sync="page.current"
+						:page-sizes="[10, 20, 50, 100]"
+						:page-size.sync="page.size"
+						layout="total, sizes, prev, pager, next, jumper"
+						:total="page.total"
+						@size-change="getData(1)"
+						@current-change="getData"
 					>
-						<el-table-column type="selection" width="44" align="center"></el-table-column>
-						<el-table-column sortable="custom" label="源表/目标表">
-							<template slot-scope="scope">
-								<div class="table-item">
-									<div class="table-source">[S] {{ scope.row.stages.name }}</div>
-									<div class="from-db">{{ scope.row.databaseName }}</div>
-									<div v-for="item in scope.row.outf" :key="item.name">
-										<div class="table-target">[T] {{ item.name }}</div>
-										<div class="from-db">{{ item.databaseName }}</div>
-									</div>
-								</div>
-							</template>
-						</el-table-column>
-						<el-table-column label="所在任务/执行时间" width="150" align="center">
-							<template slot-scope="scope">
-								<div class="table-item">
-									<div>{{ scope.row.name }}</div>
-									<div class="dark-color">{{ scope.row.startTime }}</div>
-								</div>
-							</template>
-						</el-table-column>
-						<el-table-column sortable="custom" label="状态" width="120">
-							<template slot-scope="scope">
-								<img
-									v-if="scope.row.status == 'running'"
-									style="width: 12px;height: 15px;vertical-align: middle;"
-									src="../../../static/editor/running-blue.svg"
-								/>
-								<span class="primary-color" v-show="scope.row.name">{{ scope.row.statusLabel }}</span>
-							</template>
-						</el-table-column>
-						<el-table-column sortable="custom" label="阶段" width="120">
-							<template slot-scope="scope">
-								<span>
-									初始化中 (<span class="dark-color">{{ scope.row.num }}%</span>)
-								</span>
-							</template>
-						</el-table-column>
-						<el-table-column label="输出/输入" width="120">
-							<template slot-scope="scope">
-								<div class="table-item">
-									<div>
-										<span class="dark-color">[输出]</span>&nbsp;
-										<span>{{ scope.row.out }}</span>
-									</div>
-									<div>
-										<span class="dark-color">[输入]</span>&nbsp;
-										<span>{scope.row.in}}</span>
-									</div>
-								</div>
-							</template>
-						</el-table-column>
-						<el-table-column label="更新/插入/删除" width="120">
-							<template slot-scope="scope">
-								<div class="table-item">
-									<div>
-										<span class="dark-color">[更新]</span>&nbsp;
-										<span>{{ scope.row.out }}</span>
-									</div>
-									<div>
-										<span class="dark-color">[插入]</span>&nbsp;
-										<span>{{ scope.row.out }}</span>
-									</div>
-									<div>
-										<span class="dark-color">[删除]</span>&nbsp;
-										<span>{{ scope.row.out }}</span>
-									</div>
-								</div>
-							</template>
-						</el-table-column>
-						<el-table-column sortable="custom" label="速度(条/s)" width="120" align="center">
-							<template slot-scope="scope">
-								<span>{{ scope.row.out }}</span>
-							</template>
-						</el-table-column>
-						<el-table-column sortable="custom" label="传输状况" width="120" align="center">
-							<template slot-scope="scope">
-								<div v-if="scope.row.out < 1000">
-									<el-tag size="mini" type="success">Normal</el-tag>
-								</div>
-								<div>
-									<el-tag size="mini" type="warning">Warning</el-tag>
-								</div>
-								<div>
-									<el-tag size="mini" type="danger">Error</el-tag>
-								</div>
-							</template>
-						</el-table-column>
-						<el-table-column lsortable="custom" label="行数对比" width="150" align="center">
-							<template slot-scope="scope">
-								<div class="table-item">
-									<div>{{ scope.row.in }}/{{ scope.row.out }}</div>
-									<div class="dark-color">2020-02-21 12:22:33</div>
-								</div>
-							</template>
-						</el-table-column>
-						<el-table-column width="120" align="center">
-							<template>
-								<el-link size="mini" type="primary">行数检查</el-link><br />
-								<el-link size="mini" type="primary">忽略异常</el-link>
-							</template>
-						</el-table-column>
-					</el-table>
-				</div>
-				<el-pagination
-					background
-					class="pagination"
-					:current-page.sync="page.current"
-					:page-sizes="[10, 20, 50, 100]"
-					:page-size.sync="page.size"
-					layout="total, sizes, prev, pager, next, jumper"
-					:total="page.total"
-					@size-change="search(1)"
-					@current-change="search"
-				>
-				</el-pagination>
-			</el-main>
+					</el-pagination>
+				</el-main>
+			</el-container>
+			<SelectClassify
+				ref="SelectClassify"
+				:dialogVisible="dialogVisible"
+				type="dataflow"
+				:tagLists="tagList"
+				v-on:dialogVisible="handleDialogVisible"
+				v-on:operationsClassify="handleOperationClassify"
+			></SelectClassify>
 		</el-container>
-		<SelectClassify
-			ref="SelectClassify"
-			:dialogVisible="dialogVisible"
-			type="dataflow"
-			:tagLists="tagList"
-			v-on:dialogVisible="handleDialogVisible"
-			v-on:operationsClassify="handleOperationClassify"
-		></SelectClassify>
-	</el-container>
+	</div>
 </template>
 
 <script>
+import axios from 'axios';
 import factory from '../../api/factory';
 import ws from '../../api/ws';
 import metaData from '../metaData';
@@ -319,8 +319,8 @@ export default {
 		};
 	},
 	created() {
+		axios.get('/api/web/users/gateway/userInfo4Web');
 		this.getFlowOptions();
-		this.search(1);
 		this.getData();
 	},
 	methods: {
@@ -433,7 +433,7 @@ export default {
 			}
 		},
 		screenFn() {
-			this.currentPage = 1;
+			this.page.current = 1;
 			this.getData();
 		},
 		keyupEnter() {
@@ -443,17 +443,15 @@ export default {
 				}
 			};
 		},
-		async getData() {
+		async getData(pageNum) {
 			this.loading = true;
-
 			this.$store.commit('tableFlows', this.formData);
-
+			let { current, size, sortBy, order } = this.page;
+			//let { keyword, flowId } = this.searchParams;
+			let currentPage = pageNum || current + 1;
 			let where = {};
+
 			if (!parseInt(this.$cookie.get('isAdmin'))) where.user_id = { regexp: `^${this.$cookie.get('user_id')}$` };
-			let order = 'createTime DESC';
-			if (this.order) {
-				order = this.order;
-			}
 			if (this.formData) {
 				if (this.formData.status && this.formData.status !== '') where.status = this.formData.status;
 				if (this.formData.way && this.formData.way !== '') where['setting.sync_type'] = this.formData.way;
@@ -466,12 +464,12 @@ export default {
 				};
 			}
 			let _params = Object.assign({}, where, {
-				order: order,
-				limit: this.pagesize,
-				skip: (this.currentPage - 1) * this.pagesize
+				order: order === 'descending' ? 'DESC' : 'ASC',
+				orderBy: sortBy,
+				limit: size,
+				skip: (currentPage - 1) * size
 			});
 
-			_params = { name: 'e', skip: 0, limit: 20 };
 			await dataFlows.tableFlow(_params).then(res => {
 				if (res.statusText === 'OK' || res.status === 200) {
 					if (res.data) {
