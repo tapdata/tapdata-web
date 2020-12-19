aafa45d200ff3a90e619ea5646c688c734cd8d79
Payne <payne@tapdata.io>
2020-12-19 16:47:35 +0800

    fix: 修复自定义节点配置

--- templateSetting.js
+++ templateSetting.js
@@ -1,4 +1,4 @@
-export default [
+let nodeConfig = [
 	{
 		"nodeConfig": {
 			"name": "加前后缀",
@@ -34,41 +34,42 @@ export default [
 				}
 			]
 		},
-		"scriptTemplate": `function process(record) {
-			//当前模型的字段列表，可全选，多选
-			var selected_fields = \${fields};
-			//前缀还是后缀 (单选按钮） 前缀：before，后缀：after;
-			var beforeOrAfter = \${type};
-			//字符串输入
-			var text = \${text};
+		"scriptTemplate":
+`function process(record) {
+	//当前模型的字段列表，可全选，多选
+	var selected_fields = \${fields};
+	//前缀还是后缀 (单选按钮） 前缀：before，后缀：after;
+	var beforeOrAfter = \${type};
+	//字符串输入
+	var text = \${text};
 
-			// validations
-			if(!text || !selected_fields) {
-				// report error
-				log.warn("Missing processing node configuration");
-				return record;
-			}
-			//核心代码
-			selected_fields.forEach(function(fieldName) {
-				if(fieldName.indexOf('.') >= 0) {
-					log.error("字段 "+ fieldName + " 加前后缀失败，暂不支持内嵌字段");
-					return record;
-				}
-				var newFieldName = "";
-				if(beforeOrAfter === "before") {
-					newFieldName = text + fieldName;
-				} else {
-					newFieldName = fieldName + text;
-				}
-				if(newFieldName in record){
-					log.error("Field "+ newFieldName + " already exists");
-					return record;
-				}
-				record[newFieldName] = record[fieldName];
-				record.remove(fieldName);
-			})
+	// validations
+	if(!text || !selected_fields) {
+		// report error
+		log.warn("Missing processing node configuration");
+		return record;
+	}
+	//核心代码
+	selected_fields.forEach(function(fieldName) {
+		if(fieldName.indexOf('.') >= 0) {
+			log.error("字段 "+ fieldName + " 加前后缀失败，暂不支持内嵌字段");
 			return record;
-		}`
+		}
+		var newFieldName = "";
+		if(beforeOrAfter === "before") {
+			newFieldName = text + fieldName;
+		} else {
+			newFieldName = fieldName + text;
+		}
+		if(newFieldName in record){
+			log.error("Field "+ newFieldName + " already exists");
+			return record;
+		}
+		record[newFieldName] = record[fieldName];
+		record.remove(fieldName);
+	})
+	return record;
+}`
 	},
 	{
 		"nodeConfig": {
@@ -96,26 +97,27 @@ export default [
 				}
 			]
 		},
-		"scriptTemplate": `function process(record) {
-			//当前模型的字段列表，可选1个或多个
-			var fields = \${fields};
-			// 时区调整值, 单选数值 -12 到 + 12
-			var timeArea = \${timeArea};
-			//核心代码
-			fields.forEach(function(key) {
-				var value = record[key];
-				if (Object.prototype.toString.call(value) == '[object java.util.Date]'){
-					value = new Date(value.getTime() + timeArea * 60 * 60 * 1000);
-				record[key] = value;
-				} else {
-					log.error('Column ' + key +
-						' type is not Date, data flow will stop, please confirm this record: ' + record);
-					// return null ? keep going ? interupt
-				}
+		"scriptTemplate":
+`function process(record) {
+	//当前模型的字段列表，可选1个或多个
+	var fields = \${fields};
+	// 时区调整值, 单选数值 -12 到 + 12
+	var timeArea = \${timeArea};
+	//核心代码
+	fields.forEach(function(key) {
+		var value = record[key];
+		if (Object.prototype.toString.call(value) == '[object java.util.Date]'){
+			value = new Date(value.getTime() + timeArea * 60 * 60 * 1000);
+		record[key] = value;
+		} else {
+			log.error('Column ' + key +
+				' type is not Date, data flow will stop, please confirm this record: ' + record);
+			// return null ? keep going ? interupt
+		}
 
-			})
-			return record;
-		}`
+	})
+	return record;
+}`
 
 	},
 	{
@@ -232,55 +234,56 @@ export default [
 				}
 			]
 		},
-		"scriptTemplate": `function process(record) {
-			//当前模型的字段列表，可选1个或多个
-			var selected_fields = \${fields};
-			var masking_method = \${type};  // length or regex
-			var visible_length = \${charLength};
-			var visible_position = \${charPosition}; // left or right
-			var visible_regex = \${regex}; // string regex
-			var visible_regex_groups = \${groups}; // comma separated regex capturing group number
-			visible_regex_groups = visible_regex_groups.split(',');
-			// validations
+		"scriptTemplate":
+`function process(record) {
+	//当前模型的字段列表，可选1个或多个
+	var selected_fields = \${fields};
+	var masking_method = \${type};  // length or regex
+	var visible_length = \${charLength};
+	var visible_position = \${charPosition}; // head or foot
+	var visible_regex = \${regex}; // string regex
+	var visible_regex_groups = \${groups}; // comma separated regex capturing group number
+	visible_regex_groups = visible_regex_groups.split(',');
+	// validations
 
-			function maskByLength(value){
-			   if(visible_position == 'left'){
-				   // show first N letters, the rest to use ####
-				   return value.substr(0, visible_length) + " ###";
-			   }
-			   else{
-				   // show last N letters, the rest to use ####
-				   return "### "+ value.substr( 0 - visible_length, visible_length)
-			   }
-		   }
-			function maskByRegex(value){
-				 var reg = new RegExp(visible_regex);
-				 var matches = reg.exec(value);
-				 if(!matches){
-					//log.debug("No matching record");
-					return value;
-				 }
-				 var replacement = "";
-				 for(var i =0 ;i< visible_regex_groups.length;i++){
-				   replacement += "$"+visible_regex_groups[i]
-				   if(i+1 < visible_regex_groups)
-					 replacement += " ### ";
-				 }
-				 return value.replace(reg, replacement);
+	function maskByLength(value){
+		if(visible_position == 'head'){
+			// show first N letters, the rest to use ####
+			return value.substr(0, visible_length) + " ###";
+		}
+		else{
+			// show last N letters, the rest to use ####
+			return "### "+ value.substr( 0 - visible_length, visible_length)
+		}
+	}
+	function maskByRegex(value){
+			var reg = new RegExp(visible_regex);
+			var matches = reg.exec(value);
+			if(!matches){
+			//log.debug("No matching record");
+			return value;
 			}
+			var replacement = "";
+			for(var i =0 ;i< visible_regex_groups.length;i++){
+			replacement += "$"+visible_regex_groups[i]
+			if(i+1 < visible_regex_groups)
+				replacement += " ### ";
+			}
+			return value.replace(reg, replacement);
+	}
 
-			//核心代码
-			selected_fields.forEach(function(fieldName) {
-				var value = record[fieldName];
-				if(!value) return;
-				if(masking_method == "length")
-				   value = maskByLength(value);
-				else
-				   value = maskByRegex(value);
-				record[fieldName] = value;
-			})
-			return record;
-		}`
+	//核心代码
+	selected_fields.forEach(function(fieldName) {
+		var value = record[fieldName];
+		if(!value) return;
+		if(masking_method == "length")
+			value = maskByLength(value);
+		else
+			value = maskByRegex(value);
+		record[fieldName] = value;
+	})
+	return record;
+}`
 	},
 	{
 		"nodeConfig": {
@@ -311,29 +314,30 @@ export default [
 				}
 			]
 		},
-		"scriptTemplate": `function process(record) {
-			var selected_field  = \${field};
-			//分隔符
-			var delimiter = \${separator};
-			//字符串输入
-			var targetFieldName = \${text};
+		"scriptTemplate":
+`function process(record) {
+	var selected_field  = \${field};
+	//分隔符
+	var delimiter = \${separator};
+	//字符串输入
+	var targetFieldName = \${text};
 
-			// validations
-			if(! targetFieldName || !selected_field) {
-				// report error
-				log.warn("Missing processing node configuration");
-				return record;
-			}
+	// validations
+	if(! targetFieldName || !selected_field) {
+		// report error
+		log.warn("Missing processing node configuration");
+		return record;
+	}
 
-			//核心代码
+	//核心代码
 
-			var value = record[selected_field];
-			if(!value) return record;
-			var values = value.split(delimiter);
-			record[targetFieldName] = values;
+	var value = record[selected_field];
+	if(!value) return record;
+	var values = value.split(delimiter);
+	record[targetFieldName] = values;
 
-			return record;
-		}`
+	return record;
+}`
 	},
 	{
 		"nodeConfig": {
@@ -367,30 +371,32 @@ export default [
 				}
 			]
 		},
-		"scriptTemplate": `function process(record) {
-			var selected_fields = \${fields};
-			var mappings= \${mappings};
-		   // validations
-			if(  ! selected_fields || !mappings) {
-				// report error
-				log.warn("Missing processing node configuration");
-				return record;
-			}
-
-			//核心代码
-			selected_fields.forEach(function(selected_field){
-				var value = record[selected_field];
-				if(!value) return record;
-				for(var i =0; i <mappings.length; i++){
-					var mapping = mappings[i];
-					if(mapping[0] == value){
-						record[selected_field] = mapping[1];
-						break;
-					}
-				}
-			});
-
-		   return record;
-		}`
+		"scriptTemplate":
+`function process(record) {
+	var selected_fields = \${fields};
+	var mappings= \${mappings};
+	// validations
+	if(  ! selected_fields || !mappings) {
+		// report error
+		log.warn("Missing processing node configuration");
+		return record;
 	}
-]
+
+	//核心代码
+	selected_fields.forEach(function(selected_field){
+		var value = record[selected_field];
+		if(!value) return record;
+		for(var i =0; i <mappings.length; i++){
+			var mapping = mappings[i];
+			if(mapping[0] == value){
+				record[selected_field] = mapping[1];
+				break;
+			}
+		}
+	});
+
+	return record;
+}`
+	}
+];
+export default nodeConfig;
