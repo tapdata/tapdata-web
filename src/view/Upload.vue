369b078eba1d05c39326d3aed66b16b2f9d85f31
唐晓敏 <bassie@tapdata.io>
2021-04-09 17:42:19 +0800

    修改校验上传type

--- src/view/Upload.vue
+++ src/view/Upload.vue
@@ -64,6 +64,8 @@ export default {
 		this.type = this.$route.query && this.$route.query.type ? this.$route.query.type : '';
 		if (this.type === 'api') {
 			this.downType = 'APIServer';
+		} else if (this.type === 'Inspect') {
+			this.downType = 'Inspect';
 		} else {
 			this.downType = 'dataflow';
 		}
