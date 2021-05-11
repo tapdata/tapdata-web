90a3b682038af62283cbab29000e6adee7d395d5
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-05-11 15:22:14 +0800

    添加local环境，支持mock

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 const { npm_config_argv } = process.env
 const URL = {
+  local: 'http://localhost:30300',
   dev: 'http://192.168.1.181:30300',
   pro: 'http://backend:3030'
 }
 let ENV
 if (npm_config_argv) {
   const argv = JSON.parse(process.env.npm_config_argv).original
-  ENV = Object.keys(URL).find((k) => argv.includes('--' + k))
+  ENV = Object.keys(URL).find(k => argv.includes('--' + k))
 }
 const proxy = {
   target: URL[ENV || 'dev'],
@@ -15,7 +16,7 @@ const proxy = {
 const { resolve } = require('path')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
-module.exports = {
+const config = {
   assetsDir: 'static',
   lintOnSave: true,
   productionSourceMap: false,
@@ -26,13 +27,6 @@ module.exports = {
       '/oauth/': proxy,
       '/old/': {
         target: 'http://192.168.1.101:8081/'
-      },
-      '/ws/': {
-        ...proxy,
-        ws: true,
-        secure: false,
-        logLevel: 'debug',
-        target: proxy.target.replace(/^https?/, 'ws')
       }
     }
   },
@@ -99,3 +93,17 @@ module.exports = {
     ]
   }
 }
+
+if (ENV !== 'local') {
+  config.devServer.proxy['/ws/'] = {
+    ...proxy,
+    ws: true,
+    secure: false,
+    logLevel: 'debug',
+    target: proxy.target.replace(/^https?/, 'ws')
+  }
+} else {
+  config.publicPath = './'
+}
+
+module.exports = config
