c77788dc219414bbb2ca3930365dcd4739fdc7bd
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-05-20 14:35:42 +0800

    dfs，连接管理-创建连接，去掉连接类型的配置，默认参数

--- src/views/connections/config/dfs_sqlserver.js
+++ src/views/connections/config/dfs_sqlserver.js
@@ -8,29 +8,29 @@ export default function (vm) {
       connection_type: 'source_and_target'
     },
     items: [
-      {
-        type: 'radio',
-        field: 'connection_type',
-        label: vm.$t('dataForm.form.connectionType'),
-        options: [
-          {
-            label: vm.$t('dataForm.form.options.sourceAndTarget'),
-            tip: vm.$t('dataForm.form.options.sourceAndTargetTips'),
-            value: 'source_and_target'
-          },
-          {
-            label: vm.$t('dataForm.form.options.source'),
-            tip: vm.$t('dataForm.form.options.sourceTips'),
-            value: 'source'
-          },
-          {
-            label: vm.$t('dataForm.form.options.target'),
-            tip: vm.$t('dataForm.form.options.targetTips'),
-            value: 'target'
-          }
-        ],
-        required: true
-      },
+      // {
+      //   type: 'radio',
+      //   field: 'connection_type',
+      //   label: vm.$t('dataForm.form.connectionType'),
+      //   options: [
+      //     {
+      //       label: vm.$t('dataForm.form.options.sourceAndTarget'),
+      //       tip: vm.$t('dataForm.form.options.sourceAndTargetTips'),
+      //       value: 'source_and_target'
+      //     },
+      //     {
+      //       label: vm.$t('dataForm.form.options.source'),
+      //       tip: vm.$t('dataForm.form.options.sourceTips'),
+      //       value: 'source'
+      //     },
+      //     {
+      //       label: vm.$t('dataForm.form.options.target'),
+      //       tip: vm.$t('dataForm.form.options.targetTips'),
+      //       value: 'target'
+      //     }
+      //   ],
+      //   required: true
+      // },
       {
         type: 'input',
         field: 'database_host',
