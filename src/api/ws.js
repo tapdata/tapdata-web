f9548e978c24f2e078157b7ef11101c9c3c0864e
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-05-29 05:21:46 +0800

    ws路径

--- src/api/ws.js
+++ src/api/ws.js
@@ -242,7 +242,7 @@ class WSClient extends EventEmitter {
     let host = window.getSettingByKey('DFS_TM_WS_HOST') || loc.host
     let apiPre = window.getSettingByKey('DFS_TM_API_PRE_URL') || ''
     let tcmApiPre = window.getSettingByKey('DFS_TCM_API_PRE_URL') || ''
-    let path = tcmApiPre + apiPre
+    let path = (tcmApiPre === '/console' ? '' : tcmApiPre) + apiPre
     new_uri += '//' + host
     new_uri += path + '/ws/agent'
     return new_uri
