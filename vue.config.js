b90daf752d183934a7b481e0b86e4a8799bce6d6
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-05-29 00:38:36 +0800

    相对路径

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
   assetsDir: 'static',
   lintOnSave: true,
   productionSourceMap: false,
-  publicPath: './',
+  publicPath: '',
   devServer: {
     proxy: {
       '/api/': proxy,
