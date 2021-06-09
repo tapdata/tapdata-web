0cf3d4dcb23988f0fe27dabb774b2e3c72e1f675
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-06-09 12:26:00 +0800

    工作台

--- src/views/Workbench/Workbench.vue
+++ src/views/Workbench/Workbench.vue
@@ -1,88 +1,48 @@
 <template>
   <div
     v-if="$route.name === 'Workbench'"
-    class="workbench-container flex flex-grow-1"
+    class="workbench-container flex flex-column"
   >
-    <div class="main flex-grow-1">
-      <div class="main-header py-6 px-7">
-        <div class="main-title flex align-center">
-          <img src="../../assets/image/logoIcon.png" alt="" />
-          <span class="fs-3 inline-block">欢迎使用Tapdata Cloud</span>
+    <!--  快速开始  -->
+    <div class="workbench-start workbench-section flex">
+      <div class="main flex-grow-1">
+        <div class="main-header flex py-6">
+          <div class="main-title mr-4 fs-6 fw-bolder">快速开始</div>
+          <div class="main-subtitle py-1 px-4 fs-8 pointer">
+            <VIcon class="icon mr-1" size="9">arrow_right</VIcon>
+            <span>新手引导</span>
+          </div>
         </div>
-        <div class="main-subtitle fs-7">
-          <span class="mr-4"
-            >安全、简单、永久免费的实时数据流融合服务
-            从这里开始您的数据旅途</span
-          >
-          <img src="../../assets/image/workbench/arrow_right.png" alt="" />
-          <span
-            class="ml-2 text-decoration-underline pointer"
-            @click="showGuide"
-            >开启引导</span
-          >
-        </div>
-      </div>
-      <div class="main-content">
-        <div class="create-list py-6 px-10">
-          <ul class="create-list__list flex justify-between">
-            <li
-              v-for="(item, index) in createList"
-              :key="index"
-              class="create-list__item"
-            >
-              <div class="create-list__name flex mb-4 fs-7">
-                <VIcon class="v-icon" size="20">{{ item.icon }}</VIcon>
-                <span class="ml-2 fw-bolder">{{ item.name }}</span>
-              </div>
-              <div class="create-list__title mb-4 fs-8 text-wrap">
-                {{ item.title }}
-              </div>
-              <el-button
-                class="create-list__btn"
-                type="primary"
-                size="small"
-                :icon="item.hiddenBtnIcon ? '' : 'el-icon-plus'"
-                @click="item.action"
-                >{{ item.btnName }}</el-button
-              >
-            </li>
-          </ul>
-        </div>
-        <ul class="introduction-list justify-between flex flex-wrap px-6 py-4">
+        <ul class="create-list__list flex justify-between">
           <li
-            v-for="(item, index) in introductionList"
+            v-for="(item, index) in createList"
             :key="index"
-            class="introduction-list__item p-4"
+            class="create-list__item flex p-6 ml-10"
           >
             <div
-              class="introduction-list__box flex p-4 pointer"
-              @click="clickIntro(item)"
+              class="create-list__index block flex justify-center align-center flex-shrink-0"
             >
-              <div class="introduction-list__icon">
-                <img :src="getIconSrc(item.icon)" alt="" />
-              </div>
-              <div class="introduction-list__content ml-4">
-                <div class="introduction-list__name mb-3 fs-7">
-                  {{ item.name }}
-                </div>
-                <div
-                  class="introduction-list__title"
-                  v-for="(t, i) in item.title"
-                  :key="i"
-                >
-                  {{ t }}
-                </div>
+              {{ index + 1 }}
+            </div>
+            <div class="create-list__main ml-4">
+              <div class="create-list__name mb-4 fs-7">{{ item.name }}</div>
+              <div class="create-list__desc">{{ item.desc }}</div>
+              <div
+                class="create-list__btn text-end pointer"
+                @click="item.action"
+              >
+                <span>{{ item.btnName }}</span>
               </div>
             </div>
           </li>
         </ul>
       </div>
-    </div>
-    <div class="aside flex flex-column ml-6">
-      <div class="notice mb-6">
-        <div class="notice-header fs-7 fw-bolder p-6">公告通知</div>
-        <div class="notice-list px-5">
-          <ul class="notice-list__list pt-3">
+      <div class="aside flex flex-column ml-6 flex-shrink-0">
+        <div class="aside-header flex py-6 fs-6 fw-bolder">
+          <div class="aside-title">公告通知</div>
+        </div>
+        <div class="aside-main flex-grow-1 p-6">
+          <ul class="notice-list__list">
             <li
               v-for="(item, index) in notices.slice(0, 5)"
               :key="index"
@@ -101,34 +61,118 @@
             </li>
           </ul>
         </div>
-        <div class="notice-footer p-6">
-          <span
-            v-if="notices.length > 5"
-            class="color-primary pointer"
-            @click="toNotice"
-            >更多</span
-          >
-        </div>
       </div>
-
-      <div class="guide flex-grow-1">
-        <div class="guide-header fs-7 fw-bolder p-6">新手入门</div>
-        <div class="guide-list px-5">
+    </div>
+    <!--  概览  -->
+    <div class="workbench-overview workbench-section flex">
+      <div class="main flex-grow-1">
+        <div class="main-header py-6">
+          <div class="main-title mr-4 fs-6 fw-bolder">概览</div>
+        </div>
+        <ul class="agent-list__list flex justify-between">
+          <li
+            v-for="(item, index) in agentList"
+            :key="index"
+            class="agent-list__item p-6 ml-10"
+          >
+            <div
+              class="agent-list__name flex align-center justify-center mx-auto mb-3"
+            >
+              <VIcon size="12" class="icon">{{ item.icon }}</VIcon>
+              <span class="ml-1 fs-7">{{ item.name }}</span>
+            </div>
+            <div class="agent-list__value text-center fs-1">
+              {{ item.value }}
+            </div>
+            <div class="agent-list__detail flex justify-around mt-3 py-2">
+              <div
+                v-for="(detail, dIndex) in item.list"
+                :key="dIndex"
+                class="agent-list__status"
+              >
+                <span>{{ detail.label }}</span>
+                <span>:</span>
+                <span
+                  :class="[
+                    'ml-1',
+                    { status: ['正常'].indexOf(detail.value) > -1 }
+                  ]"
+                  >{{ detail.value }}</span
+                >
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="aside flex flex-column ml-6 flex-shrink-0">
+        <div class="aside-header flex py-6 fs-6 fw-bolder">
+          <div class="aside-title">新手入门</div>
+        </div>
+        <div class="aside-main flex-grow-1 p-6">
           <ul class="guide-list__list pt-3">
             <li
               v-for="(item, index) in guides"
               :key="index"
-              class="guide-list__item flex mb-4 py-2 px-4 pointer"
+              class="guide-list__item flex mb-4 pointer"
               @click="clickGuide(item)"
             >
-              <div class="guide-list__icon">
-                <img :src="getIconSrc(item.icon)" alt="" />
-              </div>
-              <div class="guide-list__name ml-4 flex align-center">
+              <div class="guide-list__name">
                 {{ item.name }}
               </div>
             </li>
           </ul>
+        </div>
+        <div class="notice mb-6 none">
+          <div class="notice-header fs-7 fw-bolder p-6">公告通知</div>
+          <div class="notice-list px-5">
+            <ul class="notice-list__list pt-3">
+              <li
+                v-for="(item, index) in notices.slice(0, 5)"
+                :key="index"
+                class="notice-list__item flex align-center py-3 px-1 pointer"
+              >
+                <div v-if="item.type" class="notice-list__type mr-4 p-1">
+                  {{ item.type }}
+                </div>
+                <div
+                  class="notice-list__name flex-grow-1 ellipsis pointer"
+                  @click="toNotice"
+                >
+                  {{ item.name }}
+                </div>
+                <div class="notice-list__time">{{ item.time }}</div>
+              </li>
+            </ul>
+          </div>
+          <div class="notice-footer p-6">
+            <span
+              v-if="notices.length > 5"
+              class="color-primary pointer"
+              @click="toNotice"
+              >更多</span
+            >
+          </div>
+        </div>
+
+        <div class="guide flex-grow-1 none">
+          <div class="guide-header fs-7 fw-bolder p-6">新手入门</div>
+          <div class="guide-list px-5">
+            <ul class="guide-list__list pt-3">
+              <li
+                v-for="(item, index) in guides"
+                :key="index"
+                class="guide-list__item flex mb-4 pointer"
+                @click="clickGuide(item)"
+              >
+                <div class="guide-list__icon">
+                  <img :src="getIconSrc(item.icon)" alt="" />
+                </div>
+                <div class="guide-list__name ml-4 flex align-center">
+                  {{ item.name }}
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -150,6 +194,8 @@ export default {
           // icon: 'account',
           icon: 'agent',
           title: '部署Agent，并对已有Agent的状态进行查看和管理',
+          desc:
+            '欢迎来到Tapdata Cloud，即将开启您的实时数据同步之旅！初次使用请先安装部署Agent，不然无法创建连接和任务哦。',
           btnName: '部署Agent',
           hiddenBtnIcon: true,
           action: this.createAgent
@@ -158,6 +204,8 @@ export default {
           name: '连接管理',
           icon: 'connection',
           title: '创建数据连接，并对已有数据连接的状态进行测试和管理',
+          desc:
+            '数据连接主要用来建立Agent与用户源数据库和目标数据库的连接。快来创建数据连接，并对已有数据连接的状态进行测试和管理吧。',
           btnName: '创建连接',
           action: this.createConnection
         },
@@ -166,10 +214,59 @@ export default {
           icon: 'task',
           title:
             '创建数据同步任务，并对已有数据同步任务的类型和状态进行查看和管理',
+          desc:
+            '任务管理主要用来创建同步任务并进行管理。快来创建数据同步任务，并对已有数据同步任务的类型和状态进行查看和管理吧。',
           btnName: '创建任务',
           action: this.createTask
         }
       ], // 创建列表
+      agentList: [
+        {
+          name: 'Agent',
+          icon: 'agent',
+          value: 1,
+          list: [
+            {
+              label: 'Agent状态',
+              value: '正常'
+            }
+          ]
+        },
+        {
+          name: '连接',
+          icon: 'connection',
+          value: 1,
+          list: [
+            {
+              label: '有效连接',
+              value: 10
+            },
+            {
+              label: '无效连接',
+              value: 10
+            }
+          ]
+        },
+        {
+          name: '任务',
+          icon: 'task',
+          value: 1,
+          list: [
+            {
+              label: '全量',
+              value: 10
+            },
+            {
+              label: '增量',
+              value: 10
+            },
+            {
+              label: '全量增量',
+              value: 10
+            }
+          ]
+        }
+      ],
       introductionList: [
         {
           name: '什么是Tapdata Cloud',
@@ -234,9 +331,58 @@ export default {
   },
   methods: {
     init() {
-      this.loadData()
+      this.loadAgent()
+      this.loadConection()
+      this.loadTask()
+      this.loadNotices() // 通知公告
     },
-    loadData() {
+    loadAgent() {
+      // agent列表
+      let agentList = this.agentList
+      this.$axios
+        .get('tm/api/clusterStates')
+        .then(data => {
+          // this.agentList = data || []
+
+          // agent状态
+          let obj = {
+            total: data.length,
+            running: 0,
+            stopped: 0,
+            down: 0
+          }
+          data.forEach(v => {
+            if (v.engine && v.engine.status) {
+              obj[v.engine.status] = obj[v.engine.status] + 1
+            }
+          })
+          agentList[0].value = obj.total
+        })
+        .finally(() => {
+          this.transferLoading = false
+        })
+    },
+    loadConection() {
+      this.$axios.get('tm/api/Connections/count').then(res => {
+        console.log('res', res)
+      })
+    },
+    loadTask() {
+      let agentList = this.agentList
+      this.$axios.get('tm/api/DataFlows/chart').then(data => {
+        // 运行任务状态
+        const chart4 = data.chart4 || {}
+        chart4.total =
+          (chart4.initializing || 0) +
+          (chart4.initialized || 0) +
+          (chart4.cdc || 0)
+        agentList[2].value = chart4.total
+        agentList[2].list[0].value = chart4.initializing || 1
+        agentList[2].list[1].value = chart4.cdc || 2
+        agentList[2].list[1].value = chart4.initialized || 3
+      })
+    },
+    loadNotices() {
       this.notices = [
         {
           id: 0,
@@ -284,98 +430,88 @@ export default {
   height: 100%;
   min-height: 610px;
   min-width: 1060px;
+  .workbench-start {
+    height: 270px;
+  }
   .main {
-    width: 0;
-    background: #fff;
-    border-bottom-right-radius: 6px;
-    border-bottom-left-radius: 6px;
-    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.02);
     .main-header {
-      color: #fff;
-      background: url(../../assets/image/workbench/workbench_bg.png) no-repeat
-        100% / cover;
-      border-top-left-radius: 4px;
-      border-top-right-radius: 6px;
-    }
-    .main-title {
-      height: 40px;
-      line-height: 40px;
-      > img {
-        width: 45px;
-        height: 33px;
+      .main-title {
+        height: 24px;
+        line-height: 24px;
       }
-    }
-    .main-subtitle {
-      > img {
-        width: 12px;
-        height: 12px;
-      }
-    }
-    .main-content {
-      border-bottom-right-radius: 6px;
-      border-bottom-left-radius: 6px;
-    }
-    .create-list {
-      .create-list__item {
-      }
-      .create-list__name {
-        color: #000;
-        > img {
-          width: 22px;
-          height: 22px;
-        }
-        ::v-deep .v-icon {
-          color: #888;
+      .main-subtitle {
+        background-color: #dfe8ee;
+        .icon {
+          color: map-get($color, primary);
         }
       }
-      .create-list__title {
-        width: 195px;
-        color: rgba(0, 0, 0, 49.87%);
-      }
-      .create-list__btn {
-        border-radius: 4px;
+    }
+    .create-list__item {
+      background-color: #fff;
+      &:first-child {
+        margin-left: 0 !important;
       }
     }
-    .introduction-list {
-      .introduction-list__item {
-        width: 50%;
-        box-sizing: border-box;
+    // 快速开始
+    .create-list__index {
+      width: 22px;
+      height: 22px;
+      color: map-get($color, primary);
+      border: 1px solid map-get($color, primary);
+      border-radius: 50%;
+    }
+    .create-list__name {
+      color: #000;
+    }
+    .create-list__desc {
+      height: 95px;
+      color: rgba(0, 0, 0, 0.49);
+    }
+    .create-list__btn {
+      color: map-get($color, primary);
+    }
+    // 概览
+    .agent-list__list {
+      background-color: #fff;
+    }
+    .agent-list__item {
+      width: 240px;
+    }
+    .agent-list__name {
+      .vicon {
+        color: #888;
       }
-      .introduction-list__box {
-        height: 150px;
-        box-sizing: border-box;
-        background: rgba(0, 0, 0, 0.02);
-        &:hover {
-          background: rgba(234, 239, 254, 0.64);
-          .introduction-list__name {
-            color: #2c65ff;
-          }
-          .introduction-list__title {
-            color: rgba(44, 101, 255, 49.87%);
-          }
-        }
-      }
-      .introduction-list__icon {
-        > img {
-          width: 56px;
-          height: 56px;
-        }
-      }
-      .introduction-list__content {
-        .introduction-list__title {
-          //min-width: 240px;
-          color: rgba(0, 0, 0, 49.87%);
-        }
+    }
+    .agent-list__value {
+      color: map-get($color, primary);
+    }
+    .agent-list__detail {
+      background-color: #fafafb;
+      color: rgba(0, 0, 0, 0.5);
+      .status {
+        color: #599f3f;
       }
     }
   }
   .aside {
     width: 392px;
-    > div {
-      box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.02);
-      border-radius: 4px;
-      background: #fff;
+    .aside-header {
+      .aside-title {
+        height: 24px;
+        line-height: 24px;
+      }
     }
+    .aside-main {
+      background-color: #fff;
+    }
+    .guide-list__name {
+      color: map-get($color, primary);
+    }
+    //> div {
+    //  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.02);
+    //  border-radius: 4px;
+    //  background: #fff;
+    //}
   }
   .notice {
     .notice-list__list {
@@ -396,27 +532,6 @@ export default {
     }
     .notice-footer {
       color: map-get($color, primary);
-    }
-  }
-  .guide {
-    .guide-list__list {
-      border-top: 1px solid #f2f2f2;
-      .guide-list__item {
-        border-radius: 2px;
-        background-color: rgba(0, 0, 0, 2%);
-        &:hover {
-          background: rgba(234, 239, 254, 0.64);
-          .guide-list__name {
-            color: #2c65ff;
-          }
-        }
-      }
-      .guide-list__icon {
-        > img {
-          width: 42px;
-          height: 42px;
-        }
-      }
     }
   }
   .pointer {
