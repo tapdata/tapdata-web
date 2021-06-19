3b83ee361488cd517365173c3162675baeb4a8db
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-06-19 14:46:47 +0800

    fix[任务列表]：修改创建任务的图标

--- src/views/Task/Migration.vue
+++ src/views/Task/Migration.vue
@@ -34,7 +34,7 @@
 				</div>
 				<div class="migration-operation-right">
 					<ElButton type="primary" @click="createTask">
-						<i class="iconfont td-icon-dinggou" style="margin-right: 5px;"></i>
+						<i class="el-icon-plus" style="margin-right: 5px;"></i>
 						<span>创建任务</span>
 					</ElButton>
 				</div>
