03f5f04d0cb872c6c0a3d9941d2eb0a35e8693b9
Payne <payne@tapdata.io>
2021-06-28 19:30:26 +0800

    fix: 去掉无用的elint文件

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  endOfLine: 'lf', // 统一换行符为\n
-  singleQuote: true, // 单引号
-  trailingComma: 'none', // 取消尾逗号
-  arrowParens: 'avoid', // 箭头函数参数括号 默认avoid 可选 avoid| always
-  semi: false, // 取消尾分号
-  tabWidth: 2 // 两个空格缩进
-}
