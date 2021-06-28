9156c2b73209512c65a0195a7954ef2d233fd1c8
Payne <payne@tapdata.io>
2021-06-28 19:31:22 +0800

    fix: 更新配置文件

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,16 @@
 module.exports = {
-  endOfLine: 'lf', // 统一换行符为\n
-  singleQuote: true, // 单引号
-  trailingComma: 'none', // 取消尾逗号
-  arrowParens: 'avoid', // 箭头函数参数括号 默认avoid 可选 avoid| always
-  semi: false, // 取消尾分号
-  tabWidth: 2 // 两个空格缩进
+  // 使用两个空格缩进
+  tabWidth: 2,
+  // 单引号
+  singleQuote: true,
+  // 取消尾分号
+  semi: false,
+  // 取消尾逗号
+  trailingComma: 'none',
+  // 统一换行符为\n
+  endOfLine: 'lf',
+  // 为了便于阅读，每一行建议不要使用超过80个字符
+  printWidth: 120,
+  // 箭头函数参数只有一个时省略括号
+  arrowParens: 'avoid'
 }
