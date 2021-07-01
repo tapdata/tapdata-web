6bb65424834bc0478ed18524a8761acb274713f2
Payne <payne@tapdata.io>
2021-07-01 19:06:03 +0800

    test

--- index.js
+++ index.js
@@ -31,9 +31,11 @@ const install = function(Vue, opts = {}) {
     }
   })
 }
+
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
+
 export default {
   install,
   locale: use,
