dc158dc1f58b225baa1f85518af22b9ae71de6f8
Payne <payne@tapdata.io>
2021-07-01 18:08:08 +0800

    test

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-/* eslint-disable */
-import { t, use } from './locale'
-import FormBuilder from './components/form-builder'
-import ConnectionTypeSelector from './components/connection-type-selector'
-import ConnectionTest from './components/ConnectionTest'
+import { t, use } from "./locale";
+import FormBuilder from "./components/form-builder";
+import ConnectionTypeSelector from "./components/connection-type-selector";
+import ConnectionTest from "./components/ConnectionTest";
 
-import WSClient from './plugins/ws-client'
+import WSClient from "./plugins/ws-client";
 
-const components = [ConnectionTypeSelector, ConnectionTest]
+const components = [ConnectionTypeSelector, ConnectionTest];
 
 const install = function (Vue, opts = {}) {
-  use(opts.lang)
-  Vue.use(FormBuilder)
+  use(opts.lang);
+  Vue.use(FormBuilder);
 
-  components.forEach(component => {
-    Vue.component(component.name, component)
-  })
+  components.forEach((component) => {
+    Vue.component(component.name, component);
+  });
 
   Vue.mixin({
     created() {
       // 创建实例时传入wsOptions，即可默认开启websocket
-      let wsOptions = this.$options.wsOptions
+      let wsOptions = this.$options.wsOptions;
       // 根实例才有ws
       if (wsOptions) {
-        Vue.prototype.$ws = new WSClient(wsOptions.url, wsOptions.protocols, wsOptions)
+        Vue.prototype.$ws = new WSClient(
+          wsOptions.url,
+          wsOptions.protocols,
+          wsOptions
+        );
       }
     },
     methods: {
       t(...args) {
-        return t.apply(this, args)
-      }
-    }
-  })
-}
-
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+        return t.apply(this, args);
+      },
+    },
+  });
+};
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
 }
 
 export default {
@@ -42,5 +44,5 @@ export default {
   locale: use,
   ConnectionTypeSelector,
   ConnectionTest,
-  WSClient
-}
+  WSClient,
+};
