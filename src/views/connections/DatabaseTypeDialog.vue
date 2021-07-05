ec37ffe8d3494dbf2cec0a57e596a8ea578524f6
fannie <fannie@tapdata.io>
2021-07-05 16:48:17 +0800

    feat: 新增Hbase节点配置

--- src/views/connections/DatabaseTypeDialog.vue
+++ src/views/connections/DatabaseTypeDialog.vue
@@ -123,6 +123,7 @@ export default {
       allowDataType.filter(type => this.database.includes(type)) || []
     this.otherType =
       allowDataType.filter(type => this.otherType.includes(type)) || []
+    this.database.push('hbase') //临时代码
   },
   methods: {
     getImgByType,
