7f11edf44164561a4e195906a2ed6472958de9ef
Payne <payne@tapdata.io>
2021-07-08 14:44:20 +0800

    lint

--- components/form-builder/mixin.js
+++ components/form-builder/mixin.js
@@ -5,15 +5,11 @@ export default {
       on: {
         input: (...args) => {
           self.$emit('input', ...args)
-          self.config.on &&
-            self.config.on.input &&
-            self.config.on.input(...args)
+          self.config.on && self.config.on.input && self.config.on.input(...args)
         },
         change: (...args) => {
           self.$emit('change', ...args)
-          self.config.on &&
-            self.config.on.change &&
-            self.config.on.change(...args)
+          self.config.on && self.config.on.change && self.config.on.change(...args)
         }
       }
     }
