bfaa16828d47c54a9c188c15a5f9fac08c84588f
唐晓敏 <bassie@tapdata.io>
2021-07-12 20:04:26 +0800

    Agent 删除、停止、创建多个Agent

--- src/views/Instance/Details.vue
+++ src/views/Instance/Details.vue
@@ -11,65 +11,47 @@
         </div>
       </div>
       <div class="panel mt-5">
-        <div class="title">
+        <!-- <div class="title">
           <i class="el-icon-notebook-2"></i>
           <span style="margin-left: 1px">实例信息</span>
-        </div>
+        </div> -->
         <ul class="info">
           <li class="info-item">
-            <div class="label">实例ID</div>
+            <div class="label">Agent ID：</div>
             <div class="value">{{ agent.id }}</div>
           </li>
           <li class="info-item">
-            <div class="label">地域及可用区</div>
+            <div class="label">Agent 版本：</div>
             <div class="value">
-              {{ agent.regionFmt }}
+              {{ agent.spec.version }}
             </div>
           </li>
           <li class="info-item">
-            <div class="label">同步拓扑</div>
-            <div class="value">{{ agent.topology }}</div>
+            <div class="label">Agent 创建时间：</div>
+            <div class="value">{{ agent.createAt }}</div>
           </li>
           <li class="info-item">
-            <div class="label">实例规格</div>
+            <div class="label">宿主机IP：</div>
             <div class="value">
               {{ comSpecType }}
             </div>
           </li>
           <li class="info-item">
-            <div class="label">版本</div>
+            <div class="label">宿主机CPU数量：</div>
             <div class="value">
               {{ agent.spec ? agent.spec.version : '' }}
             </div>
           </li>
-        </ul>
-      </div>
-      <div class="panel mt-20">
-        <div class="title">
-          <i class="el-icon-money"></i>
-          <span style="margin-left: 1px">计费信息</span>
-        </div>
-        <ul class="info">
           <li class="info-item">
-            <div class="label">计费模式</div>
+            <div class="label">宿主机内存大小：</div>
             <div class="value">
-              {{ chargeMap[agent.orderInfo.chargingMode + ',' + agent.orderInfo.periodType] }}
+              {{ agent.spec ? agent.spec.version : '' }}
             </div>
           </li>
-          <li class="info-item" v-if="isMonth">
-            <div class="label">订购时长</div>
-            <div class="value">{{ agent.orderInfo.duration }}个月</div>
-          </li>
           <li class="info-item">
-            <div class="label">创建时间</div>
+            <div class="label">安装目录：</div>
             <div class="value">
-              {{ $moment(agent.createAt).format('YYYY-MM-DD HH:mm:ss') }}
-            </div>
-          </li>
-          <li class="info-item" v-if="isInternet && isMonth">
-            <div class="label">到期时间</div>
-            <div class="value">
-              {{ agent.endTimeStr }}
+              {{ agent.spec ? agent.spec.version : '' }}
             </div>
           </li>
         </ul>
@@ -81,7 +63,7 @@
 
 <script>
 import { SPEC_MAP, CHARGE_MAP } from '../../const'
-import { formatAgent } from '../../util'
+// import { formatAgent } from '../../util'
 import InlineInput from '../../components/InlineInput'
 import StatusTag from '../../components/StatusTag'
 import ChangeInstance from '../../components/ChangeInstance'
@@ -116,9 +98,10 @@ export default {
     fetch() {
       this.loading = true
       this.$axios
-        .get('api/tcm/agent/' + this.$route.params.id)
+        .get('api/tcm/agent/' + this.$route.query.id)
         .then(data => {
-          this.agent = formatAgent(data)
+          // this.agent = formatAgent(data)
+          this.agent = data
         })
         .finally(() => {
           this.loading = false
@@ -161,6 +144,7 @@ export default {
 <style lang="scss" scoped>
 .agent-details-wrap {
   height: 100%;
+  padding: 10px 20px;
   .panel {
     position: relative;
     background: #fff;
@@ -177,11 +161,11 @@ export default {
       display: flex;
       flex-wrap: wrap;
       .info-item {
-        margin-top: 20px;
+        margin: 30px 0;
         display: flex;
         width: 30%;
         .label {
-          width: 75px;
+          width: 100px;
           text-align: right;
           color: map-get($fontColor, slight);
         }
@@ -192,7 +176,7 @@ export default {
     }
   }
   .header {
-    border-left: 3px solid map-get($color, primary);
+    // border-left: 3px solid map-get($color, primary);
     .lignt {
       color: map-get($fontColor, slight);
     }
--- src/views/Instance/Instance.vue
+++ src/views/Instance/Instance.vue
@@ -27,6 +27,12 @@
             </li>
           </ul>
         </div>
+        <div class="instance-operation-right">
+          <ElButton type="primary" @click="createAgent">
+            <i class="iconfont td-icon-dinggou" style="margin-right: 5px;"></i>
+            <span>创建 Agent</span>
+          </ElButton>
+        </div>
         <div v-if="VUE_APP_INSTANCE_TEST_BTN === 'true'" class="instance-operation-right">
           <ElButton type="primary" @click="toOldPurchase">
             <i class="iconfont td-icon-dinggou mr-1"></i>
@@ -41,7 +47,7 @@
       <El-table class="instance-table  table-border mt-3" height="100%" :data="list" @sort-change="sortChange">
         <ElTableColumn min-width="200px" label="实例ID/名称">
           <template slot-scope="scope">
-            <ElLink class="agent-link" type="primary">{{ scope.row.id }}</ElLink>
+            <ElLink class="agent-link" type="primary" @click="handleDetails(scope.row)">{{ scope.row.id }}</ElLink>
             <ClipButton :value="scope.row.id"></ClipButton>
             <InlineInput
               style="display: block;"
@@ -99,8 +105,18 @@
           </template>
         </ElTableColumn>
         <ElTableColumn label="操作" width="120" fixed="right">
-          <template>
-            <ElLink type="primary" class="mr-2" @click="toDeploy">部署</ElLink>
+          <template slot-scope="scope">
+            <ElLink type="primary" class="mr-2" :disabled="scope.row.deployDisable" @click="toDeploy">部署</ElLink>
+            <ElLink
+              type="primary"
+              class="mr-2"
+              :disabled="scope.row.status !== 'Running'"
+              @click="handleStop(scope.row)"
+              >停止</ElLink
+            >
+            <ElLink type="danger" class="mr-2" @click="handleDel(scope.row)" :disabled="scope.row.status !== 'Offline'"
+              >删除</ElLink
+            >
           </template>
         </ElTableColumn>
         <div class="instance-table__empty" slot="empty">
@@ -263,6 +279,7 @@ export default {
             this.list = list.map(item => {
               item.status = item.status === 'Running' ? 'Running' : 'Offline'
               item.updataStatus = ''
+              item.deployDisable = item.tmInfo.pingTime || false
               return item
             })
             // 不存在版本号
@@ -323,6 +340,56 @@ export default {
 
       window.open(downloadUrl.href, '_blank')
     },
+    // 停止
+    handleStop(row) {
+      let flag = false
+      if (row.metric?.runningTaskNum) {
+        flag = true
+      }
+      let message = flag
+        ? '当前Agent有任务正在运行，强行停止Agent可能会导致任务出现异常，是否要强行停止！'
+        : 'Agent停止后将无法再继续运行任务，您需要去Agent安装目录下才能再次启动Agent，是否确认停止？'
+      this.$confirm(message, '是否停止', {
+        type: 'warning'
+      }).then(res => {
+        if (res) {
+          this.$axios
+            .post('api/tcm/agent/stop', {
+              id: row.id
+            })
+            .then(() => {
+              this.$message.success('Agent 已停止')
+              this.fetch()
+            })
+            .catch(() => {
+              this.$message.error('Agent 停止失败')
+              this.loading = false
+            })
+        }
+      })
+    },
+    // 删除
+    handleDel(row) {
+      this.$confirm('删除后该Agent将无法再继续使用，是否确认删除？', '是否删除', {
+        type: 'warning'
+      }).then(res => {
+        if (res) {
+          this.$axios
+            .post('api/tcm/agent/delete', {
+              id: row.id
+            })
+            .then(() => {
+              this.$message.success('Agent 删除成功')
+              this.fetch()
+            })
+            .catch(() => {
+              this.$message.error('Agent 删除失败')
+              this.loading = false
+            })
+        }
+      })
+    },
+
     updateName(val, id) {
       this.loading = true
       this.$axios
@@ -372,6 +439,34 @@ export default {
         }
       })
       window.open(routeUrl.href, '_blank')
+    },
+    // agent详情
+    handleDetails(data) {
+      this.$router.push({
+        name: 'InstanceDetails',
+        query: {
+          id: data.id
+        }
+      })
+    },
+    // 创建Agent
+    createAgent() {
+      this.$confirm('是否创建 Agent？', '创建 Agent', {
+        type: 'warning'
+      }).then(res => {
+        if (res) {
+          this.$axios
+            .post('api/tcm/orders', {
+              agentType: 'Local'
+            })
+            .then(() => {
+              this.fetch()
+            })
+            .catch(() => {
+              this.$router.replace('/500')
+            })
+        }
+      })
     }
   }
 }
