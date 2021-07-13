1b7194b9e0a4394da338fb275a135c153b5e94f6
Payne <payne@tapdata.io>
2021-07-13 13:17:58 +0800

    fix: 更新脚本问题

--- init_submodule.js
+++ init_submodule.js
@@ -6,7 +6,7 @@ cp.exec(
   `git submodule update --init --recursive & git config submodule.recurse true & git config status.submodulesummary 1`
 )
 cp.exec(`git config --remove-section alias`, function() {
-  fs.readFile(join(__dirname, '.git/config'), 'utf8', function(err, data) {
+  fs.readFile(join(__dirname, './.git/config'), 'utf8', function(err, data) {
     if (err) throw err
     let lines = data.split('\n')
     let array = []
@@ -37,7 +37,7 @@ cp.exec(`git config --remove-section alias`, function() {
       '\tupdate = submodule update'
     )
     lines.push('\n')
-    fs.writeFile(join(__dirname, '.git/config'), lines.join('\n'), function(err) {
+    fs.writeFile(join(__dirname, './.git/config'), lines.join('\n'), function(err) {
       if (err) throw err
       console.log('写入git别名成功: sdiff, sstatus, sadd, scommit, spull, spush')
     })
