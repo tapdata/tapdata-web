f89635943438d8975a66c1b92f74c588e8089aa5
fannie <fannie@tapdata.io>
2021-07-14 16:20:11 +0800

    fix(kudu): kudu 国际化

--- src/views/job/Monitor.vue
+++ src/views/job/Monitor.vue
@@ -30,7 +30,7 @@
         <EchartHeader :data="stageType ? nodeDetailsObj : taskDetailsObj"></EchartHeader>
         <div
           class="info fl"
-          v-if="['table', 'collection', 'json', 'excel', 'csv', 'xml', 'database', 'hive', 'mq','hbase'].includes(stageType)"
+          v-if="['table', 'collection', 'json', 'excel', 'csv', 'xml', 'database', 'hive', 'mq','hbase','kudu'].includes(stageType)"
           v-loading="apiLoading"
         >
           <div class="info-list">
@@ -910,7 +910,8 @@ export default {
               'mysql pxc',
               'hive',
               'mq',
-              'hbase'
+              'hbase',
+              'kudu'
             ].includes(this.stageType)
           ) {
             this.getStageDataApi(currentStageData.connectionId, this.tableName)
