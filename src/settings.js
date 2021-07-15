67cdc8a16f503e25e2a401bc68ce175b4a0b4763
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-07-15 17:47:06 +0800

    fix[任务管理]:创建任务时，缺少三种类型： sqlserver, postgres, elasticsearch

--- src/settings.js
+++ src/settings.js
@@ -3,5 +3,5 @@ module.exports = {
   DFS_IGNORE_PERMISSION: true,
   DFS_TM_API_PRE_URL: '/tm',
   DFS_CREATE_DATAFLOW_BY_FORM: 1,
-  ALLOW_CONNECTION_TYPE: ['mysql', 'oracle', 'mongodb']
+  ALLOW_CONNECTION_TYPE: ['mysql', 'oracle', 'sqlserver', 'mongodb', 'postgres', 'elasticsearch']
 }
