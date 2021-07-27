ccc96f90b79dc8db5f1c161aaa4e0d361c6241b9
xufei <xufei@tapdata.io>
2021-07-27 16:23:11 +0800

    feat：formily用到的form组件移动到web-core目录，添加web-core的alias

--- src/components/form/ComboSelect.js
+++
@@ -1,4 +0,0 @@
-import { connect, mapProps } from '@formily/vue'
-import VSelect from '@/components/VSelect'
-
-export const ComboSelect = connect(VSelect, mapProps({ dataSource: 'options', loading: true }))
