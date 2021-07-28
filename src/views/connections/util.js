8c7d462ce9445f3733e9ddf85c31250164c176f8
zhangxin <dexter@tapdata.io>
2021-07-28 17:56:47 +0800

    fix(hive:frontend): show hive entrances

--- src/views/connections/util.js
+++ src/views/connections/util.js
@@ -62,7 +62,7 @@ export const TYPEMAP = {
   'mysql pxc': 'MySQL PXC',
   jira: 'jira',
   dameng: 'DM DB',
-  // hive: 'Hive',
+  hive: 'Hive',
   tcp_udp: 'TCP/IP',
   mq: 'MQ',
   hbase: 'HBase',
