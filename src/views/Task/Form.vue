708c4d4a8723855a164ff313db35cb9c12136b4b
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-08-30 16:16:22 +0800

    fix[创建任务]：点击上一步，会重新渲染数据

--- src/views/Task/Form.vue
+++ src/views/Task/Form.vue
@@ -703,12 +703,14 @@ export default {
         let returnData = this.$refs.fieldMappingDom.returnData()
         if (!returnData.valid) {
           this.activeStep -= 1
+          this.getFormConfig()
           return //检验不通过
         }
         let deleteLen = returnData.target.filter(v => !v.is_deleted)
         if (deleteLen.length === 0) {
           this.$message.error('当前表被删除了所有字段，不允许保存操作')
           this.activeStep -= 1
+          this.getFormConfig()
           return //所有字段被删除了 不可以保存任务
         }
         this.saveOperations()
