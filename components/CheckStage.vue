e91c2a175f19a412feb62314ad8cb82635fc2094
fannie <fannie@tapdata.io>
2021-09-03 21:48:33 +0800

    fix:初始化数据

--- components/CheckStage.vue
+++ components/CheckStage.vue
@@ -11,14 +11,14 @@
   >
     <el-table :data="data" style="width: 100%" class="test-block">
       <el-table-column type="index">序号</el-table-column>
-      <el-table-column :prop="stages[0].tableName" label="源表名">
+      <el-table-column label="源表名">
         <template slot-scope="scope">
-          {{ scope.row.stages[0].tableName }}
+          {{ scope.row.stages ? scope.row.stages[0].tableName : '' }}
         </template>
       </el-table-column>
       <el-table-column label="源表名">
         <template slot-scope="scope">
-          {{ scope.row.stages[-1].tableName }}
+          {{ scope.row.stages ? scope.row.stages[scope.row.stages.length - 1].tableName:'' }}
         </template>
       </el-table-column>
       <el-table-column prop="syncType" label="目标表名">
@@ -40,47 +40,7 @@
 export default {
   name: 'CheckStage',
   props: ['visible', 'data'],
-  mounted() {
-    this.data = [
-      {
-        stages: [
-          {
-            id: '12345',
-            tableName: 'source'
-          },
-          {
-            id: '1234',
-            tableName: 'target'
-          }
-        ],
-        type: 'add',
-        syncType: ''
-      },
-      {
-        stages: [
-          {
-            id: '123456',
-            tableName: 'source2'
-          },
-          {
-            id: '12347',
-            tableName: 'target2'
-          }
-        ],
-        type: 'add',
-        syncType: ''
-      }
-    ]
-    this.data = this.initData(this.data)
-  },
   methods: {
-    /*
-     * 只展示type类型为add
-     * stages 过滤其他的处理器*/
-    initData(initData) {
-      console.log(initData)
-      return initData.filter(v => v.type === 'add')
-    },
     Save() {
       this.$emit('complete', this.data)
     }
