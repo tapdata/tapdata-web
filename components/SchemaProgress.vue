97c3459b85ddec4a9c7d0ed5deb3a4eb88989470
payne <liuke_ext@139.com>
2021-09-06 12:37:45 +0800

    fix: 修复schema progress样式问题

--- components/SchemaProgress.vue
+++ components/SchemaProgress.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="schema-progress-wrapper flex align-items-center">
-    <i class="connections-status__icon mr-1" :class="'el-icon-' + schemaInfo.icon"></i>
+    <i
+      class="connections-schema-status__icon mr-1"
+      :class="'el-icon-' + schemaInfo.icon + ' color-' + schemaInfo.icon"
+    ></i>
     <ElLink v-if="schemaInfo.icon === 'error'" type="danger" @click="showErrorMsg">{{ schemaInfo.text }}</ElLink>
     <span v-else :class="'color-' + schemaInfo.icon">{{ schemaInfo.text }}</span>
   </div>
@@ -57,5 +60,8 @@ export default {
 <style lang="scss">
 .schema-error-dialog {
   width: 80%;
+}
+.schema-progress-wrapper .connections-schema-status__icon {
+  font-size: 14px;
 }
 </style>
