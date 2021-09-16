8aa31f59fc5026339f76dd032fe89b20c3b06a3a
xufei <xufei@thoughtup.cn>
2021-09-16 16:41:11 +0800

    feat: 子模块增加任务编辑

---
+++ views/dataflow/components/BaseNode.vue
@@ -0,0 +1,159 @@
+<template functional>
+  <div v-bind="data.attrs" v-on="listeners" class="df-node" :class="[data.class, data.staticClass]" :style="data.style">
+    <div class="df-node-icon">
+      <VIcon>{{ $options.getIcon(props.node) }}</VIcon>
+    </div>
+    <div class="df-node-text">{{ props.node.name }}</div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { NODE_TYPE_ICON } from 'web-core/views/dataflow/constants'
+import VIcon from '@/components/VIcon'
+
+export default {
+  name: 'BaseNode',
+  components: { VIcon },
+  props: {
+    node: Object
+  },
+
+  getIcon(node) {
+    return NODE_TYPE_ICON[node.type]
+  }
+}
+</script>
+
+<style lang="scss">
+.layout-content .df-node {
+  cursor: move;
+}
+
+.min-width-unset {
+  min-width: unset;
+}
+
+.df-node {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  width: 180px;
+  height: 48px;
+  background-color: #fff;
+  border: 1px solid #2c65ff;
+  border-radius: 10px;
+  box-sizing: border-box;
+
+  &-icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 45px;
+    height: 100%;
+    background-color: rgba(44, 101, 255, 0.2);
+    border-right-style: solid;
+    border-right-width: inherit;
+    border-color: inherit;
+    border-top-left-radius: inherit;
+    border-bottom-left-radius: inherit;
+
+    .v-icon {
+      color: #2c65ff;
+      font-size: 24px;
+    }
+  }
+
+  &-text {
+    margin: 0 10px;
+    flex: auto;
+    width: 0;
+    font-size: 14px;
+    line-height: 1;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  &-options {
+    position: absolute;
+    top: 100%;
+    display: none;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 32px;
+  }
+
+  .node-option {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 6px;
+    width: 20px;
+    height: 20px;
+    background-color: #9bb6ff;
+    border-radius: 100%;
+    cursor: pointer;
+    .v-icon {
+      width: 16px;
+      height: 16px;
+      font-size: 12px;
+      background-color: #2c65ff;
+      color: #fff;
+      border-radius: 100%;
+      &__svg {
+        width: 1em;
+        height: 1em;
+      }
+    }
+  }
+
+  &.node--data {
+    border-color: #6236ff;
+    .df-node-icon {
+      background-color: rgba(98, 54, 255, 0.2);
+      .v-icon {
+        width: 34px !important;
+        height: 34px !important;
+        background-color: #6236ff;
+        color: #fff;
+        font-size: 14px;
+        border-radius: 100%;
+        .v-icon__svg {
+          width: 1em;
+          height: 1em;
+        }
+      }
+    }
+  }
+
+  &.active,
+  &.jtk-drag-selected {
+    border-color: #fa6303;
+  }
+
+  &:hover,
+  &.options-active {
+    .df-node-options {
+      display: flex;
+    }
+  }
+}
+
+.df-menu-list {
+  margin: -6px;
+  .df-menu-item {
+    margin-bottom: 2px;
+    padding: 0 16px;
+    height: 32px;
+    line-height: 32px;
+    font-size: 12px;
+    border-radius: 6px;
+    cursor: pointer;
+    &:hover {
+      background-color: #eef3ff;
+    }
+  }
+}
+</style>
---
+++ views/dataflow/helpers.js
@@ -0,0 +1,79 @@
+export const getLeftmostTopNode = nodes => {
+  return nodes.reduce((leftmostTop, node) => {
+    if (node.position[0] > leftmostTop.position[0] || node.position[1] > leftmostTop.position[1]) {
+      return leftmostTop
+    }
+
+    return node
+  })
+}
+
+export const getDataflowCorners = nodes => {
+  return nodes.reduce(
+    (accu, node) => {
+      if (node.position[0] < accu.minX) {
+        accu.minX = node.position[0]
+      }
+      if (node.position[1] < accu.minY) {
+        accu.minY = node.position[1]
+      }
+      if (node.position[0] > accu.maxX) {
+        accu.maxX = node.position[0]
+      }
+      if (node.position[1] > accu.maxY) {
+        accu.maxY = node.position[1]
+      }
+
+      return accu
+    },
+    {
+      minX: nodes[0].position[0],
+      minY: nodes[0].position[1],
+      maxX: nodes[0].position[0],
+      maxY: nodes[0].position[1]
+    }
+  )
+}
+
+export const scaleSmaller = ({ scale, offset: [xOffset, yOffset] }) => {
+  scale /= 1.25
+  xOffset /= 1.25
+  yOffset /= 1.25
+  xOffset += window.innerWidth / 10
+  yOffset += window.innerHeight / 10
+
+  return {
+    scale,
+    offset: [xOffset, yOffset]
+  }
+}
+
+export const scaleBigger = ({ scale, offset: [xOffset, yOffset] }) => {
+  scale *= 1.25
+  xOffset -= window.innerWidth / 10
+  yOffset -= window.innerHeight / 10
+  xOffset *= 1.25
+  yOffset *= 1.25
+
+  return {
+    scale,
+    offset: [xOffset, yOffset]
+  }
+}
+
+export const scaleReset = config => {
+  if (config.scale > 1) {
+    // zoomed in
+    while (config.scale > 1) {
+      config = scaleSmaller(config)
+    }
+  } else {
+    while (config.scale < 1) {
+      config = scaleBigger(config)
+    }
+  }
+
+  config.scale = 1
+
+  return config
+}
