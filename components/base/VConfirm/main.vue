1de3f3559b5a832a0cde197835cc3231e2978405
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-09-24 14:39:36 +0800

    fix[对话框]：删除console

--- components/base/VConfirm/main.vue
+++ components/base/VConfirm/main.vue
@@ -120,7 +120,6 @@ export default {
     },
     onlyMessage() {
       let { title, message } = this
-      console.log('onlyMessage', !title && !!message)
       return !title && !!message
     }
   },
@@ -137,7 +136,6 @@ export default {
   },
   methods: {
     init() {
-      console.log('init', this.type)
       this.getIconByType(this.type)
     },
     handleAction(action) {
