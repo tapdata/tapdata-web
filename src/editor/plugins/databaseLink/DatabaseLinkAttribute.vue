5c34b4396cd301316f66bd8b156c33e43fb02e34
bassie <bassie@tapdata.io>
2021-09-26 18:38:13 +0800

    迁移转大小写

--- src/editor/plugins/databaseLink/DatabaseLinkAttribute.vue
+++ src/editor/plugins/databaseLink/DatabaseLinkAttribute.vue
@@ -33,29 +33,29 @@
             <el-row :gutter="20">
               <el-col :span="12">
                 <span class="span-label">{{ $t('dag_data_node_label_database_link_table') }}</span>
-                <el-select v-model="model.dropType" size="mini">
-                  <el-option :label="$t('dag_data_node_label_database_link_unchang')" value="no_drop"></el-option>
+                <el-select v-model="model.tableNameTransform" size="mini">
+                  <el-option :label="$t('dag_data_node_label_database_link_unchang')" value="noOperation"></el-option>
                   <el-option
                     :label="$t('dag_data_node_label_database_link_to_uppercase')"
-                    value="drop_data"
+                    value="toUpperCase"
                   ></el-option>
                   <el-option
                     :label="$t('dag_data_node_label_database_link_to_lowercase')"
-                    value="drop_schema"
+                    value="toLowerCase"
                   ></el-option>
                 </el-select>
               </el-col>
               <el-col :span="12">
                 <span class="span-label">{{ $t('dag_data_node_label_database_link_field') }}</span>
-                <el-select v-model="model.dropType" size="mini">
-                  <el-option :label="$t('dag_data_node_label_database_link_unchang')" value="no_drop"></el-option>
+                <el-select v-model="model.fieldsNameTransform" size="mini">
+                  <el-option :label="$t('dag_data_node_label_database_link_unchang')" value="noOperation"></el-option>
                   <el-option
                     :label="$t('dag_data_node_label_database_link_to_uppercase')"
-                    value="drop_data"
+                    value="toUpperCase"
                   ></el-option>
                   <el-option
                     :label="$t('dag_data_node_label_database_link_to_lowercase')"
-                    value="drop_schema"
+                    value="toLowerCase"
                   ></el-option>
                 </el-select>
               </el-col>
@@ -129,6 +129,7 @@
               <MqTransfer
                 v-model="mqActiveData"
                 :source="sourceData"
+                :tableNameTransform="model.tableNameTransform"
                 :table_prefix="model.table_prefix"
                 :table_suffix="model.table_suffix"
               ></MqTransfer>
@@ -166,7 +167,7 @@ import ws from '@/api/ws'
 const metadataApi = new MetadataInstances()
 
 import CodeEditor from '@/components/CodeEditor'
-import { ALLOW_FIELD_MAPPING } from '../../constants'
+// import { ALLOW_FIELD_MAPPING } from '../../constants'
 let connections = factory('connections')
 let editorMonitor = null
 export default {
@@ -371,10 +372,14 @@ export default {
           }
         }
         targetCell && this.loadTables(connectionId, sourceFormData.database_type)
+        this.loadDataModels(connectionId)
+        //获取目标节点ID
+        this.stageId = targetCell.id || ''
       }
 
       editorMonitor = vueAdapter.editor
       this.configJoinTable = cell.configJoinTable && cell.configJoinTable()
+      this.getDataFlow()
       this.getDataFlow()
       //是否显示字段推演
       let param = {
@@ -528,6 +533,14 @@ export default {
         this.transformModelVersion = true
       } else {
         this.transformModelVersion = false
+      }
+      return this.dataFlow
+    },
+    // 获取表名称
+    loadDataModels(connectionId) {
+      let self = this
+      if (!connectionId) {
+        return
       }
       return this.dataFlow
     },
