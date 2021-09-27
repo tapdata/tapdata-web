a61c71fe4faf333aad77cc2214a4089478c452fb
jackin <jackin@tapdata.io>
2021-09-27 20:39:26 +0800

    1. Add oracle concurrency manually logminer.

--- src/editor/constants.js
+++ src/editor/constants.js
@@ -66,7 +66,8 @@ export const FORM_DATA_KEY = 'form_data',
     transformerConcurrency: 8,
     lagTimeFalg: false,
     userSetLagTime: 0,
-    noPrimaryKey: false
+    noPrimaryKey: false,
+    oracleLogminer: 'automatically'
   },
   DATABASE_TYPE_MAPPING = {
     mysql: {
