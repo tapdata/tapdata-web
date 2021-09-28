81e4981fd1c24122078bdd618af8c6b5a70ed211
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-09-28 20:16:53 +0800

    fix[任务管理]：监控页，没有启动过的任务，结束时间为空

--- src/views/Monitor/Dashboard.vue
+++ src/views/Monitor/Dashboard.vue
@@ -317,7 +317,7 @@ export default {
       data.typeText = data.mappingTemplate === 'cluster-clone' ? '迁移任务' : '同步任务'
       let cdcTime = data.cdcLastTimes?.[0]?.cdcTime || ''
       data.startTimeFmt = this.formatTime(data.startTime)
-      data.endTimeFmt = this.formatTime(data.finishTime)
+      data.endTimeFmt = data.startTime ? this.formatTime(data.finishTime) : '-'
       data.cdcTimeFmt = this.formatTime(cdcTime)
       return data
     },
