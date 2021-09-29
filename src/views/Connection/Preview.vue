4ac7ba0ba4338242ee3eccd96f09ec657c1562cd
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-09-29 11:41:36 +0800

    fix[连接管理]：详情，显示密码

--- src/views/Connection/Preview.vue
+++ src/views/Connection/Preview.vue
@@ -313,7 +313,7 @@ export default {
           items: [
             {
               label: '密码',
-              key: '-'
+              key: 'database_password'
             }
           ]
         },
