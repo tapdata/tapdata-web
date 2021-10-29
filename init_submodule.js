106bccdfc49bcb39a887b9a45dfcc6bd8b23e7db
xufei <xufei@tapdata.io>
2021-10-30 00:29:03 +0800

    fix: 删掉 config 里面的子模块分支配置，防止覆盖 .gitmodules

--- init_submodule.js
+++ init_submodule.js
@@ -2,6 +2,9 @@ const { join } = require('path')
 const fs = require('fs')
 const cp = require('child_process')
 
+// 删掉 config 里面的子模块分支配置，防止覆盖 .gitmodules
+cp.exec(`git config --unset submodule.frontend-el/src/_packages/tapdata-web-core.branch`)
+
 cp.exec(
   `git submodule update --init --recursive & git config submodule.recurse true & git config status.submodulesummary 1`
 )
@@ -9,6 +12,7 @@ cp.exec(`git config --remove-section alias`, function () {
   fs.readFile(join(__dirname, '../.git/config'), 'utf8', function (err, data) {
     if (err) throw err
     let lines = data.split('\n')
+    console.log('lines', lines)
     let array = []
     for (let i = 0; i < lines.length; i++) {
       const str = lines[i]
@@ -18,30 +22,28 @@ cp.exec(`git config --remove-section alias`, function () {
     }
     lines = array
     let index = lines.findIndex(str => str === '[alias]')
-    if (index == -1) {
-      lines.push('[alias]')
-      index = lines.length
-    }
-    const spushIndex = lines.findIndex(str => str.includes('\tspush = submodule foreach git push origin HEAD:release'))
-    if (~spushIndex) {
-      lines.splice(
-        spushIndex,
-        1,
-        "\tspush = submodule foreach 'git push origin HEAD:$(git config -f ../../../../.gitmodules submodule.frontend-el/src/_packages/tapdata-web-core.branch)'"
-      )
-    } else {
-      lines.splice(
-        index,
-        0,
+    let spushIndex = lines.findIndex(str => str.includes('\tspush = submodule foreach git push origin HEAD:release'))
+    let ifUpdate = false
+    if (index < 0) {
+      ifUpdate = true
+      lines.push(
+        '[alias]',
         "\tsdiff = !git diff && git submodule foreach 'git diff'",
         '\tsstatus = submodule foreach git status',
         '\tsadd = submodule foreach git add',
         '\tscommit = submodule foreach git commit',
         '\tspull = submodule update --remote --merge',
         "\tspush = submodule foreach 'git push origin HEAD:$(git config -f ../../../../.gitmodules submodule.frontend-el/src/_packages/tapdata-web-core.branch)'",
-        '\tsu = submodule update'
+        '\tsu = submodule update',
+        '\n'
       )
-      lines.push('\n')
+    } else if (~spushIndex) {
+      ifUpdate = true
+      lines.splice(
+        spushIndex,
+        1,
+        "\tspush = submodule foreach 'git push origin HEAD:$(git config -f ../../../../.gitmodules submodule.frontend-el/src/_packages/tapdata-web-core.branch)'"
+      )
     }
 
     fs.writeFile(join(__dirname, '../.git/config'), lines.join('\n'), function (err) {
