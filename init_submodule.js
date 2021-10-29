5677940f1c9817982d0f91f07cb4d3631ddd138c
xufei <xufei@tapdata.io>
2021-10-29 23:58:27 +0800

    feat: V1.25创建V1.25-webcore子模块分支

--- init_submodule.js
+++ init_submodule.js
@@ -22,21 +22,28 @@ cp.exec(`git config --remove-section alias`, function () {
       lines.push('[alias]')
       index = lines.length
     }
-    if (lines.find(str => str.includes("\tsdiff = !git diff && git submodule foreach 'git diff'"))) {
-      return
+    const spushIndex = lines.findIndex(str => str.includes('\tspush = submodule foreach git push origin HEAD:release'))
+    if (~spushIndex) {
+      lines.splice(
+        spushIndex,
+        1,
+        "\tspush = submodule foreach 'git push origin HEAD:$(git config -f ../../../../.gitmodules submodule.frontend-el/src/_packages/tapdata-web-core.branch)'"
+      )
+    } else {
+      lines.splice(
+        index,
+        0,
+        "\tsdiff = !git diff && git submodule foreach 'git diff'",
+        '\tsstatus = submodule foreach git status',
+        '\tsadd = submodule foreach git add',
+        '\tscommit = submodule foreach git commit',
+        '\tspull = submodule update --remote --merge',
+        "\tspush = submodule foreach 'git push origin HEAD:$(git config -f ../../../../.gitmodules submodule.frontend-el/src/_packages/tapdata-web-core.branch)'",
+        '\tsu = submodule update'
+      )
+      lines.push('\n')
     }
-    lines.splice(
-      index,
-      0,
-      "\tsdiff = !git diff && git submodule foreach 'git diff'",
-      '\tsstatus = submodule foreach git status',
-      '\tsadd = submodule foreach git add',
-      '\tscommit = submodule foreach git commit',
-      '\tspull = submodule update --remote --merge',
-      '\tspush = submodule foreach git push origin HEAD:release',
-      '\tsu = submodule update'
-    )
-    lines.push('\n')
+
     fs.writeFile(join(__dirname, '../.git/config'), lines.join('\n'), function (err) {
       if (err) throw err
       console.log('写入git别名成功: sdiff, sstatus, sadd, scommit, spull, spush')
