fa7800790f81042637dbab73db7aa0f1762059f3
xufei <xufei@tapdata.io>
2021-11-01 11:02:42 +0800

    feat: 新增子模块的版本分支初始化命令 git sinit，执行之后会自动创建子模块版本分支，格式为：主分支名-webcore

--- init_submodule.js
+++ init_submodule.js
@@ -2,6 +2,9 @@ const { join } = require('path')
 const fs = require('fs')
 const cp = require('child_process')
 
+// 删掉 config 里面的子模块分支配置，防止覆盖 .gitmodules
+cp.exec(`git config --unset submodule.frontend-el/src/_packages/tapdata-web-core.branch`)
+
 cp.exec(
   `git submodule update --init --recursive & git config submodule.recurse true & git config status.submodulesummary 1`
 )
@@ -22,21 +25,30 @@ cp.exec(`git config --remove-section alias`, function () {
       lines.push('[alias]')
       index = lines.length
     }
-    if (lines.find(str => str.includes("\tsdiff = !git diff && git submodule foreach 'git diff'"))) {
-      return
+    const spushIndex = lines.findIndex(str => str.includes('\tspush = submodule foreach git push origin HEAD:release'))
+    if (~spushIndex) {
+      lines.splice(
+        spushIndex,
+        1,
+        "\tspush = submodule foreach 'git push origin HEAD:$(git config -f ../../../.gitmodules submodule.src/_packages/tapdata-web-core.branch)'",
+        "\tsinit = submodule foreach 'git checkout -b $(git -C ../../../ rev-parse --abbrev-ref HEAD)-webcore'"
+      )
+    } else {
+      lines.splice(
+        index,
+        0,
+        "\tsdiff = !git diff && git submodule foreach 'git diff'",
+        '\tsstatus = submodule foreach git status',
+        '\tsadd = submodule foreach git add',
+        '\tscommit = submodule foreach git commit',
+        '\tspull = submodule update --remote --merge',
+        "\tspush = submodule foreach 'git push origin HEAD:$(git config -f ../../../.gitmodules submodule.src/_packages/tapdata-web-core.branch)'",
+        "\tsinit = submodule foreach 'git checkout -b $(git -C ../../../ rev-parse --abbrev-ref HEAD)-webcore'",
+        '\tsu = submodule update'
+      )
+      lines.push('\n')
     }
-    lines.splice(
-      index,
-      0,
-      "\tsdiff = !git diff && git submodule foreach 'git diff'",
-      '\tsstatus = submodule foreach git status',
-      '\tsadd = submodule foreach git add',
-      '\tscommit = submodule foreach git commit',
-      '\tspull = submodule update --remote --merge',
-      '\tspush = submodule foreach git push origin HEAD:release',
-      '\tsu = submodule update'
-    )
-    lines.push('\n')
+
     fs.writeFile(join(__dirname, './.git/config'), lines.join('\n'), function (err) {
       if (err) throw err
       console.log('写入git别名成功: sdiff, sstatus, sadd, scommit, spull, spush')
