511e7cdcd24d56adba9f78111af7fc0a603e46b3
fannie <fannie@tapdata.io>
2021-11-02 17:23:48 +0800

    hotfix[连接管理]:字段推演，请求源表目标表接口参数转码

--- src/views/Task/Form.vue
+++ src/views/Task/Form.vue
@@ -1268,11 +1268,11 @@ export default {
     //获取表设置
     async intiFieldMappingTableData(row) {
       let source = await this.$axios.get(
-        'tm/api/MetadataInstances/originalData?qualified_name=' + row.sourceQualifiedName
+        'tm/api/MetadataInstances/originalData?qualified_name=' + encodeURIComponent(row.sourceQualifiedName)
       )
       source = source && source.length > 0 ? source[0].fields : []
       let target = await this.$axios.get(
-        'tm/api/MetadataInstances/originalData?isTarget=true&qualified_name=' + row.sinkQulifiedName
+        'tm/api/MetadataInstances/originalData?isTarget=true&qualified_name=' + encodeURIComponent(row.sinkQulifiedName)
       )
       // 初始化所有字段都映射 只取顶级字段
       source = source.filter(field => field.field_name.indexOf('.') === -1)
