cfc9ffe9373d7f9375919d7d19daaabbe857fc9b
bassie <bassie@tapdata.io>
2021-11-25 11:27:24 +0800

    fix(kudu): kudu可以作为源

--- src/views/connections/config/kudu.js
+++ src/views/connections/config/kudu.js
@@ -18,16 +18,16 @@ export default function (vm) {
         field: 'connection_type',
         label: vm.$t('dataForm.form.connectionType'),
         options: [
-          // {
-          //   label: vm.$t('dataForm.form.options.sourceAndTarget'),
-          //   tip: vm.$t('dataForm.form.options.sourceAndTargetTips'),
-          //   value: 'source_and_target'
-          // },
-          // {
-          //   label: vm.$t('dataForm.form.options.source'),
-          //   tip: vm.$t('dataForm.form.options.sourceTips'),
-          //   value: 'source'
-          // },
+          {
+            label: vm.$t('dataForm.form.options.sourceAndTarget'),
+            tip: vm.$t('dataForm.form.options.sourceAndTargetTips'),
+            value: 'source_and_target'
+          },
+          {
+            label: vm.$t('dataForm.form.options.source'),
+            tip: vm.$t('dataForm.form.options.sourceTips'),
+            value: 'source'
+          },
           {
             label: vm.$t('dataForm.form.options.target'),
             tip: vm.$t('dataForm.form.options.targetTips'),
