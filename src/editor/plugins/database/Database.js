87c8b5a4b628ad4b6de2af696468714e00afe399
bassie <bassie@tapdata.io>
2021-11-29 11:32:13 +0800

    merge(合并分支): 分支合并

--- src/editor/plugins/database/Database.js
+++ src/editor/plugins/database/Database.js
@@ -152,13 +152,8 @@ export const databaseConfig = {
         // sourceCell 拖动的时候会产生一条outLink,但是没有target.id，以此来限制当节点有目标时，不允许再去连接其他目标
         return (
           ['app.Database'].includes(sourceCell.get('type')) &&
-<<<<<<< HEAD
           !['hive', 'dameng', 'clickhouse'].includes(databaseType) &&
           // !['kudu'].includes(sourceCell?.attributes?.form_data?.database_type) &&
-=======
-          !['hive', 'dameng', 'clickhouse', 'adb_postgres'].includes(databaseType) &&
-          !['kudu'].includes(sourceCell?.attributes?.form_data?.database_type) &&
->>>>>>> bassie-v1.28
           outLinks.length === 1 &&
           !outLinks[0].attributes.target.id
         )
