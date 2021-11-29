4d9994ca5abb74edc428565dc53896e66dc019ad
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-11-29 15:53:54 +0800

    fix(任务管理-运行监控): 创建人取createUser

--- src/views/monitor/Dashboard.vue
+++ src/views/monitor/Dashboard.vue
@@ -438,7 +438,7 @@ export default {
     formatTask(data) {
       data.totalOutput = data.stats?.output?.rows || 0
       data.totalInput = data.stats?.input?.rows || 0
-      data.creator = data?.creator || data.username || data.user?.username || '-'
+      data.creator = data.createUser || '-'
       data.typeText = data.mappingTemplate === 'cluster-clone' ? '迁移任务' : '同步任务'
       let cdcTime = data.cdcLastTimes?.[0]?.cdcTime || ''
       data.startTimeFmt = this.formatTime(data.startTime)
