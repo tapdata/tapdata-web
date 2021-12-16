89683454f348be57616c806d109d036d737c850e
payne <payne@tapdata.io>
2021-12-16 18:45:07 +0800

    fix(函数管理): 修复详情页样式问题

--- src/views/function/Details.vue
+++ src/views/function/Details.vue
@@ -40,7 +40,7 @@
           </ElFormItem>
         </ElForm>
         <div v-if="details.type === 'custom'" class="mb-4">
-          <div class="details-panel-title mb-4">{{ $t('function_script_label') }}:</div>
+          <div class="details-panel-title mb-4">{{ $t('function_script_label') }}</div>
           <div class="script-editor">
             <CodeEditor
               v-model="details.script"
@@ -127,7 +127,7 @@ export default {
       }
     }
     .details-panel-title {
-      font-size: 12px;
+      font-size: 14px;
     }
     .script-editor {
       max-width: 940px;
