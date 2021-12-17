2e37211428e1ef928695b56734dca448cb5e9f04
fannie <fannie@tapdata.io>
2021-12-17 19:54:56 +0800

    fix(dag): 字段映射，文案优化

--- src/i18n/langs/zh-TW.js
+++ src/i18n/langs/zh-TW.js
@@ -12,6 +12,7 @@ export default {
   button_check: '查看',
   button_delete: '刪除',
   button_setting: '設置',
+  button_reduction: '還原',
 
   // 通用下拉框
   select_option_all: '全部',
@@ -145,11 +146,11 @@ export default {
   dag_dialog_field_mapping_error_range: '當前值不符合該字段範圍',
   dag_dialog_field_mapping_error_save_prefix: '檢測到您還有',
   dag_dialog_field_mapping_error_save_suffix: '張表的字段類型設置存在問題，請在左側表區域選擇有問題的表進行處理',
-  dag_dialog_field_mapping_change_type_field_rename:'類型修改',
-  dag_dialog_field_mapping_batch_change_type:'批量修改字段類型',
-  dag_dialog_field_mapping_batch_change_type_source:'源字段類型',
-  dag_dialog_field_mapping_batch_change_type_target:'目標字段類型',
-  dag_dialog_field_mapping_batch_change_type_error_tip:'請先選擇需要源表/目標表轉換的類型',
+  dag_dialog_field_mapping_change_type_field_rename: '類型修改',
+  dag_dialog_field_mapping_batch_change_type: '批量修改字段類型',
+  dag_dialog_field_mapping_batch_change_type_source: '源字段類型',
+  dag_dialog_field_mapping_batch_change_type_target: '目標字段類型',
+  dag_dialog_field_mapping_batch_change_type_error_tip: '請先選擇需要源表/目標表轉換的類型',
 
   //任務編輯
   dag_job_check_source: '作為源僅支持全量任務',
