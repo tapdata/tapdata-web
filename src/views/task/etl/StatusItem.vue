1fb80081378fe8f7a4f4107e76a9c053bdf2ae9f
liangzhikai@tapdata.io <liangzhiai@tapdata.io>
2021-12-22 17:40:25 +0800

    feat(同步任务列表): 状态统计

---
+++ src/views/task/etl/StatusItem.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="etl-status-item">
+    <div v-for="(item, index) in result" :key="index">
+      <span
+        :class="['circle-icon', 'mr-2', !!item.type && `bg-color-${item.type}`]"
+        :style="{ 'background-color': item.color }"
+      ></span>
+      <span class="etl-status-item__text font-color-sub">{{ item.text }}</span>
+      <span v-if="showCount" class="etl-status-item__count font-color-sub">{{ ': ' + (item.count || 0) }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ETL_STATUS_MAP, ETL_SUB_STATUS_MAP } from '@/const'
+import { deepCopy } from '@/utils/util'
+export default {
+  name: 'StatusItem',
+  props: {
+    rows: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      result: [],
+      statusMap: {
+        running: ['scheduling', 'running', 'pausing', 'stopping', 'scheduling'],
+        not_running: ['wait_run', 'pause', 'stop', 'complete', 'schedule_failed'],
+        error: ['error']
+      }
+    }
+  },
+  computed: {
+    showCount() {
+      return this.result.length > 1
+    }
+  },
+  watch: {
+    rows: {
+      deep: true,
+      handler() {
+        this.init()
+      }
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.result = this.getData(this.rows)
+      this.$emit('input', this.result)
+    },
+    getData(rows = []) {
+      const len = rows.length
+      let result
+      switch (len) {
+        case 0:
+          result = [Object.assign({ count: 1 }, ETL_STATUS_MAP['not_running'])]
+          break
+        case 1:
+          result = [Object.assign({ count: 1 }, ETL_SUB_STATUS_MAP[rows[0]?.status])]
+          break
+        default:
+          result = this.formatResult()
+          break
+      }
+      return result
+    },
+    formatResult() {
+      const { rows, statusMap } = this
+      let result = []
+      let obj = deepCopy(ETL_STATUS_MAP)
+      for (let key in obj) {
+        obj[key].count = 0
+        rows.forEach(el => {
+          if (statusMap[key].includes(el.status)) {
+            obj[key].count++
+          }
+        })
+        result.push(obj[key])
+      }
+      return result
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.etl-status-item {
+  font-size: 12px;
+  line-height: 16px;
+}
+.circle-icon {
+  display: inline-block;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+}
+</style>
