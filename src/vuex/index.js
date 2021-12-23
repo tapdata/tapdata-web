10b8d703d20c559297950c20b8b1a0a452c3a370
xufei <xufei@tapdata.io>
2021-12-23 21:31:44 +0800

    chore: 更新子模块

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -18,7 +18,7 @@ const store = new Vuex.Store({
         removeItem: () => {}
       },
       filter({ type }) {
-        console.log('vux-type', type)
+        // console.log('vux-type', type)
         return !type.startsWith('dataflow')
       }
     })
