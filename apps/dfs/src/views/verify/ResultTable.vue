03dafc7318b2dc3afc06125ef7fccb7d03fb9a21
kennen <liangzhikai@tapdata.io>
2022-02-25 18:37:00 +0800

    fix(数据校验): 屏蔽重新校验

--- apps/dfs/src/views/verify/ResultTable.vue
+++ apps/dfs/src/views/verify/ResultTable.vue
@@ -72,13 +72,13 @@
         </div>
       </template>
     </ElTableColumn>
-    <ElTableColumn prop="status" :label="$t('verify_operation')">
-      <template slot-scope="scope">
-        <ElButton type="text" :disabled="verifyAgainDisabled" @click.prevent.stop="verifyAgain(scope.row)">{{
-          $t('verify_operation_verify_again')
-        }}</ElButton>
-      </template>
-    </ElTableColumn>
+    <!--    <ElTableColumn prop="status" :label="$t('verify_operation')">-->
+    <!--      <template slot-scope="scope">-->
+    <!--        <ElButton type="text" :disabled="verifyAgainDisabled" @click.prevent.stop="verifyAgain(scope.row)">{{-->
+    <!--          $t('verify_operation_verify_again')-->
+    <!--        }}</ElButton>-->
+    <!--      </template>-->
+    <!--    </ElTableColumn>-->
   </el-table>
 </template>
 <style lang="scss" scoped>
