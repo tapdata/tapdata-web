f5f7e0143936ab051520bb7b73360e3ba3d32f69
payne <payen@tapdata.io>
2022-03-08 11:10:02 +0800

    feat: 完成多表合并原地更新联调

--- packages/dag/src/components/FormPanel.vue
+++ packages/dag/src/components/FormPanel.vue
@@ -285,6 +285,18 @@ export default {
             }
           })
         },
+        getTargetNode: field => {
+          const id = field.form.values.id
+          const edges = this.$store.getters['dataflow/allEdges']
+          const nodes = this.$store.getters['dataflow/allNodes']
+          const sourceArr = edges.filter(({ source }) => source === id)
+          return sourceArr.map(({ target }) => {
+            return {
+              value: target,
+              label: nodes.find(node => node.id === target).name
+            }
+          })
+        },
 
         /**
          * 加载源节点的schema
