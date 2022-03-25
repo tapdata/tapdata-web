b15148e643d9c0b44f08f4388f8058544a8b5a6c
bassie <bassie@tapdata.io>
2022-03-25 14:45:49 +0800

    fix(函数详情): 获取函数详情数据  #61161

--- apps/daas/src/views/function/Details.vue
+++ apps/daas/src/views/function/Details.vue
@@ -52,7 +52,7 @@
           ></CodeEditor>
         </div>
       </div>
-      <div class="footer p-6">
+      <div class="footer pt-6 pb-4">
         <ElButton class="btn" size="mini" @click="$router.back()">{{ $t('button_back') }}</ElButton>
       </div>
     </div>
@@ -81,15 +81,9 @@ export default {
         jar: this.$t('function_type_option_jar'),
         system: this.$t('function_type_option_system')
       }
-      let filter = {
-        where: {
-          id: this.$route.params.id
-        }
-      }
+
       this.$api('Javascript_functions')
-        .findOne({
-          filter: JSON.stringify(filter)
-        })
+        .get([this.$route.params.id])
         .then(res => {
           let details = res?.data || {}
           // 处理老数据问题
@@ -122,6 +116,10 @@ export default {
   //   box-shadow: 0px 0px 3px 0px #cccccc;
   //   overflow: hidden;
   ::v-deep {
+    .el-form {
+      flex: 1;
+      overflow-y: auto;
+    }
     .el-form-item__label {
       font-size: 12px;
     }
@@ -142,7 +140,7 @@ export default {
   }
   .footer {
     border-top: 1px solid #f0f0f0;
-    box-shadow: 0px -1px 2px 0px #f6f6f6;
+    // box-shadow: 0px -1px 2px 0px #f6f6f6;
     .btn {
       width: 80px;
     }
