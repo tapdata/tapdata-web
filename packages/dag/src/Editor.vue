bedba6ae71cab213c593ac425f3033444b831f61
xufei <xufei@tapdata.io>
2022-03-29 16:18:46 +0800

    fix(dag): 节点不应该连接自己的上游

--- packages/dag/src/Editor.vue
+++ packages/dag/src/Editor.vue
@@ -417,7 +417,7 @@ export default {
         return target.__Ctor.allowSource(source) && source.__Ctor.allowTarget(target, source)
       }
 
-      return true
+      return false
     },
 
     initNodeView() {
@@ -1231,6 +1231,8 @@ export default {
     },
 
     nodeELIsConnected(s, t) {
+      const connections = this.jsPlumbIns.getConnections('*')
+      console.log('connections', connections)
       return this.jsPlumbIns.getConnections('*').some(c => `${c.sourceId}` === s && `${c.targetId}` === t)
     },
 
