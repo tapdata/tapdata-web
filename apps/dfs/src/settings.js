d7b434a946da97d03f788cdff952a29bf3c111dd
kennen <liangzhikai@tapdata.io>
2022-04-02 17:12:39 +0800

    chrome(setting): 清理没引用的属性

--- apps/dfs/src/settings.js
+++ apps/dfs/src/settings.js
@@ -1,24 +1,3 @@
 module.exports = {
-  DFS_TCM_PLATFORM: 'dfs',
-  DFS_IGNORE_PERMISSION: true,
-  DFS_TM_API_PRE_URL: '/tm',
-  DFS_CREATE_DATAFLOW_BY_FORM: 1,
-  ALLOW_CONNECTION_TYPE: [
-    'mysql',
-    'oracle',
-    'sqlserver',
-    'mongodb',
-    'postgres',
-    'elasticsearch',
-    'dameng',
-    'kafka',
-    'greenplum',
-    'mq',
-    'clickhouse',
-    'kundb',
-    'adb_postgres',
-    'adb_mysql',
-    'dummydb',
-    'tidb'
-  ]
+  DFS_TM_API_PRE_URL: '/tm'
 }
