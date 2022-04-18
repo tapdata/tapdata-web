6073207559c28d1dde52e3fdabe2a2fb5ae901f6
bassie <bassie@tapdata.io>
2022-04-18 16:26:52 +0800

    feat(api审计): 修改api审计详情数据

--- apps/daas/src/views/api-page/ApiAuditInfo.vue
+++ apps/daas/src/views/api-page/ApiAuditInfo.vue
@@ -2,34 +2,48 @@
   <section class="apiaudit-info-wrap section-wrap">
     <div class="details-box bg-white p-6 rounded-2">
       <div class="title fs-7 fw-sub font-color-normal">{{ $t('apiaudit_log_info') }}</div>
-      <ElRow class="pt-4">
-        <ElCol class="font-color-light pb-4" :span="12"> <span>API ID </span>:{{ auditData.id }}</ElCol>
+      <ElRow class="pt-4" v-if="auditData">
+        <ElCol class="font-color-light pb-4" :span="12">
+          API ID: <span>{{ auditData.id ? auditData.id : '-' }}</span></ElCol
+        >
         <ElCol class="font-color-light pb-4" :span="12"
-          ><span>{{ $t('apiaudit_name') }}</span
-          >: {{ auditData.name }}</ElCol
+          >{{ $t('apiaudit_name') }}: <span class="fw-sub">{{ auditData.name || '-' }}</span></ElCol
         >
         <ElCol class="font-color-light" :span="12"
-          ><span>{{ $t('apiaudit_link') }}</span
-          >: {{ auditData.apiPath }}</ElCol
+          >{{ $t('apiaudit_link') }}: <span class="fw-sub">{{ auditData.apiPath || '-' }}</span></ElCol
         >
         <ElCol class="font-color-light" :span="12"
-          ><span>{{ $t('apiaudit_interview_time') }}</span
-          >: {{ auditData.createAt }}</ElCol
+          >{{ $t('apiaudit_interview_time') }}: <span class="fw-sub"> {{ auditData.createAt }}</span></ElCol
         >
       </ElRow>
     </div>
-    <div class="details-box bg-white p-6 mt-6 rounded-2">
+    <div class="details-box bg-white py-6 mt-6 rounded-2">
       <ul class="flex flex-row justify-content-center">
-        <li v-for="item in list" :key="item.key" class="details-box-item">
-          <div class="fs-8 font-color-light">{{ item.label }}</div>
-          <div class="link-primary fs-2 din-font">{{ item.value }}</div>
+        <li
+          v-for="item in list"
+          :key="item.key"
+          class="details-box-item flex flex-sm-row justify-content-between text-center align-items-center"
+        >
+          <div class="w-100 text-center">
+            <div class="fs-8 font-color-light">{{ item.label }}</div>
+            <!-- <div
+              class="link-primary pt-4 din-font details-box-item-num"
+              v-if="['latency', 'averResponseTime'].includes(item.key)"
+            >
+              {{ item.value }}
+            </div> -->
+            <div class="link-primary pt-4 din-font details-box-item-num">
+              {{ item.value }}
+            </div>
+          </div>
+          <div class="line" v-if="item.key !== 'averResponseTime'"></div>
         </li>
       </ul>
     </div>
 
-    <div class="details-box bg-white p-6 mt-6 rounded-2">
+    <div class="details-box flex-1 bg-white p-6 mt-6 rounded-2">
       <div class="title fs-7 fw-sub font-color-normal">{{ $t('apiaudit_parameter') }}</div>
-      <div class="pt-4 editor-box">
+      <div class="pt-4 editor-box" v-if="auditData">
         <CodeEditor
           v-model="auditData.reqParams"
           ref="editor"
@@ -44,6 +58,7 @@
 
 <script>
 import CodeEditor from '@/components/CodeEditor'
+import { formatMs } from '@/utils/util'
 export default {
   name: 'ApiAudit',
   components: { CodeEditor },
@@ -72,8 +87,22 @@ export default {
         .then(res => {
           if (res) {
             this.auditData = res.data
+            this.auditData.createAt = res.data['createAt']
+              ? this.$moment(res.data['createAt']).format('YYYY-MM-DD HH:mm:ss')
+              : '-'
+
+            this.list.forEach(item => {
+              for (let el in res.data) {
+                if (item.key === el) {
+                  item.value = res.data[el]
+                }
+              }
+            })
           }
         })
+    },
+    formatMs(ms) {
+      return formatMs(ms)
     }
   }
 }
@@ -81,8 +110,17 @@ export default {
 <style lang="scss" scoped>
 .apiaudit-info-wrap {
   .details-box-item {
-    padding: 0 20px;
+    flex: 1;
     text-align: center;
+    .line {
+      width: 1px;
+      height: 58px;
+      background-color: #f2f2f2;
+    }
+
+    .details-box-item-num {
+      font-size: 30px;
+    }
   }
 }
 </style>
