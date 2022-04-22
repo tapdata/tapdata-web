28e3f71810e35c3593638e783062117a87e0da4b
fannie <fannie@tapdata.io>
2022-04-22 10:58:19 +0800

    fix(api发布): 发布状态统一样式

--- apps/daas/src/views/api-page/Modules.vue
+++ apps/daas/src/views/api-page/Modules.vue
@@ -76,8 +76,10 @@
         </template>
       </el-table-column>
       <el-table-column :label="$t('modules_header_status')">
-        <template slot-scope="scope">
-          <span>{{ $t('modules_' + scope.row.status) }}</span>
+        <template #default="{ row }">
+          <span :class="['status-' + row.status, 'status-block', 'mr-2']">
+            {{ $t('modules_' + row.status) }}
+          </span>
         </template>
       </el-table-column>
       <el-table-column :label="$t('modules_header_basePath')" show-overflow-tooltip>
