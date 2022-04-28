5815d1e3e5f83136e3e22988b2caf8acdde62f07
kennen <liangzhikai@tapdata.io>
2022-04-28 15:55:11 +0800

    feat(国际化): 添加矫正文案的文件

--- apps/dfs/src/i18n/index.js
+++ apps/dfs/src/i18n/index.js
@@ -5,10 +5,14 @@ import locale from 'element-ui/lib/locale'
 import enLocale from 'element-ui/lib/locale/lang/en'
 import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
 import tcLocale from 'element-ui/lib/locale/lang/zh-TW'
-import en from './langs/en'
 import zhCN from './langs/zh-CN'
-import zhTW from './langs/zh-TW'
+import enSource from './langs/en'
+import enModify from './modify/en'
+import zhTWSource from './langs/zh-TW'
+import zhTWModify from './modify/zh-TW'
 
+const en = Object.assign({}, enSource, enModify)
+const zhTW = Object.assign({}, zhTWSource, zhTWModify)
 let eleLangs = {
   'zh-CN': zhLocale,
   'zh-TW': tcLocale,
