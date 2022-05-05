dc2efe2dcd5996ebe7aa2877cb39d20d6ffda113
kennen <liangzhikai@tapdata.io>
2022-05-05 15:54:06 +0800

    feat(国际化): 矫正文案

--- apps/dfs/src/i18n/modify/en.js
+++ apps/dfs/src/i18n/modify/en.js
@@ -1,1 +1,17 @@
-export default {}
+export default {
+  gl_select_option_all: 'all',
+  button_test_connection: 'Test',
+  list_operation: 'Operation',
+  notify_agent_notification: 'Agent notification',
+  notify_all_read: 'Mark All as Read',
+  task_start_task: 'Start',
+  task_operation_monitor: 'Monitor',
+  task_initial_sync: 'INITIAL SYNC',
+  task_initial_sync_cdc: 'INITIAL SYNC+ CDC',
+  operation_log_migration_start: 'Start task1111',
+  verify_configuration: 'Config',
+  dataVerification_verifyProgress: 'Verify Progress',
+  dataVerification_verifyResult: 'Verify Progress',
+  dataVerification_done: 'Verify finished',
+  dataVerification_sourceRows: 'Source Table Rows'
+}
