b84d0323842488ad7d87d69ebcf73ab47c7f9266
fannie <fannie@tapdata.io>
2022-05-25 21:06:41 +0800

    fix(@tap/form): &任务设置 添加增量开始时间

--- apps/daas/vue.config.js
+++ apps/daas/vue.config.js
@@ -7,7 +7,7 @@ const serveUrlMap = {
   dev: 'http://localhost:3000', // TM端本地默认地址
   jet: 'http://jet.devops.tapdata.net:31613',
   // test: 'http://192.168.1.181:31438/' // 自定义节点
-  test: 'http://192.168.1.181:31025' // v2.0
+  test: 'http://192.168.1.132:31787' // v2.0
   // test: 'http://192.168.1.181:30726' // v2-dev
   // test: 'http://192.168.1.126:3003/' // tm 重构
   // test: 'http://192.168.1.181:30474/' // v1-30
