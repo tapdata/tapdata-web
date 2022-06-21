f0e4d9a3bc086c0079c445b482a08e602c5a4ef5
kennen <liangzhikai@tapdata.io>
2022-06-21 18:58:22 +0800

    fix(国际化): 公共模块component

--- packages/component/src/locale/lang/zh-CN.js
+++ packages/component/src/locale/lang/zh-CN.js
@@ -3,5 +3,8 @@ export default {
   filter_placeholder: '请输入搜索内容',
   loading: '加载中',
   no_match: '无匹配数据',
-  no_data: '无数据'
+  no_data: '无数据',
+  function_type_option_custom: '自定义函数',
+  function_type_option_jar: '第三方jar包',
+  function_type_option_system: '系统函数'
 }
