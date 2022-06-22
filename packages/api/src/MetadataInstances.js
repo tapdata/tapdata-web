8a85a4c41b3c40903a70529061981547ae0792fd
xufei <xufei@tapdata.io>
2022-06-22 12:21:00 +0800

    fix(@tap/api): MetadataInstances 补上 getSourceTables方法

--- packages/api/src/MetadataInstances.js
+++ packages/api/src/MetadataInstances.js
@@ -80,6 +80,10 @@ export default class MetadataInstances extends Http {
     return this.axios.get(this.url + '/tables?connectionId=' + connectionId)
   }
 
+  getSourceTables(connectionId) {
+    return this.axios.get(this.url + '/tables?connectionId=' + connectionId + '&sourceType=SOURCE')
+  }
+
   /**
    * 主从合并获取父节点字段
    * @param taskId
