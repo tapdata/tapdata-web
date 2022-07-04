7f11e9390e5bea58b88c3dc1660edb01d5b82171
kennen <liangzhikai@tapdata.io>
2022-07-04 14:27:56 +0800

    feat(form模块): 添加text组件

---
+++ packages/form/src/components/text/index.js
@@ -0,0 +1,29 @@
+import { defineComponent } from '@vue/composition-api'
+
+export const Text = defineComponent({
+  name: 'Text',
+  props: ['content', 'className', 'contentStyle', 'icon', 'iconClass'],
+  setup(props) {
+    return () => {
+      const content = props.content
+      const className = props.className
+      const icon = props.icon
+      const iconClass = props.iconClass || 'color-primary mr-2 fs-7'
+      const style = Object.assign(
+        {
+          'white-space': 'pre',
+          color: '#535f72'
+        },
+        props.contentStyle || {}
+      )
+      return (
+        <div className={className} style={style}>
+          {icon && <i class={'el-icon-' + icon + ' ' + iconClass}></i>}
+          {content}
+        </div>
+      )
+    }
+  }
+})
+
+export default Text
