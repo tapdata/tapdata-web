0555c650959df638d3e2eab0d61fa61986f93997
xufei <xufei@tapdata.io>
2022-07-18 19:01:51 +0800

    feat(@tap/dag): dag观测增加节点的指标数据结构

--- packages/dag/src/components/monitor/Node.vue
+++ packages/dag/src/components/monitor/Node.vue
@@ -3,72 +3,91 @@
     <div class="node-card rounded-lg px-2 pt-6 mt-n3">
       <div class="node-card-content p-2">
         <div class="grid statistic-list">
-          <div v-if="taskType !== 'initial_sync' && !isProcessorNode" class="statistic">
+          <div v-if="taskType !== 'initial_sync' && isSource" class="statistic">
             <div class="statistic-title">增量时间点</div>
             <div class="statistic-content">
-              <div class="statistic-value">2022-12-01 12:12:12</div>
+              <div class="statistic-value">{{ cdcEventStartTime }}</div>
             </div>
           </div>
 
-          <div class="statistic">
+          <div v-if="taskType !== 'initial_sync' && isSource" class="statistic">
+            <!--增量耗时-->
             <div class="statistic-title">耗时</div>
             <div class="statistic-content">
-              <div class="statistic-value">123ms</div>
+              <div class="statistic-value">{{ sample.incrementalDelay }}ms</div>
             </div>
           </div>
 
-          <div class="statistic">
+          <div v-if="isTarget" class="statistic">
+            <!--写入-->
+            <div class="statistic-title">耗时</div>
+            <div class="statistic-content">
+              <div class="statistic-value">{{ sample.writeDelay }}ms</div>
+            </div>
+          </div>
+
+          <div v-if="isProcessorNode" class="statistic">
+            <!--处理-->
+            <div class="statistic-title">耗时</div>
+            <div class="statistic-content">
+              <div class="statistic-value">{{ sample.processDelay }}ms</div>
+            </div>
+          </div>
+
+          <div v-if="isTarget || isProcessorNode" class="statistic">
             <div class="statistic-title">累积输入事件</div>
             <div class="statistic-content">
-              <div class="statistic-value">666</div>
+              <div class="statistic-value">{{ sample.inputEventTotal }}</div>
             </div>
           </div>
 
-          <div class="statistic">
+          <div v-if="isSource || isProcessorNode" class="statistic">
             <div class="statistic-title">累积输出事件</div>
             <div class="statistic-content">
-              <div class="statistic-value">666</div>
+              <div class="statistic-value">{{ sample.outputEventTotal }}</div>
             </div>
           </div>
 
           <div class="statistic">
             <div class="statistic-title">QPS</div>
             <div class="statistic-content">
-              <div class="statistic-value">200</div>
+              <div class="statistic-value">{{ sample.qps }}</div>
             </div>
           </div>
         </div>
-        <div v-if="showStatus" class="grid statistic-list mt-2">
+        <div v-if="isSource" class="grid statistic-list mt-2">
           <div v-if="taskType !== 'cdc'" class="statistic">
             <div class="statistic-title">全量</div>
             <div class="statistic-content">
-              <Chart ref="chart" :extend="options" style="width: 80px; height: 80px"></Chart>
+              <Chart ref="chart" :extend="initialSyncOption" style="width: 80px; height: 80px"></Chart>
             </div>
           </div>
           <div v-if="taskType !== 'initial_sync'" class="statistic">
             <div class="statistic-title">增量</div>
             <div class="statistic-content">
-              <Chart ref="chart" :extend="options" style="width: 80px; height: 80px"></Chart>
+              <Chart ref="chart" :extend="cdcOption" style="width: 80px; height: 80px"></Chart>
             </div>
           </div>
         </div>
       </div>
       <div class="node-card-footer flex align-center justify-content-end">
-        <ElProgress v-if="taskType !== 'cdc' && showStatus" class="flex-1 mr-3" :show-text="false" :percentage="50" />
-        <VIcon size="16">menu</VIcon>
+        <ElTooltip v-if="taskType !== 'cdc' && isSource" :content="initialSyncProcessTip">
+          <ElProgress class="flex-1 mr-3" :show-text="false" :percentage="initialSyncProcess" />
+        </ElTooltip>
+
+        <button @click="$emit('open-detail')" class="icon-btn">
+          <VIcon size="16">menu</VIcon>
+        </button>
       </div>
     </div>
   </DFNode>
 </template>
 
 <script>
-import { mapGetters, mapMutations, mapState } from 'vuex'
-import deviceSupportHelpers from 'web-core/mixins/deviceSupportHelpers'
-import { sourceEndpoint, targetEndpoint } from '../style'
-import { NODE_PREFIX } from '../constants'
 import DFNode from '../DFNode'
 import { Chart } from '@tap/component'
 import VIcon from 'web-core/components/VIcon'
+import dayjs from 'dayjs'
 
 export default {
   name: 'Node',
@@ -80,95 +99,62 @@ export default {
 
   props: {
     node: {},
+    sample: {
+      type: Object,
+      default: () => ({
+        cdcEventStartTime: 1658117353136,
+        inputEventTotal: 996,
+        outputEventTotal: 996,
+        incrementalDelay: 600,
+        writeDelay: 700,
+        processDelay: 500,
+        qps: 888,
+        initialSyncState: {
+          wait: 60,
+          process: 100,
+          success: 120,
+          error: 15
+        },
+        cdcState: {
+          wait: 160,
+          process: 100,
+          success: 120,
+          error: 15
+        },
+        milestone: {
+          finish: 10,
+          total: 100,
+          planTime: 100
+        }
+      })
+    },
     taskType: String
   },
 
   data() {
     return {
       id: this.$attrs.id,
-      options: {
-        tooltip: {
-          trigger: 'item'
+      seriesItemMap: {
+        wait: {
+          name: '待进行',
+          color: '#F7D762'
         },
-        legend: {
-          show: false
+        process: {
+          name: '进行中',
+          color: '#88DBDA'
         },
-        series: [
-          {
-            type: 'pie',
-            radius: ['50%', '80%'],
-            center: ['50%', '50%'],
-            label: { show: false },
-            labelLine: { show: false },
-            data: [
-              {
-                name: '待进行',
-                value: 10,
-                color: '#F7D762'
-              },
-              {
-                name: '无需创建',
-                value: 20,
-                color: '#88DBDA'
-              },
-              {
-                name: '已完成',
-                value: 30,
-                color: '#82C647'
-              },
-              {
-                name: '错误',
-                value: 40,
-                color: '#EC8181'
-              }
-            ]
-          }
-        ]
+        success: {
+          name: '已完成',
+          color: '#82C647'
+        },
+        error: {
+          name: '错误',
+          color: '#EC8181'
+        }
       },
-      syncData: [
-        {
-          name: '待进行',
-          value: 10,
-          color: '#F7D762'
-        },
-        {
-          name: '无需创建',
-          value: 20,
-          color: '#88DBDA'
-        },
-        {
-          name: '已完成',
-          value: 30,
-          color: '#82C647'
-        },
-        {
-          name: '错误',
-          value: 40,
-          color: '#EC8181'
-        }
-      ],
-      cdcData: [
-        {
-          name: '待进行',
-          value: 10,
-          color: '#F7D762'
-        },
-        {
-          name: '无需创建',
-          value: 20,
-          color: '#88DBDA'
-        },
-        {
-          name: '已完成',
-          value: 30,
-          color: '#82C647'
-        },
-        {
-          name: '错误',
-          value: 40,
-          color: '#EC8181'
-        }
-      ]
+
+      initialSyncOption: this.getPieOption(),
+      cdcOption: this.getPieOption()
     }
   },
 
@@ -186,9 +172,83 @@ export default {
       return type !== 'database' && type !== 'table'
     },
 
+    isSource() {
+      const { type, $inputs } = this.node
+      return (type === 'database' || type === 'table') && !$inputs.length
+    },
+
+    isTarget() {
+      const { type, $outputs } = this.node
+      return (type === 'database' || type === 'table') && !$outputs.length
+    },
+
     showStatus() {
       const { type, $inputs } = this.node
       return (type === 'database' || type === 'table') && !$inputs.length
+    },
+
+    cdcEventStartTime() {
+      return dayjs(this.sample.cdcEventStartTime).format('YYYY-MM-DD HH:mm:ss')
+    },
+
+    initialSyncProcess() {
+      const { milestone } = this.sample
+      return Math.round((milestone.finish / milestone.total) * 100)
+    },
+
+    initialSyncProcessTip() {
+      const { milestone } = this.sample
+      return ` ${milestone.finish}/${milestone.total} | 预计全量完成还需 ${milestone.planTime}ms`
+    }
+  },
+
+  watch: {
+    'sample.initialSyncState': {
+      immediate: true,
+      handler(v) {
+        this.seriesHandler(this.initialSyncOption, v)
+      }
+    },
+
+    'sample.cdcState': {
+      immediate: true,
+      handler(v) {
+        this.seriesHandler(this.cdcOption, v)
+      }
+    }
+  },
+
+  methods: {
+    getPieOption() {
+      return {
+        tooltip: {},
+        legend: {
+          show: false
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: ['50%', '80%'],
+            center: ['50%', '50%'],
+            label: { show: false },
+            labelLine: { show: false },
+            data: []
+          }
+        ]
+      }
+    },
+
+    seriesHandler(option, data) {
+      const { seriesItemMap } = this
+      option.series[0].data = Object.entries(data).map(([key, value]) => {
+        return {
+          value,
+          name: seriesItemMap[key].name,
+          itemStyle: {
+            color: seriesItemMap[key].color
+          }
+        }
+      })
     }
   }
 }
@@ -229,6 +289,27 @@ export default {
       font-family: DIN;
       white-space: nowrap;
     }
+  }
+}
+
+.icon-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 24px;
+  height: 24px;
+  color: #4e5969;
+  background: #fff;
+  outline: none;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  transition: background, color 0.3s cubic-bezier(0.25, 0.8, 0.5, 1);
+  cursor: pointer;
+
+  &.active,
+  &:hover {
+    color: map-get($color, primary);
+    background: #eef3ff;
   }
 }
 </style>
