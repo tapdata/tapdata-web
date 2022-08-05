e95f299b47a90429fd60dea9f84441c387fe1917
payne <payen@tapdata.io>
2022-08-05 11:19:28 +0800

    fix: Fix the bug of task status error

--- packages/business/src/shared/task.js
+++ packages/business/src/shared/task.js
@@ -25,6 +25,7 @@ export function getSubTaskStatus(rows = []) {
     if (Object.keys(tempMap).length === 1) {
       let status = rows[0]?.status
       status = status === 'edit' ? 'ready' : status
+      status = status === 'schedule_failed' ? 'error' : status
       result = [
         Object.assign({ count: 1 }, ETL_SUB_STATUS_MAP[status], {
           status: status
