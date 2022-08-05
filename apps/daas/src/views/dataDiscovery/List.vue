a1b720ff7ae4f34914b738ed64e02d621e4152ad
fannie <fannie@tapdata.io>
2022-08-05 16:18:25 +0800

    feat(discovery): add discovery preview

--- apps/daas/src/views/dataDiscovery/List.vue
+++ apps/daas/src/views/dataDiscovery/List.vue
@@ -14,7 +14,7 @@
       <el-table-column min-width="110" :label="$t('object_list_source_type')"></el-table-column>
       <el-table-column min-width="210" :label="$t('object_list_source_information')"> </el-table-column>
     </TablePage>
-    <Drawer :width="'800px'" :visible.sync="isShowDetails">
+    <Drawer class="object-drawer-wrap" :width="'800px'" :visible.sync="isShowDetails">
       <div class="flex align-items-center ml-4">
         <header class="font-weight-bold mr-4">
           <span class="drawer__header_text inline-block">对象详情</span>
@@ -32,35 +32,107 @@
           </el-select>
         </div>
         <div class="details_data_info mt-4 p-5">
-          <el-row>
+          <el-row class="mt-2">
             <el-col
               ><span class="drawer__header_text inline-block">数据表</span><span class="ml-2">Personinfo</span></el-col
             >
           </el-row>
-          <el-row>
-            <el-col :span="8"><span class="w-30">创建时间</span><span class="ml-2">2022-06-08</span></el-col>
-            <el-col :span="8"><span class="w-30">变更时间</span><span class="ml-2">2022-06-08</span></el-col>
-            <el-col :span="8"><span class="w-30">数据项</span><span class="ml-2">7</span></el-col>
+          <el-row class="mt-2">
+            <el-col :span="8"
+              ><span class="max-label inline-block">创建时间</span><span class="ml-2">2022-06-08</span></el-col
+            >
+            <el-col :span="8"
+              ><span class="max-label inline-block">变更时间</span><span class="ml-2">2022-06-08</span></el-col
+            >
+            <el-col :span="8"><span class="max-label inline-block">数据项</span><span class="ml-2">7</span></el-col>
           </el-row>
-          <el-row>
-            <el-col :span="8"><span class="w-30">数据量</span><span class="ml-2">1000</span></el-col>
-            <el-col :span="8"><span class="w-30">来源信息</span><span class="ml-2">无</span></el-col>
-            <el-col :span="8"><span class="w-30">来源类型</span><span class="ml-2">数据连接</span></el-col>
+          <el-row class="mt-2">
+            <el-col :span="8"><span class="max-label inline-block">数据量</span><span class="ml-2">1000</span></el-col>
+            <el-col :span="8"><span class="max-label inline-block">来源信息</span><span class="ml-2">无</span></el-col>
+            <el-col :span="8"
+              ><span class="max-label inline-block">来源类型</span><span class="ml-2">数据连接</span></el-col
+            >
           </el-row>
-          <el-row>
-            <el-col :span="8"><span class="w-30">连接名</span><span class="ml-2">fannie_test</span></el-col>
-            <el-col :span="8"><span class="w-30">连接类型</span><span class="ml-2">Orcale</span></el-col>
-            <el-col :span="8"><span class="w-30">连接描述</span><span class="ml-2">地址、端口</span></el-col>
+          <el-row class="mt-2">
+            <el-col :span="8"
+              ><span class="max-label inline-block">连接名</span><span class="ml-2">fannie_test</span></el-col
+            >
+            <el-col :span="8"
+              ><span class="max-label inline-block">连接类型</span><span class="ml-2">Orcale</span></el-col
+            >
+            <el-col :span="8"
+              ><span class="max-label inline-block">连接描述</span><span class="ml-2">地址、端口</span></el-col
+            >
           </el-row>
-          <el-row>
-            <el-col :span="8"><span class="w-30">业务名称</span><span class="ml-2">1234</span></el-col>
+          <el-row class="mt-2">
+            <el-col :span="8"
+              ><span class="max-label inline-block">业务名称</span><span class="ml-2">1234</span></el-col
+            >
           </el-row>
-          <el-row>
-            <el-col :span="8"><span class="w-30">业务描述</span><span class="ml-2">3456</span></el-col>
+          <el-row class="mt-2">
+            <el-col :span="8"
+              ><span class="max-label inline-block">业务描述</span><span class="ml-2">3456</span></el-col
+            >
           </el-row>
         </div>
         <div class="mt-4">
           <span class="drawer__header_text inline-block">数据项</span>
+          <TableList :columns="columns" :hide-on-single-page="true"> </TableList>
+        </div>
+      </div>
+      <div class="ml-4" v-if="activeName === 'second'">
+        <div class="user">
+          <span class="mr-4">管理员</span>
+          <el-select v-model="activeUser">
+            <el-option label="admin" value="admin"></el-option>
+          </el-select>
+        </div>
+        <div class="details_data_info mt-4 p-5">
+          <el-row class="mt-2">
+            <el-col
+              ><span class="drawer__header_text inline-block">数据表</span><span class="ml-2">Personinfo</span></el-col
+            >
+          </el-row>
+          <el-row class="mt-2">
+            <el-col :span="8"
+              ><span class="max-label inline-block">创建时间</span><span class="ml-2">2022-06-08</span></el-col
+            >
+            <el-col :span="8"
+              ><span class="max-label inline-block">变更时间</span><span class="ml-2">2022-06-08</span></el-col
+            >
+            <el-col :span="8"><span class="max-label inline-block">数据项</span><span class="ml-2">7</span></el-col>
+          </el-row>
+          <el-row class="mt-2">
+            <el-col :span="8"><span class="max-label inline-block">数据量</span><span class="ml-2">1000</span></el-col>
+            <el-col :span="8"><span class="max-label inline-block">来源信息</span><span class="ml-2">无</span></el-col>
+            <el-col :span="8"
+              ><span class="max-label inline-block">来源类型</span><span class="ml-2">数据连接</span></el-col
+            >
+          </el-row>
+          <el-row class="mt-2">
+            <el-col :span="8"
+              ><span class="max-label inline-block">连接名</span><span class="ml-2">fannie_test</span></el-col
+            >
+            <el-col :span="8"
+              ><span class="max-label inline-block">连接类型</span><span class="ml-2">Orcale</span></el-col
+            >
+            <el-col :span="8"
+              ><span class="max-label inline-block">连接描述</span><span class="ml-2">地址、端口</span></el-col
+            >
+          </el-row>
+          <el-row class="mt-2">
+            <el-col :span="8"
+              ><span class="max-label inline-block">业务名称</span><span class="ml-2">1234</span></el-col
+            >
+          </el-row>
+          <el-row class="mt-2">
+            <el-col :span="8"
+              ><span class="max-label inline-block">业务描述</span><span class="ml-2">3456</span></el-col
+            >
+          </el-row>
+        </div>
+        <div class="mt-4">
+          <span class="drawer__header_text inline-block">数据预览</span>
           <TableList :columns="columns" :hide-on-single-page="true"> </TableList>
         </div>
       </div>
@@ -203,14 +275,19 @@ export default {
 .object-list-wrap {
   height: 100%;
 }
-.drawer__header_text {
-  height: 22px;
-  font-size: 14px;
-  line-height: 7px;
-  font-weight: 500;
-}
-.details_data_info {
-  background: #fafafa;
-  border-radius: 4px;
+.object-drawer-wrap {
+  .drawer__header_text {
+    height: 22px;
+    font-size: 14px;
+    line-height: 7px;
+    font-weight: 500;
+  }
+  .max-label {
+    width: 50px;
+  }
+  .details_data_info {
+    background: #fafafa;
+    border-radius: 4px;
+  }
 }
 </style>
