edf978725a8b7205a6f3d35cb9c829c644da2ce3
xufei <xufei@tapdata.io>
2022-08-11 17:26:45 +0800

    fix(@tap/dag): fix sync task goto monitor

--- packages/dag/src/Editor.vue
+++ packages/dag/src/Editor.vue
@@ -658,7 +658,7 @@ export default {
 
     handleDetail() {
       this.$router.push({
-        name: 'dataflowDetails',
+        name: 'dataflowStatistics',
         params: {
           id: this.dataflow.id
         }
