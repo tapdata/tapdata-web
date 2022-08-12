b160bba935e91473b76514aea6a8f68f3e3e8654
xufei <xufei@tapdata.io>
2022-08-12 10:21:57 +0800

    feat(@tap/component): js editor add before/after props

--- packages/form/src/components/JsEditor.js
+++ packages/form/src/components/JsEditor.js
@@ -1,10 +1,13 @@
 import { JsEditor as _JsEditor } from '@tap/component'
 import { connect, mapProps } from '@formily/vue'
+import { HighlightCode } from './highlight-code'
 
 export const JsEditor = connect(
   {
     props: {
       value: String,
+      before: String,
+      after: String,
       height: {
         type: [String, Number],
         default: 200
@@ -31,9 +34,34 @@ export const JsEditor = connect(
         ...this.options,
         readOnly: this.disabled
       }
-      return (
+      return this.before || this.after ? (
+        <div class="code-editor border rounded-2 overflow-hidden">
+          <div class="code-before">
+            <HighlightCode class="m-0" code={this.before} />
+          </div>
+          <_JsEditor
+            class="border rounded-2 py-0"
+            style={{
+              background: '#fff'
+            }}
+            theme="chrome"
+            value={this.value}
+            lang="javascript"
+            height={this.height}
+            onBlur={this.onBlur}
+            options={options}
+          />
+          <div class="code-after">
+            <HighlightCode code={this.after} />
+          </div>
+        </div>
+      ) : (
         <_JsEditor
           class="border rounded-2"
+          style={{
+            background: '#fff'
+          }}
+          theme="chrome"
           value={this.value}
           lang="javascript"
           height={this.height}
