9599884b9a988f2358c730bc0bf34d8f8543cf42
xufei <xufei@tapdata.io>
2022-08-24 15:16:03 +0800

    fix(@tap/dag): Adjust sync task JavaScript node configuration layout

--- packages/dag/src/nodes/JavaScript.js
+++ packages/dag/src/nodes/JavaScript.js
@@ -15,48 +15,37 @@ export class JavaScript extends NodeType {
   formSchema = {
     type: 'object',
     properties: {
-      grid: {
-        type: 'void',
-        'x-component': 'FormGrid',
+      script: {
+        title: '脚本',
+        type: 'string',
+        required: true,
+        default: 'function process(record){\n\t// Enter you code at here\n\treturn record;\n}',
+        'x-decorator': 'FormItem',
+        'x-decorator-props': { gridSpan: 2 },
+        'x-component': 'JsEditor',
         'x-component-props': {
-          columnGap: 16,
-          minColumns: 3,
-          maxColumns: 3
-        },
-        properties: {
-          script: {
-            title: '脚本',
-            type: 'string',
-            required: true,
-            default: 'function process(record){\n\t// Enter you code at here\n\treturn record;\n}',
-            'x-decorator': 'FormItem',
-            'x-decorator-props': { gridSpan: 2 },
-            'x-component': 'JsEditor',
-            'x-component-props': {
-              height: 500,
-              options: { showPrintMargin: false, wrap: false },
-              includeBeforeAndAfter: true,
-              before: 'function process(record){',
-              beforeRegexp: '^[^]*function\\s+process\\s*\\(record\\)\\{',
-              afterRegexp: '}[^}]*$',
-              after: '}'
-            }
-          },
-          declareScript: {
-            title: '模型声明',
-            type: 'string',
-            default: '',
-            'x-decorator': 'FormItem',
-            'x-decorator-props': { gridSpan: 1 },
-            'x-component': 'JsEditor',
-            'x-component-props': {
-              height: 500,
-              options: { showPrintMargin: false, wrap: false },
-              before: 'function declare(tapTable) {',
-              after: `  return tapTable\n}`,
-              handleAddCompleter: '{{addDeclaredCompleterForSync}}'
-            }
-          }
+          height: 500,
+          options: { showPrintMargin: false, wrap: false },
+          includeBeforeAndAfter: true,
+          before: 'function process(record){',
+          beforeRegexp: '^[^]*function\\s+process\\s*\\(record\\)\\{',
+          afterRegexp: '}[^}]*$',
+          after: '}'
+        }
+      },
+      declareScript: {
+        title: '模型声明',
+        type: 'string',
+        default: '',
+        'x-decorator': 'FormItem',
+        'x-decorator-props': { gridSpan: 1 },
+        'x-component': 'JsEditor',
+        'x-component-props': {
+          height: 300,
+          options: { showPrintMargin: false, wrap: false },
+          before: 'function declare(tapTable) {',
+          after: `  return tapTable\n}`,
+          handleAddCompleter: '{{addDeclaredCompleterForSync}}'
         }
       }
     }
