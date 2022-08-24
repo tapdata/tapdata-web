fabb99a49d465a74531141faab77e9dae0aaee39
xufei <xufei@tapdata.io>
2022-08-24 15:45:00 +0800

    fix(@tap/dag): Adjust sync task Aggregate node configuration layout

--- packages/dag/src/nodes/Aggregate.js
+++ packages/dag/src/nodes/Aggregate.js
@@ -25,7 +25,7 @@ export class Aggregate extends NodeType {
         'x-reactions': [
           '{{useAsyncDataSourceByConfig({service: loadNodeFieldOptions, fieldName: "value", withoutField: true}, $values.$inputs[0])}}',
           {
-            target: '*(primaryKeys,aggregations.*.aggExpression,aggregations.*.groupByExpression)',
+            target: '*(aggregations.*.aggExpression,aggregations.*.groupByExpression)',
             fulfill: {
               state: {
                 dataSource: '{{$self.value}}',
@@ -35,184 +35,97 @@ export class Aggregate extends NodeType {
           }
         ]
       },
-      grid: {
-        type: 'void',
-        'x-component': 'FormGrid',
-        'x-component-props': {
-          minColumns: 2,
-          columnGap: 16
-        },
+
+      aggregations: {
+        type: 'array',
+        display: 'none',
+        default: [
+          {
+            filterPredicate: '',
+            aggFunction: 'COUNT',
+            aggExpression: '',
+            groupByExpression: []
+          }
+        ]
+      },
+
+      'aggregations.0': {
+        type: 'object',
         properties: {
-          wrap: {
+          row: {
             type: 'void',
-            'x-component': 'FormGrid.GridColumn',
+            'x-component': 'FormGrid',
             properties: {
-              // primaryKeys: {
-              //   title: '主键',
-              //   type: 'array',
-              //   required: true,
-              //   'x-decorator': 'FormItem',
-              //   'x-component': 'FieldSelect',
-              //   'x-component-props': {
-              //     multiple: true
-              //   }
-              // },
-              aggregations: {
-                type: 'array',
-                'x-decorator': 'FormItem',
-                'x-component': 'ArrayCards',
-                'x-component-props': {
-                  title: '聚合'
-                },
-                default: [
+              aggFunction: {
+                type: 'string',
+                title: '聚合函数',
+                enum: [
                   {
-                    // name: 'COUNT',
-                    filterPredicate: '',
-                    aggFunction: 'COUNT',
-                    aggExpression: '',
-                    groupByExpression: []
+                    label: 'AVG',
+                    value: 'AVG'
+                  },
+                  {
+                    label: 'SUM',
+                    value: 'SUM'
+                  },
+                  {
+                    label: 'MAX',
+                    value: 'MAX'
+                  },
+                  {
+                    label: 'MIN',
+                    value: 'MIN'
+                  },
+                  {
+                    label: 'COUNT',
+                    value: 'COUNT'
                   }
                 ],
-                items: {
-                  type: 'object',
-                  properties: {
-                    index: {
-                      type: 'void',
-                      'x-component': 'ArrayCards.Index'
-                    },
-                    remove: {
-                      type: 'void',
-                      'x-component': 'ArrayCards.Remove',
-                      'x-reactions': {
-                        dependencies: ['aggregations'],
-                        fulfill: {
-                          state: {
-                            visible: `{{$deps[0].length > 1}}`
-                          }
-                        }
-                      }
-                    },
-                    row: {
-                      type: 'void',
-                      'x-component': 'FormGrid',
-                      properties: {
-                        aggFunction: {
-                          type: 'string',
-                          title: '聚合函数',
-                          enum: [
-                            {
-                              label: 'AVG',
-                              value: 'AVG'
-                            },
-                            {
-                              label: 'SUM',
-                              value: 'SUM'
-                            },
-                            {
-                              label: 'MAX',
-                              value: 'MAX'
-                            },
-                            {
-                              label: 'MIN',
-                              value: 'MIN'
-                            },
-                            {
-                              label: 'COUNT',
-                              value: 'COUNT'
-                            }
-                          ],
-                          required: true,
-                          'x-decorator': 'FormItem',
-                          'x-component': 'Select'
-                        },
-                        aggExpression: {
-                          type: 'string',
-                          title: '作用目标',
-                          required: true,
-                          'x-decorator': 'FormItem',
-                          'x-decorator-props': { gridSpan: 2 },
-                          'x-component': 'FieldSelect',
-                          'x-component-props': {
-                            allowCreate: true
-                          },
-                          'x-reactions': [
-                            {
-                              dependencies: ['.aggFunction'],
-                              fulfill: {
-                                state: {
-                                  value: `{{$deps[0]==='COUNT' ? null:'1'}}`,
-                                  disabled: `{{$deps[0]==='COUNT'}}`
-                                }
-                              }
-                            }
-                          ]
-                        }
-                      }
-                    },
-
-                    // name: {
-                    //   type: 'string',
-                    //   title: '子处理名称',
-                    //   required: true,
-                    //   'x-decorator': 'FormItem',
-                    //   'x-decorator-props': {
-                    //     tooltip: `后续节点的脚本编辑需要引用此子处理的名称进行指定的数据处理，\n故不同的子处理名称不可重复。`
-                    //   },
-                    //   'x-component': 'Input',
-                    //   'x-reactions': [
-                    //     {
-                    //       dependencies: ['.aggFunction'],
-                    //       fulfill: {
-                    //         state: {
-                    //           value: `{{$deps[0]+($index > 0 ? '_'+($index):'')}}`
-                    //         }
-                    //       }
-                    //     }
-                    //   ]
-                    // },
-                    // filterPredicate: {
-                    //   title: '过滤器',
-                    //   'x-decorator': 'FormItem',
-                    //   'x-component': 'Input.TextArea'
-                    // },
-                    groupByExpression: {
-                      title: '分组字段',
-                      type: 'array',
-                      'x-decorator': 'FormItem',
-                      'x-component': 'FieldSelect',
-                      'x-component-props': {
-                        multiple: true
+                required: true,
+                'x-decorator': 'FormItem',
+                'x-component': 'Select'
+              },
+              aggExpression: {
+                type: 'string',
+                title: '作用目标',
+                required: true,
+                'x-decorator': 'FormItem',
+                'x-decorator-props': { gridSpan: 2 },
+                'x-component': 'FieldSelect',
+                'x-component-props': {
+                  allowCreate: true
+                },
+                'x-reactions': [
+                  {
+                    dependencies: ['.aggFunction'],
+                    fulfill: {
+                      state: {
+                        value: `{{$deps[0]==='COUNT' ? null:'1'}}`,
+                        disabled: `{{$deps[0]==='COUNT'}}`
                       }
                     }
                   }
-                }
-                // properties: {
-                //   addition: {
-                //     type: 'void',
-                //     title: '添加聚合',
-                //     'x-component': 'ArrayCards.Addition',
-                //     'x-component-props': {
-                //       plain: true,
-                //       defaultValue: {
-                //         filterPredicate: '',
-                //         aggFunction: 'COUNT',
-                //         // name: '',
-                //         aggExpression: '',
-                //         groupByExpression: []
-                //       }
-                //     }
-                //   }
-                // }
+                ]
               }
             }
           },
-          return: {
-            title: '返回示例',
-            type: 'void',
+          groupByExpression: {
+            title: '分组字段',
+            type: 'array',
             'x-decorator': 'FormItem',
-            'x-component': 'ArrayAggregate'
+            'x-component': 'FieldSelect',
+            'x-component-props': {
+              multiple: true
+            }
           }
         }
+      },
+
+      return: {
+        title: '返回示例',
+        type: 'void',
+        'x-decorator': 'FormItem',
+        'x-component': 'ArrayAggregate'
       }
     }
   }
