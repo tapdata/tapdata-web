85fb3918c2109afc8cad53a1fdbfb1ef4a81734c
StormKennen <317052920@qq.com>
2022-08-30 12:22:36 +0800

    fix(@tap/dag-monitor): stopTime update

--- packages/dag/src/mixins/editor.js
+++ packages/dag/src/mixins/editor.js
@@ -491,6 +491,7 @@ export default {
       this.$set(this.dataflow, 'status', data.status)
       this.$set(this.dataflow, 'disabledData', data.btnDisabled)
       this.$set(this.dataflow, 'taskRecordId', data.taskRecordId)
+      this.$set(this.dataflow, 'stopTime', data.stopTime)
 
       if (!fromWS) {
         Object.keys(data).forEach(key => {
