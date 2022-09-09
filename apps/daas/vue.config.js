6753f35ebc6ce8dee786c09a409f98b8588c1747
payne <payen@tapdata.io>
2022-09-09 17:27:09 +0800

    fix: 修复创建连接不显示外存配置的问题

--- apps/daas/vue.config.js
+++ apps/daas/vue.config.js
@@ -6,7 +6,7 @@ const serveUrlMap = {
   mock: 'http://localhost:30300',
   dev: 'http://localhost:3000', // TM端本地默认地址
   jet: 'http://jet.devops.tapdata.net:31613',
-  test: 'http://139.198.127.204:30282' // v2.0
+  test: 'http://192.168.1.30:8080' // v2.0
 }
 let origin
 const { argv } = process
