09b181a47946b0f43370a1c97b0eec681647c04f
StormKennen <317052920@qq.com>
2022-09-15 11:43:20 +0800

    fix(@tap/dag/Record): not disabled

--- packages/dag/src/components/monitor/components/Record.vue
+++ packages/dag/src/components/monitor/components/Record.vue
@@ -15,7 +15,7 @@
         <TaskStatus :task="scope.row" />
       </template>
       <template slot="operation" slot-scope="scope">
-        <div class="operate-columns" v-if="dataflow.taskRecordId !== scope.row.taskRecordId">
+        <div class="operate-columns">
           <ElButton size="mini" type="text" @click="handleDetail(scope.row)">{{
             $t('packages_dag_components_record_xiangqing')
           }}</ElButton>
@@ -31,6 +31,7 @@ import i18n from '@tap/i18n'
 import { VTable } from '@tap/component'
 import { taskApi } from '@tap/api'
 import { TaskStatus } from '@tap/business'
+import { openUrl } from '@tap/shared'
 
 export default {
   name: 'Record',
@@ -104,7 +105,7 @@ export default {
       const { taskId, taskRecordId, startDate, endDate } = row
       const start = new Date(startDate).getTime()
       const end = new Date(endDate).getTime()
-      this.$router.push({
+      const routeUrl = this.$router.resolve({
         name: 'MigrationMonitorViewer',
         params: {
           id: taskId
@@ -115,9 +116,8 @@ export default {
           end
         }
       })
+      openUrl(routeUrl.href)
     }
   }
 }
 </script>
-
-<style scoped></style>
