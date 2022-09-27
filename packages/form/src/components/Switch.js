558b6b930d63085ee64afe75384247bbbfcc49cf
xufei <xufei@tapdata.io>
2022-09-27 17:02:12 +0800

    refactor: Clean up useless code

--- packages/form/src/components/Switch.js
+++
@@ -1,7 +0,0 @@
-import { connect, mapProps, mapReadPretty } from '@formily/vue'
-import { getComponentByTag } from './utils'
-import PreviewText from 'web-core/components/form/preview-text'
-
-const ElSwitch = getComponentByTag('el-switch')
-
-export const Switch = connect(ElSwitch, mapProps({ readOnly: 'readonly' }), mapReadPretty(PreviewText.Switch))
--- packages/form/src/components/Transfer.js
+++
@@ -1,6 +0,0 @@
-import { connect, mapProps } from '@formily/vue'
-import { getComponentByTag } from './utils'
-
-const ElTransfer = getComponentByTag('el-transfer')
-
-export const Transfer = connect(ElTransfer, mapProps({ dataSource: 'data' }))
