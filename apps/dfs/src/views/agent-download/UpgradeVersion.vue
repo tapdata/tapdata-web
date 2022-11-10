6e5b1feb79f5018a0241027b61f3237761de890c
fannie <fannie@tapdata.io>
2022-11-10 16:42:50 +0800

    feat(agent): upgrade aliComputeNest

--- apps/dfs/src/views/agent-download/UpgradeVersion.vue
+++ apps/dfs/src/views/agent-download/UpgradeVersion.vue
@@ -79,6 +79,34 @@
         </div>
         <div>{{ $t('agent_upgrade_step_linux_third') }}</div>
       </div>
+      <!--   AliComputenest   -->
+      <div v-else-if="downLoadType === 'AliComputenest'" class="content-container">
+        <div class="py-2 text-style">{{ $t('agent_upgrade_step_title') }}</div>
+        <div>1. 登录到安装了Tapdata Agent的阿里云ECS实例</div>
+        <div>2. 进入原Agent安装目录下，计算巢会自动安装在/data/tapdata下面</div>
+        <div v-if="agentId">
+          <div class="box title-text">
+            <span class="com-url">{{ comUrl }}</span>
+            <ElTooltip
+              placement="top"
+              manual
+              :content="$t('agent_deploy_upgrade_button_copied')"
+              popper-class="copy-tooltip"
+              :value="showTooltip"
+            >
+              <span
+                class="operaKey"
+                v-clipboard:copy="comUrl"
+                v-clipboard:success="onCopy"
+                @mouseleave="showTooltip = false"
+              >
+                <i class="click-style">{{ $t('agent_deploy_upgrade_button_copy') }}</i>
+              </span>
+            </ElTooltip>
+          </div>
+        </div>
+        <div>{{ $t('agent_upgrade_step_linux_third') }}</div>
+      </div>
       <!--   Docker   -->
       <div v-else-if="downLoadType === 'Docker'" class="content-container">
         <div class="py-2 text-style">{{ $t('agent_upgrade_step_title') }}</div>
@@ -138,7 +166,8 @@ export default {
       downType: [
         { name: 'Linux (64 bit)', value: 'Linux' },
         { name: 'Docker', value: 'Docker' },
-        { name: 'Windows (64 bit)', value: 'windows' }
+        { name: 'Windows (64 bit)', value: 'windows' },
+        { name: '阿里云计算巢', value: 'AliComputenest' }
       ],
       showTooltip: false,
       agentId: '',
@@ -155,6 +184,7 @@ export default {
       let map = {
         windows: `tapdata start backend --downloadUrl ${downloadUrl} --token ${token}`,
         Linux: `./tapdata stop agent && rm -f tapdata-bak && mv tapdata tapdata-bak && rm -f .tapdata-agent && wget "${downloadUrl}tapdata" && chmod +x tapdata && ./tapdata start backend --downloadUrl ${downloadUrl} --token ${token}`,
+        AliComputenest: `./tapdata stop agent && rm -f tapdata-bak && mv tapdata tapdata-bak && rm -f .tapdata-agent && wget "${downloadUrl}tapdata" && chmod +x tapdata && ./tapdata start backend --downloadUrl ${downloadUrl} --token ${token}`,
         Docker: `./tapdata stop agent && rm -f tapdata-bak && mv tapdata tapdata-bak && rm -f .tapdata-agent && wget "${downloadUrl}tapdata" && chmod +x tapdata && ./tapdata start backend --downloadUrl ${downloadUrl} --token ${token}`
       }
       return map[this.downLoadType]
