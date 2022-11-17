d4274386fddfb03b4b084f853b090b77f806f5ba
xufei <xufei@tapdata.io>
2022-11-17 14:59:42 +0800

    refactor(@tap/dag): The development task is compatible with js nodes

--- packages/dag/src/nodes/JavaScript.js
+++ packages/dag/src/nodes/JavaScript.js
@@ -30,27 +30,16 @@ export class JavaScript extends NodeType {
         title: '脚本',
         type: 'string',
         required: true,
-        default: 'function process(record){\n\t// Enter you code at here\n\treturn record;\n}',
-        'x-decorator': 'FormItem',
-        'x-decorator-props': { gridSpan: 2 },
-        'x-component': 'JsEditor',
+        default: 'function process(record){\n\n\t// Enter you code at here\n\treturn record;\n}',
+        'x-component': 'JsProcessor',
         'x-component-props': {
           height: 500,
-          showFullscreen: true,
           options: { showPrintMargin: false, wrap: false },
           includeBeforeAndAfter: true,
           before: 'function process(record){',
           beforeRegexp: '^[^]*function\\s+process\\s*\\(record\\)\\{',
           afterRegexp: '}[^}]*$',
-          after: '}'
-        }
-      },
-      declareScript: {
-        type: 'string',
-        'x-component': 'JsDeclare',
-        'x-component-props': {
-          height: 300,
-          options: { showPrintMargin: false, wrap: false },
+          after: '}',
           handleAddCompleter: '{{addDeclaredCompleterForSync}}'
         }
       }
