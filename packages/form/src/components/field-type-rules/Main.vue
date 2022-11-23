40f6b2dae45a289cf9ccc36308233ff9c7cae4c9
kennen <317052920@qq.com>
2022-11-23 12:08:31 +0800

    refactor(form/field-type-rules): change function name

--- packages/form/src/components/field-type-rules/Main.vue
+++ packages/form/src/components/field-type-rules/Main.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <ElLink type="primary" @click="batchFieldTypeForm.visible = true">批量修改字段类型</ElLink>
+    <ElLink type="primary" @click="form.visible = true">批量修改字段类型</ElLink>
     <ElDialog
       width="800px"
       append-to-body
       title="规则设置"
       custom-class="batch-field-type-maping-table-dialog"
-      :visible.sync="batchFieldTypeForm.visible"
+      :visible.sync="form.visible"
       :close-on-click-modal="false"
       :close-on-press-escape="false"
     >
@@ -17,7 +17,7 @@
           <ElCol :span="9">目标类型</ElCol>
           <ElCol :span="3" class="text-center">操作</ElCol>
         </ElRow>
-        <ElRow v-for="(item, index) in batchFieldTypeForm.list" :key="index" class="mt-4">
+        <ElRow v-for="(item, index) in form.list" :key="index" class="mt-4">
           <ElCol :span="9">
             <ElInput v-model="item.accept" placeholder="参考格式: varchar(32)"></ElInput>
           </ElCol>
@@ -31,17 +31,14 @@
           </ElCol>
           <ElCol :span="3">
             <div class="flex justify-content-center align-items-center" style="height: 32px">
-              <VIcon size="16" class="cursor-pointer hover-primary" @click="batchFieldTypeForm.list.splice(index, 1)"
+              <VIcon size="16" class="cursor-pointer hover-primary" @click="form.list.splice(index, 1)"
                 >minus-circle</VIcon
               >
             </div>
           </ElCol>
         </ElRow>
         <div class="mt-4">
-          <span
-            class="inline-flex align-items-center cursor-pointer"
-            @click="fieldTypeChangeAddItem(batchFieldTypeForm.list.length)"
-          >
+          <span class="inline-flex align-items-center cursor-pointer" @click="handleAdd(form.list.length)">
             <VIcon size="16" class="mr-1 cursor-pointer color-primary">plus-circle</VIcon>
             <span class="color-primary">添加</span>
           </span>
@@ -52,8 +49,8 @@
         </div>
       </div>
       <span slot="footer" class="dialog-footer">
-        <ElButton size="mini" @click="batchFieldTypeFormCancel">{{ $t('button_cancel') }}</ElButton>
-        <ElButton size="mini" type="primary" :disabled="batchFieldTypeFormDisabled()" @click="batchFieldTypeFormSave">{{
+        <ElButton size="mini" @click="handleCancel">{{ $t('button_cancel') }}</ElButton>
+        <ElButton size="mini" type="primary" :disabled="getSubmitDisabled()" @click="submit">{{
           $t('button_confirm')
         }}</ElButton>
       </span>
@@ -67,51 +64,32 @@ import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
 export default {
   name: 'Dialog',
 
-  props: ['visible'],
-
   computed: {
     ...mapGetters('dataflow', ['activeType', 'activeNode', 'nodeById', 'stateIsReadonly']),
-    ...mapState('dataflow', ['editVersion']),
-
-    icon() {
-      return this.getIcon(this.activeNode)
-    },
-
-    showPanel() {
-      return this.onlyNode ? this.activeType === 'node' : this.activeType
-    }
+    ...mapState('dataflow', ['editVersion'])
   },
 
   data() {
     return {
-      dialogVisible: false,
-      loadingSave: false,
-      list: [],
-      batchFieldTypeForm: {
+      form: {
         visible: false,
         list: [],
         options: []
       },
+      nodeRules: [],
       fieldRules: {}
     }
   },
 
   mounted() {
-    this.loadCustomTypeMappingsData()
+    this.loadData()
   },
 
   methods: {
     ...mapMutations('dataflow', ['updateNodeProperties']),
     ...mapActions('dataflow', ['updateDag']),
 
-    updateVisible() {
-      this.loadingSave = false
-      this.$emit('update:visible', false)
-    },
-    closeDialog() {
-      this.$emit('update:visible', false)
-    },
-    getBatchFieldTypeItem() {
+    getItem() {
       return {
         scope: 'Node',
         namespace: [this.activeNode?.id],
@@ -120,33 +98,33 @@ export default {
         result: ''
       }
     },
-    fieldTypeChangeAddItem(index = 0) {
-      this.batchFieldTypeForm.list.splice(index + 1, 0, this.getBatchFieldTypeItem())
+    handleAdd(index = 0) {
+      this.form.list.splice(index + 1, 0, this.getItem())
     },
-    loadCustomTypeMappingsData() {
+    loadData() {
       const fieldChangeRules = this.activeNode?.fieldChangeRules || {}
       const { nodeRules = [], fieldRules = {} } = fieldChangeRules
       this.fieldRules = fieldRules
-      this.list = JSON.parse(JSON.stringify(nodeRules))
-      this.batchFieldTypeForm.list = JSON.parse(JSON.stringify(nodeRules))
+      this.nodeRules = JSON.parse(JSON.stringify(nodeRules))
+      this.form.list = JSON.parse(JSON.stringify(nodeRules))
     },
-    batchFieldTypeFormCancel() {
-      this.loadCustomTypeMappingsData()
-      this.batchFieldTypeForm.visible = false
+    handleCancel() {
+      this.loadData()
+      this.form.visible = false
     },
-    batchFieldTypeFormDisabled() {
+    getSubmitDisabled() {
       return (
-        this.batchFieldTypeForm.list.some(t => !t.accept || !t.result) ||
-        JSON.stringify(this.list) === JSON.stringify(this.batchFieldTypeForm.list)
+        this.form.list.some(t => !t.accept || !t.result) ||
+        JSON.stringify(this.nodeRules) === JSON.stringify(this.form.list)
       )
     },
-    batchFieldTypeFormSave() {
-      this.batchFieldTypeForm.visible = false
+    submit() {
+      this.form.visible = false
       this.updateNodeProperties({
         id: this.activeNode?.id,
         properties: {
           fieldChangeRules: {
-            nodeRules: JSON.parse(JSON.stringify(this.batchFieldTypeForm.list)),
+            nodeRules: JSON.parse(JSON.stringify(this.form.list)),
             fieldRules: this.fieldRules
           }
         }
