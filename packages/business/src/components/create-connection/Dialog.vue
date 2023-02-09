796e2535eb27a2bb5bbe016f17356fbfa1e30465
kennen <317052920@qq.com>
2023-02-09 14:59:31 +0800

    feat(@business/components): add create-connection dir

---
+++ packages/business/src/components/create-connection/Dialog.vue
@@ -0,0 +1,196 @@
+<template>
+  <ElDialog
+    :title="title"
+    :visible="dialogVisible"
+    :append-to-body="true"
+    fullscreen
+    custom-class="connection-dialog"
+    :before-close="handleClose"
+  >
+    <div slot="title" class="flex align-items-center">
+      <VIcon class="mr-3">warning</VIcon>
+      <span class="font-color-normal">{{ title }}</span>
+    </div>
+    <div class="pl-7 pt-3">
+      <div class="mb-6 font-color-normal">
+        Choose a data source connector from below and configure the connection & credentials.
+      </div>
+      <div class="flex align-items-center mb-6">
+        <el-switch v-model="settings.showBeta"> </el-switch>
+        <span class="ml-3">Show Connectors in BETA State</span>
+        <el-switch v-model="settings.showAlpha" class="ml-6"> </el-switch>
+        <span class="ml-3">Show Connectors in ALPHA State</span>
+      </div>
+      <ConnectionSelector
+        :loading="loading"
+        :types="database"
+        :otherTypes="otherType"
+        :large="true"
+        @select="handleSelect"
+      ></ConnectionSelector>
+      <ConnectionForm :params="params"></ConnectionForm>
+    </div>
+  </ElDialog>
+</template>
+
+<script>
+import i18n from '@tap/i18n'
+
+import { databaseTypesApi } from '@tap/api'
+import ConnectionSelector from './Selector'
+import ConnectionForm from './Form'
+
+export default {
+  name: 'Dialog',
+  components: {
+    ConnectionSelector,
+    ConnectionForm
+  },
+  props: {
+    title: {
+      type: String,
+      default: () => {
+        return 'Choose Data Source Type' || i18n.t('connection_form_creat_connection')
+      }
+    },
+    dialogVisible: {
+      required: true,
+      value: Boolean
+    },
+    allwoType: {
+      value: Array,
+      default: () => {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      database: [],
+      otherType: [],
+      loading: true,
+      settings: {
+        showBeta: true,
+        showAlpha: true
+      },
+      params: {}
+    }
+  },
+  watch: {
+    dialogVisible(v) {
+      if (v) {
+        this.getDatabaseType()
+      }
+    }
+  },
+  created() {
+    this.getDatabaseType()
+  },
+  methods: {
+    handleClose() {
+      this.$emit('dialogVisible', false)
+      this.$emit('update:dialogVisible', false)
+    },
+    handleSelect(item) {
+      // this.$emit('databaseType', item)
+      console.log('handleSelect', item)
+    },
+    getDatabaseType() {
+      databaseTypesApi
+        .get()
+        .then(data => {
+          if (data) {
+            let items = data?.filter(t => !this.database.length || !this.database.some(d => d.pdkHash === t.pdkHash))
+            if (!items.length) {
+              return
+            }
+            this.database.push(...items)
+          }
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.database {
+  .title {
+    color: map-get($fontColor, slight);
+    margin-left: 20px;
+    margin-bottom: 20px;
+    display: inline-block;
+  }
+  .item {
+    li {
+      float: left;
+      margin-left: 20px;
+      margin-bottom: 20px;
+      text-align: center;
+    }
+    .img-box {
+      display: flex;
+      width: 120px;
+      height: 70px;
+      justify-content: center;
+      align-items: center;
+      background: map-get($bgColor, normal);
+      border: 1px solid #dedee4;
+      border-radius: 3px;
+      cursor: pointer;
+      img {
+        width: 35%;
+      }
+      &:hover {
+        box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.1);
+      }
+      .img-box__mask {
+        margin: -1px;
+        font-size: 13px;
+        background: rgba(0, 0, 0, 0.4);
+        .mask-text {
+          opacity: 0;
+          color: map-get($fontColor, white);
+          font-weight: bold;
+        }
+        &:hover {
+          .mask-text {
+            opacity: 1;
+          }
+        }
+      }
+    }
+    .content {
+      font-size: 12px;
+      margin-top: 5px;
+      max-width: 124px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      word-break: break-word;
+      overflow: hidden;
+    }
+  }
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: '';
+  }
+
+  .clearfix:after {
+    clear: both;
+  }
+
+  .clearfix {
+    *zoom: 1;
+  }
+}
+::v-deep {
+  .connection-dialog {
+    .el-dialog__body {
+      padding: 0 20px 30px 20px;
+    }
+  }
+}
+</style>
---
+++ packages/business/src/components/create-connection/Selector.vue
@@ -0,0 +1,233 @@
+<template>
+  <div class="database">
+    <ElTabs v-model="active">
+      <ElTabPane v-for="item in tabs" :key="item.value" :name="item.value" :label="item.label"></ElTabPane>
+    </ElTabs>
+    <div>
+      <ul v-loading="loading" class="database-ul overflow-auto">
+        <li
+          v-for="(item, index) in filterList"
+          :key="index"
+          class="database-item float-start mb-4 cursor-pointer"
+          :class="{ active: item.pdkId === selectType }"
+          @click="handleSelect(item)"
+        >
+          <div class="img-box rounded-3">
+            <ElImage v-if="item.pdkType" :src="getPdkIcon(item)">{{ item.pdkType }}</ElImage>
+            <ElImage v-else :src="$util.getConnectionTypeDialogImg(item)" />
+          </div>
+          <ElTooltip class="mt-2" effect="dark" :content="item.name" placement="bottom">
+            <div class="ellipsis text-center font-color-normal">{{ item.name }}</div>
+          </ElTooltip>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getConnectionIcon } from '@tap/business/src/views/connections/util'
+import { VIcon } from '@tap/component'
+import { cloneDeep } from 'lodash'
+
+export default {
+  name: 'ConnectionTypeSelector',
+  components: { VIcon },
+  props: {
+    types: {
+      value: Array,
+      default: () => {
+        return []
+      }
+    },
+    large: {
+      value: Boolean,
+      default: () => {
+        return false
+      }
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      active: '',
+      selectType: '',
+      comingTypes: [
+        { name: 'MongoDB', type: 'mongodb' },
+        { name: 'MySQL', type: 'mysql' },
+        { name: 'Oracle', type: 'oracle' },
+        { name: 'Elasticsearch', type: 'elasticsearch' },
+        { name: 'Redis', type: 'redis' },
+        { name: 'PostgreSQL', type: 'postgres' },
+        { name: 'SQL Server', type: 'sqlserver' },
+        // { name: 'GBase 8s', type: 'gbase-8s' },
+        { name: 'Sybase ASE', type: 'sybase ase' },
+        { name: 'GaussDB200', type: 'gaussdb200' },
+        { name: 'IBM Db2', type: 'db2' },
+        { name: 'Memory Cache', type: 'mem_cache' },
+        { name: 'KunDB', type: 'kundb' },
+        { name: 'Custom connection', type: 'custom' },
+        { name: 'REST API', type: 'rest api' },
+        { name: 'Dummy', type: 'dummy' },
+        { name: 'GridFS', type: 'gridfs' },
+        { name: 'Kafka', type: 'kafka' },
+        { name: 'MariaDB', type: 'mariadb' },
+        { name: 'MySQL PXC', type: 'mysql pxc' },
+        { name: 'jira', type: 'jira' },
+        { name: 'DM DB', type: 'dameng' },
+        { name: 'hive1', type: 'hive1' },
+        { name: 'TCP/IP', type: 'tcp_udp' },
+        // { name: 'MQ', type: 'mq' },
+        { name: 'HBase', type: 'hbase' },
+        { name: 'KUDU', type: 'kudu' },
+        { name: 'Greenplum', type: 'greenplum' },
+        { name: 'TiDB', type: 'tidb' },
+        { name: 'SAP HANA', type: 'hana' },
+        { name: 'ClickHouse', type: 'clickhouse' },
+        { name: 'ADB MySQL', type: 'adb_mysql' },
+        { name: 'ADB PostgreSQL', type: 'adb_postgres' },
+        { name: 'Hazelcast Cloud', type: 'hazelcast_cloud_cluster' }
+      ],
+      tabs: [
+        {
+          label: 'All Connectors',
+          value: 'All_Connectors'
+        },
+        {
+          label: 'SaaS Connectors',
+          value: 'SaaS_Connectors'
+        },
+        {
+          label: 'Application Connectors',
+          value: 'Application_Connectors'
+        },
+        {
+          label: 'Databases Connectors',
+          value: 'Databases_Connectors'
+        },
+        {
+          label: 'File Connectors',
+          value: 'File_Connectors'
+        },
+        {
+          label: 'My Connectors',
+          value: 'My_Connectors'
+        }
+      ]
+    }
+  },
+  computed: {
+    publicList() {
+      return this.types.filter(t => t.scope === 'public' && !t.beta)
+    },
+    betaList() {
+      return this.types.filter(t => t.scope === 'public' && t.beta)
+    },
+    customerList() {
+      return this.types.filter(t => t.scope === 'customer')
+    },
+    filterList() {
+      const { active, types } = this
+      console.log('filterList', active)
+      let list = cloneDeep(types)
+      switch (active) {
+        case 'All_Connectors':
+          break
+        case 'SaaS_Connectors':
+          list = types.filter(t => t.scope === 'public' && t.beta)
+          break
+        case 'Application_Connectors':
+          break
+        case 'Databases_Connectors':
+          break
+        case 'File_Connectors':
+          list = types.filter(t => !['CSV', 'EXCEL', 'JSON', 'XML'].includes(t.database_type))
+          break
+        case 'My_Connectors':
+          list = types.filter(t => t.scope === 'customer')
+          break
+        default:
+          list = types
+      }
+      return list
+    }
+  },
+  watch: {
+    types: {
+      deep: true,
+      handler() {
+        this.getComingTypes()
+      }
+    }
+  },
+  created() {
+    this.active = this.tabs[0].value
+  },
+  mounted() {
+    this.getComingTypes()
+  },
+  methods: {
+    getPdkIcon(item) {
+      return getConnectionIcon(item.pdkHash)
+    },
+
+    getComingTypes() {
+      this.comingTypes = this.comingTypes.filter(f => !this.types.some(t => t.pdkId === f.type))
+    },
+
+    handleSelect(item) {
+      console.log('handleSelect', item)
+      this.selectType = item.pdkId
+      console.log('this.selectType', this.selectType)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.database {
+  overflow: auto;
+}
+.database-item {
+  width: 80px;
+  flex: 1;
+  margin-right: 40px;
+  &:nth-child(7n) {
+    margin-right: 0;
+  }
+  &.active,
+  &:hover {
+    .img-box {
+      background: rgba(201, 205, 212, 0.3);
+    }
+  }
+  &.disable {
+    .img-box {
+      background-color: rgba(242, 242, 242, 0.2);
+    }
+  }
+}
+.img-box {
+  width: 80px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #f2f2f2;
+}
+.el-image {
+  width: 50px;
+  text-align: center;
+}
+.my-database__desc {
+  background: #f2f2f2;
+}
+::v-deep {
+  .el-tabs__nav-wrap.is-top {
+    padding: 0;
+  }
+}
+</style>
