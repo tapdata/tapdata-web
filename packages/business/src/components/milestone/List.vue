aee6df44bb9da152ebccecb031b6b1f79c99f33a
kennen <317052920@qq.com>
2023-02-14 14:09:34 +0800

    feat(@business/milestone): change items data

--- packages/business/src/components/milestone/List.vue
+++ packages/business/src/components/milestone/List.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="flex h-100">
-    <NodeList v-model="activeNodeId"></NodeList>
-    <div v-if="activeNodeId" class="flex-fill"></div>
-    <div v-else class="flex-fill">
-      <div class="mt-4">整体进度: 3/5,全量数据同步中</div>
-      <div v-for="(item, index) in items" :key="index" class="pro-line mt-4">
-        <VIcon class="color-success">success</VIcon>
-        <span>{{ item.label }}</span>
+    <NodeList v-model="activeNodeId" title="整体进度"></NodeList>
+    <div v-if="activeNodeId" class="flex-fill overflow-auto">
+
+    </div>
+    <div v-else class="flex-fill overflow-auto">
+      <div v-for="(item, index) in items" :key="index" class="pro-line flex mt-4">
+        <VIcon v-if="item.type === 'whole'" size="18">auto-layout</VIcon>
+        <VIcon v-else class="color-success" size="18">success</VIcon>
+        <div class="ml-4 flex-fill">
+          <span class="font-color-normal fw-bold">{{ item.label }}</span>
+          <div v-if="item.desc" class="mt-2 color-info">{{ item.desc }}</div>
+          <ElProgress v-if="item.percentage" :percentage="item.percentage" class="mt-1"></ElProgress>
+        </div>
       </div>
     </div>
   </div>
@@ -25,24 +31,37 @@ export default {
       activeNodeId: '',
       items: [
         {
+          type: 'whole',
+          label: '整体进度',
+          status: 'finish',
+          icon: 'auto-layout',
+          desc: '3/5完成，全量数据同步中',
+          percentage: 50
+        },
+        {
           label: '任务调度',
-          status: 'finish'
+          status: 'finish',
+          desc: '完成'
         },
         {
           label: '连接测试与权限检测',
-          status: 'finish'
+          status: 'finish',
+          desc: '完成'
         },
         {
           label: '表结构迁移',
-          status: 'finish'
+          status: 'finish',
+          desc: '完成'
         },
         {
           label: '全量数据迁移',
-          status: 'finish'
+          status: 'finish',
+          desc: '完成'
         },
         {
           label: '增量数据迁移',
-          status: 'finish'
+          status: 'finish',
+          desc: '完成'
         }
       ]
     }
@@ -52,6 +71,6 @@ export default {
 
 <style lang="scss" scoped>
 .pro-line {
-
+  width: 400px;
 }
 </style>
