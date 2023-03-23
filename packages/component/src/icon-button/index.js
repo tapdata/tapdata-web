5bca478bec5599a4e43beb1d44d35928661efbe3
xufei <xufei@tapdata.io>
2023-03-23 16:59:57 +0800

    feat(@tap/component): add IconButton component

---
+++ packages/component/src/icon-button/index.js
@@ -0,0 +1,34 @@
+import { defineComponent } from '@vue/composition-api'
+import VIcon from '../base/VIcon'
+import './style.scss'
+
+export const IconButton = defineComponent({
+  props: {
+    iconSize: [Number, String],
+    xs: Boolean,
+    sm: Boolean,
+    md: Boolean,
+    lg: Boolean,
+    xl: Boolean
+  },
+  setup(props, { attrs, listeners, slots }) {
+    return () => {
+      return (
+        <button
+          staticClass="t-button t-button--icon"
+          class={{
+            't-button--icon-xs': props.xs,
+            't-button--icon-sm': props.sm,
+            't-button--icon-md': props.md,
+            't-button--icon-lg': props.lg,
+            't-button--icon-xl': props.xl
+          }}
+          type="button"
+          {...{ props: attrs, on: listeners }}
+        >
+          <VIcon>{slots.default()}</VIcon>
+        </button>
+      )
+    }
+  }
+})
