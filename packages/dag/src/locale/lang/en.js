8b67d07d71c45f4e3c313288d6af1a74486f19dc
kennen <317052920@qq.com>
2023-03-25 13:17:59 +0800

    refactor(i18n): incremental_delay replace with Replication Delay

--- packages/dag/src/locale/lang/en.js
+++ packages/dag/src/locale/lang/en.js
@@ -409,7 +409,7 @@ export default {
   packages_dag_monitor_leftsider_gengxin: 'Update:',
   packages_dag_monitor_leftsider_charu: 'Insert:',
   packages_dag_monitor_leftsider_renwushijiantong: 'Task event statistics (strips)',
-  packages_dag_monitor_leftsider_zuidazengliangyan: 'Maximum incremental delay:',
+  packages_dag_monitor_leftsider_zuidazengliangyan: 'Maximum replication delay:',
   packages_dag_monitor_leftsider_tongbuxinxi: 'Sync information',
   packages_dag_monitor_leftsider_tiaoshixinxi: 'Debug information',
   packages_dag_monitor_leftsider_chayixiangqing: 'Difference details',
@@ -433,7 +433,7 @@ export default {
   packages_dag_monitor_leftsider_renwuchuliwan:
     'The total time required for task processing to complete a newly generated event, when there are multiple targets, only the maximum processing time is counted',
   packages_dag_monitor_leftsider_shijiancongyuanku:
-    'The delay time between the event being generated from the source library and the completion of writing to the target after the task is processed. When there are multiple targets, only the maximum incremental delay time is counted',
+    'The delay time between the event being generated from the source library and the completion of writing to the target after the task is processed. When there are multiple targets, only the maximum Replication Delay time is counted',
   packages_dag_monitor_leftsider_qpSshizhi:
     'QPS refers to the average number of input events and output events processed by the task per second',
   packages_dag_components_nodedetaildialog_zengliangduquyan2:
