12dd41abbb8d982c87ceb8f95919f0a0c08e8662
xufei <xufei@tapdata.io>
2023-03-28 14:08:25 +0800

    refactor: CustomSql allow use custom_processor #138114

--- packages/dag/src/mixins/editor.js
+++ packages/dag/src/mixins/editor.js
@@ -1307,7 +1307,7 @@ export default {
       let enable
       let notInWhitelist
       let notAllowTarget
-      let whitelist = ['table', 'database', 'migrate_js_processor', 'js_processor']
+      let whitelist = ['table', 'database', 'migrate_js_processor', 'js_processor', 'custom_processor']
       const schemaFree = this.$store.state.dataflow.pdkSchemaFreeMap
 
       this.allNodes.some(node => {
