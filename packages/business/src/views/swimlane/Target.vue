1c9b71fb4bb89db72071e287bf89a99fa8e43e41
kennen <317052920@qq.com>
2023-04-28 11:07:43 +0800

    refactor(ldp/Target): search function no name data

--- packages/business/src/views/swimlane/Target.vue
+++ packages/business/src/views/swimlane/Target.vue
@@ -298,7 +298,7 @@ export default {
     filterList() {
       if (!this.search) return this.list.filter(item => !this.fdmAndMdmId.includes(item.id))
 
-      return this.list.filter(item => !this.fdmAndMdmId.includes(item.id) && item.name.includes(this.search))
+      return this.list.filter(item => !this.fdmAndMdmId?.includes(item.id) && item.name?.includes(this.search))
     }
   },
 
