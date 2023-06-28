8af0e8301757b72c0971e29678974c696f5720b7
xufei <xufei@tapdata.io>
2023-06-28 10:47:41 +0800

    fix(@tap/ldp): Params  input add InputNumber #146882

--- packages/business/src/views/data-server/Drawer.vue
+++ packages/business/src/views/data-server/Drawer.vue
@@ -279,7 +279,14 @@
             min-width="100"
           >
             <template #default="{ row }">
-              <ElInput v-model="debugParams[row.name]" size="mini"></ElInput>
+              <ElInputNumber
+                v-if="row.type === 'number'"
+                v-model="debugParams[row.name]"
+                :precision="0"
+                :step="1"
+                :min="0"
+              />
+              <ElInput v-else v-model="debugParams[row.name]" size="mini"></ElInput>
             </template>
           </ElTableColumn>
           <ElTableColumn v-if="isEdit && form.apiType === 'customerQuery'" align="center" width="60">
