bed6bb0eedd046bbe46d299c17935140840e6c76
fanniegirl <2849481996@qq.com>
2023-07-01 18:27:45 +0800

    fix(dfs/createAgent): adjust style

--- apps/dfs/src/views/agent-download/SubscriptionModelDialog.vue
+++ apps/dfs/src/views/agent-download/SubscriptionModelDialog.vue
@@ -4,6 +4,7 @@
     :close-on-click-modal="false"
     :close-on-press-escape="false"
     :show-close="showClose"
+    :top="'4vh'"
     :title="$t('dfs_agent_download_subscriptionmodeldialog_peizhishishishu')"
     width="1200px"
     custom-class="tap-dialog"
@@ -37,7 +38,7 @@ export default {
 
 <style lang="scss">
 .subscriptionModelDialog {
-  height: 695px;
+  height: 642px;
   .main {
     padding: 0;
   }
