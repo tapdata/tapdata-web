eab3e8807359f69e92f78737518ac7afbcae0c7b
fanniegirl <2849481996@qq.com>
2023-07-05 14:20:47 +0800

    refactor(instance): Update agent list

--- apps/dfs/src/views/instance/Instance.vue
+++ apps/dfs/src/views/instance/Instance.vue
@@ -13,6 +13,50 @@
               </ElButton>
             </div>
           </div>
+          <ul>
+            <li v-for="item in list" :key="item.id">
+              <div class="flex">
+                <div>
+                  <span v-if="item.publicAgent">{{ item.name }}</span>
+                  <InlineInput
+                    v-else
+                    :class="['inline-input', 'color-primary', { 'cursor-pointer': item.agentType !== 'Cloud' }]"
+                    :value="item.name"
+                    :icon-config="{ class: 'color-primary', size: '12' }"
+                    type="icon"
+                    @click-text="handleDetails(item)"
+                    @save="updateName($event, item.id)"
+                  ></InlineInput>
+                </div>
+                <div>
+                  <template>
+                    <StatusTag
+                      v-if="item.agentType === 'Cloud' && item.status === 'Creating'"
+                      type="tag"
+                      status="Deploying"
+                      default-status="Stopped"
+                    ></StatusTag>
+                    <StatusTag v-else type="tag" :status="item.status" default-status="Stopped"></StatusTag>
+                    <ElTooltip v-if="item.status == 'Stopped'" placement="top">
+                      <VIcon size="14" class="ml-2 color-primary">question-circle</VIcon>
+                      <template #content>
+                        <div style="max-width: 380px">
+                          {{ $t('dfs_instance_stopped_help_tip_prefix') }}
+                          <a
+                            target="_blank"
+                            href="https://docs.tapdata.io/cloud/faq/agent-installation#agent-%E6%98%BE%E7%A4%BA%E7%A6%BB%E7%BA%BF%E5%A6%82%E4%BD%95%E9%87%8D%E5%90%AF"
+                            class="color-primary"
+                            >{{ $t('dfs_online_help_docs') }}</a
+                          >
+                          {{ $t('dfs_instance_stopped_help_tip_suffix') }}
+                        </div>
+                      </template>
+                    </ElTooltip>
+                  </template>
+                </div>
+              </div>
+            </li>
+          </ul>
           <ElTable
             class="instance-table table-border mt-4"
             height="100%"
