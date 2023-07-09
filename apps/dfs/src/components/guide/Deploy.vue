089c682dcb212482a2278ce01aba308e5e14536e
fanniegirl <2849481996@qq.com>
2023-07-09 20:36:50 +0800

    refactor(guide): add pay.vue

--- apps/dfs/src/components/guide/Deploy.vue
+++ apps/dfs/src/components/guide/Deploy.vue
@@ -5,18 +5,41 @@ export default {
   name: 'Account',
   inject: ['buried'],
   components: { VIcon },
+  props: ['agentId'],
   data() {
     return {
       downLoadType: '',
       showTooltip: false,
+      showAllCode: true,
+      downloadUrl: '',
+      token: '',
+      version: '',
+      links: {},
       downType: [
-        { name: 'Linux (64 bit)', value: 'Linux' },
-        { name: 'Docker', value: 'Docker' },
+        { name: 'Linux (64 bit)', value: 'linux' },
+        { name: 'Docker', value: 'docker' },
         { name: 'Windows (64 bit)', value: 'windows' }
       ]
     }
   },
+  mounted() {
+    this.getUrl()
+  },
   methods: {
+    getUrl() {
+      this.$axios.get('api/tcm/productRelease/deploy/' + this.agentId).then(async data => {
+        this.downloadUrl = data.downloadUrl || ''
+        this.token = data.token || ''
+        this.version = data.version || ''
+        let links = data.links || []
+        links.forEach(el => {
+          this.links[el.os] = el.command
+        })
+        this.$nextTick(() => {
+          this.downLoadType = 'linux'
+        })
+      })
+    },
     onCopy() {
       this.showTooltip = true
     },
@@ -28,6 +51,10 @@ export default {
         AliComputenest: 'copyTokenInAliComputenest'
       }
       this.buried(MAP[this.downLoadType])
+    },
+    //显示隐藏完整命令
+    handleShowAllCode(type) {
+      this.showAllCode = type
     }
   }
 }
@@ -36,7 +63,7 @@ export default {
 <template>
   <div class="account">
     <div class="fs-6 font-color-dark fw-sub mb-4 mt-4">6、准备Tapdata Cloud 计算引擎环境部署，如遇问题可 咨询客服</div>
-    <div>要安装Agent,请先在下方选择您的服务器类型：</div>
+    <div class="fw-sub font-color-dark mt-4">要安装Agent,请先在下方选择您的服务器类型：</div>
     <ElRadioGroup v-model="downLoadType" class="flex gap-4 mt-4 mb-4">
       <ElRadio
         v-for="(item, index) in downType"
@@ -48,10 +75,11 @@ export default {
         <span>{{ item.name }}</span>
       </ElRadio>
     </ElRadioGroup>
-    <div>开始安装</div>
-    <div>请复制下方命令并在部署环境执行，其包含镜像的下载及运行，Tapdata Agent 的下载、自动部署及启动</div>
+    <div class="fw-sub font-color-dark mt-4">开始安装</div>
+    <div class="font-color-light fw-normal mt-2 mb-2">
+      请复制下方命令并在部署环境执行，其包含镜像的下载及运行，Tapdata Agent 的下载、自动部署及启动
+    </div>
     <div class="my-2 text-style">
-      {{ $t('dfs_agent_download_agentdownloadmodal_shilitok') }}
       <ElTooltip
         placement="top"
         manual
@@ -62,7 +90,7 @@ export default {
         <el-button
           type="primary"
           class="operaKey"
-          v-clipboard:copy="token"
+          v-clipboard:copy="links[downLoadType]"
           v-clipboard:success="onCopy"
           @mouseleave="showTooltip = false"
           @click="handleCopy"
@@ -72,7 +100,49 @@ export default {
         </el-button>
       </ElTooltip>
     </div>
+    <div class="box title-text my-2" :class="{ 'overflow-hidden': showAllCode }">
+      <span class="link-line" :class="{ 'hidden-all-code': showAllCode }">{{ links[downLoadType] }}</span>
+    </div>
+    <div
+      class="down-show-code text-center cursor-pointer color-primary bg-white mt-2 pb-2"
+      v-if="showAllCode"
+      @click="handleShowAllCode(false)"
+    >
+      <VIcon class="mr-2">arrow-down-fill</VIcon
+      ><span>{{ $t('dfs_agent_download_fastdownload_xianshiwanzhengming') }}</span>
+    </div>
+    <div class="down-show-code text-center cursor-pointer bg-white mt-2 pb-2" v-else @click="handleShowAllCode(true)">
+      <VIcon class="mr-2 fast-icon">arrow-down-fill</VIcon
+      >{{ $t('dfs_agent_download_fastdownload_yincangwanzhengming') }}
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.hidden-all-code {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: inline-block;
+  width: 100%;
+}
+.fast-icon {
+  -moz-transform: rotate(-180deg);
+  -webkit-transform: rotate(-180deg);
+}
+.down-show-code {
+  border: 1px solid #f3f3f3;
+  border-top: none;
+}
+.title-text {
+  line-height: 22px;
+  color: #fff;
+}
+.link-line {
+  word-break: break-all;
+}
+.box {
+  padding: 10px 20px;
+  background: var(--unnamed, #333c4a);
+}
+</style>
--- apps/dfs/src/components/guide/Details.vue
+++ apps/dfs/src/components/guide/Details.vue
@@ -1,15 +1,116 @@
 <script>
 import { VTable } from '@tap/component'
+import i18n from '@/i18n'
 export default {
   name: 'Pay',
-  components: { VTable }
+  components: { VTable },
+  props: ['isCard'],
+  data() {
+    return {
+      order: [],
+      form: {
+        email: ''
+      },
+      columns: [
+        {
+          label: i18n.t('dfs_order_list_dingyueleixing'),
+          prop: 'productType'
+        },
+        {
+          label: i18n.t('dfs_instance_instance_guige'),
+          prop: 'specLabel',
+          width: 180
+        },
+        {
+          label: i18n.t('dfs_instance_instance_dingyuefangshi'),
+          slotName: 'subscriptionMethodLabel',
+          width: 180
+        },
+        {
+          label: i18n.t('dfs_user_center_jine'),
+          prop: 'price',
+          slotName: 'price'
+        }
+      ],
+      payType: 'Stripe',
+      types: [
+        {
+          label: '在线支付',
+          value: 'Stripe'
+        }
+      ]
+    }
+  },
+  mounted() {
+    this.form.email = window.__USER_INFO__.email
+  },
+  methods: {
+    getEmailRules() {
+      return [
+        {
+          required: true,
+          message: i18n.t('dfs_instance_create_qingshuruninde')
+        },
+        {
+          type: 'email',
+          message: i18n.t('dfs_instance_create_qingshuruzhengque')
+        }
+      ]
+    },
+    validateForm(ref) {
+      return new Promise(resolve => {
+        this.$refs[ref].validate(valid => {
+          resolve(valid)
+        })
+      })
+    }
+  }
 }
 </script>
 <template>
   <section>
-    <header>所选配置</header>
-    <VTable></VTable>
+    <div class="mb-4" :class="{ card: isCard, 'mt-6 ': !isCard }">
+      <div class="font-color-dark fw-sub fs-5 mb-4">所选配置</div>
+      <VTable :columns="columns" :data="order" ref="table" :has-pagination="false"></VTable>
+    </div>
+    <div :class="{ card: isCard }">
+      <p class="mt-4 mb-2">{{ $t('dfs_instance_create_jieshouzhangdande') }}</p>
+      <ElForm :model="form" ref="from">
+        <ElFormItem prop="email" :rules="getEmailRules()">
+          <ElInput v-model="form.email" :placeholder="$t('dfs_instance_create_yongyujieshoumei')"></ElInput>
+        </ElFormItem>
+      </ElForm>
+    </div>
+    <div class="mt-4" :class="{ card: isCard }">
+      <div>选择支付方式</div>
+      <ElRadioGroup v-model="payType" class="flex gap-4 mt-4 mb-4">
+        <ElRadio
+          v-for="(item, index) in types"
+          :key="index"
+          :label="item.value"
+          border
+          class="rounded-4 subscription-radio m-0 position-relative"
+        >
+          <span>{{ item.label }}</span>
+        </ElRadio>
+      </ElRadioGroup>
+    </div>
+    <ul class="mt-4" :class="{ card: isCard }">
+      <li><span class="mr-4">95折</span>：<span> -￥67</span></li>
+      <li>
+        <span class="fw-sub font-color-dark mt-2 mr-4">实付金额</span>:<span class="color-primary fw-sub fs-5"
+          >¥ 8,640.00</span
+        >
+      </li>
+    </ul>
   </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.card {
+  padding: 16px 24px;
+  border-radius: 8px;
+  border-top: 1px solid var(--unnamed, #e5e6eb);
+  background: #fff;
+}
+</style>
