41d31ebc57b3d88d67d1ccc1b429abfaa15ff2c5
xufei <xufei@tapdata.io>
2023-07-18 08:50:00 +0800

    refactor(@tap/dag): PaperEmpty add svg animation

--- apps/daas/vue.config.js
+++ apps/daas/vue.config.js
@@ -44,7 +44,8 @@ module.exports = {
   transpileDependencies: [
     // 按需添加需要babel处理的模块
     /[/\\]node_modules[/\\](.+?)?element-ui(.*)[/\\]packages[/\\]table[/\\]src/,
-    /[/\\]node_modules[/\\](.+?)?element-ui(.*)[/\\]packages[/\\]tooltip[/\\]src/
+    /[/\\]node_modules[/\\](.+?)?element-ui(.*)[/\\]packages[/\\]tooltip[/\\]src/,
+    /[/\\]node_modules[/\\](.+?)?@figmania\/webcomponent(.*)[/\\]/
   ],
   chainWebpack(config) {
     //  ============ 配置别名 ============
