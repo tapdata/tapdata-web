edb2452762959e6e5a0922e1aded3b159bf3281f
Feynman <xufei@tapdata.io>
2023-08-16 10:23:15 +0800

    chore: update vue.config.js

--- apps/dfs/vue.config.js
+++ apps/dfs/vue.config.js
@@ -13,8 +13,8 @@ const serveUrlMap = {
 // const userId = '60b064e9a65d8e852c8523bc' // lemon
 // const userId = '610a3d43d7f65cfcd80837b5' // auto
 // const userId = '60cc0c304e190a579cbe306c' // jason
-const userId = '64ba40c389c61b08683c71b0' // xf
-// const userId = '64be2c812c268c9dd5afaf25' // devin
+// const userId = '64ba40c389c61b08683c71b0' // xf
+const userId = '64be2c812c268c9dd5afaf25' // devin
 let origin
 const { argv } = process
 const { SERVE_ENV = 'mock' } = process.env
