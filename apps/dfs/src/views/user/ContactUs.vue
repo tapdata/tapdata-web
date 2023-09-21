8ec1d51e8bfd80a00aa1f9b6f9c5459e548090da
Kennen <317052920@qq.com>
2023-09-21 17:15:33 +0800

    fix: Change email icon

--- apps/dfs/src/views/user/ContactUs.vue
+++ apps/dfs/src/views/user/ContactUs.vue
@@ -25,7 +25,7 @@
       </li>
       <li class="p-4 border rounded-2 ml-4 flex-grow-1">
         <div class="fs-7 flex align-items-center">
-          <VIcon size="20" class="mr-2 color-primary">email</VIcon>
+          <VIcon size="20" class="mr-2 color-primary">email-fill</VIcon>
           <span>{{ $t('dfs_user_contactus_youxiang') }}</span>
         </div>
         <ElButton type="text" class="mt-2" size="mini">team@tapdata.io</ElButton>
@@ -122,7 +122,7 @@ export default {
       s.src = isDomesticStation ? 'https://salesiq.zoho.com.cn/widget' : 'https://salesiq.zohopublic.com/widget'
       let t = d.getElementsByTagName('script')[0]
       t.parentNode.insertBefore(s, t)
-      this.hideCustomTip()
+      // this.hideCustomTip()
 
       $zoho.salesiq.ready = function () {
         const user = window.__USER_INFO__
