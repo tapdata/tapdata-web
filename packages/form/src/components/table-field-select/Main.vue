119eff02741637fabbf4f8f2b27d979c682a6f84
Kennen <317052920@qq.com>
2023-10-29 19:48:34 +0800

    feat(node/Table.js): Add concurrentWritePartitionMap

---
+++ packages/form/src/components/table-field-select/Main.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <ElSelect
+      multiple
+      filterable
+      allow-create
+      default-first-option
+      class="fields-selector--input"
+      v-model="selected.fields"
+      @change="handleChange"
+    >
+      <ElOption v-for="opt in options" :key="opt.value" :label="opt.label" :value="opt.value"></ElOption>
+    </ElSelect>
+  </div>
+</template>
+
+<script>
+import { cloneDeep, isEmpty } from 'lodash'
+import { metadataInstancesApi } from '@tap/api'
+
+export default {
+  name: 'AsyncSelect',
+
+  props: {
+    nodeId: String,
+    tableName: String,
+    defaultFields: Array
+  },
+
+  data() {
+    return {
+      result: {},
+      selected: {
+        table: '',
+        fields: []
+      },
+      options: []
+    }
+  },
+
+  watch: {
+    tableName(v) {
+      console.log('watch-tableName', v)
+      this.clear()
+      this.loadData()
+    }
+  },
+
+  mounted() {
+    this.selected.table = this.tableName
+    this.selected.fields = cloneDeep(this.defaultFields)
+    this.handleChange()
+    this.loadData()
+  },
+
+  methods: {
+    async loadData() {
+      const data = await metadataInstancesApi.nodeSchema(this.nodeId)
+      this.options = data[0].fields
+        .map(item => ({
+          label: item.field_name,
+          value: item.field_name,
+          isPrimaryKey: item.primary_key_position > 0,
+          indicesUnique: !!item.indicesUnique,
+          type: item.data_type
+        }))
+        .filter(item => !item.is_deleted)
+    },
+
+    handleChange() {
+      this.result[this.selected.table] = cloneDeep(this.selected.fields)
+      let result = cloneDeep(this.result)
+      if (isEmpty(result)) {
+        result[this.tableName] = cloneDeep(this.defaultFields)
+      }
+
+      this.$emit('input', result).$emit('change', result)
+    },
+
+    clear() {
+      this.selected.table = ''
+      this.selected.fields = []
+      this.result = {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
