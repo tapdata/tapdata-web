25c9a7beec7758e870e7a760cb3b991ec3898af1
Feynman <xufei@tapdata.io>
2023-11-24 18:08:16 +0800

    refactor: re-enables the use of `transformComponent` from the `@formily/element-plus` library in the `TableSelector` component. The inherent component, formerly named `TableSelector`, has been renamed to `_TableSelector` and transformed with `transformComponent`. The original commented-out code for this action has been removed.

--- packages/dag/src/components/form/table-selector/index.js
+++ packages/dag/src/components/form/table-selector/index.js
@@ -1,8 +1,8 @@
-// import { transformComponent } from '@formily/element-plus/lib/__builtins__/shared'
-import TableSelector from './TableSelector'
+import { transformComponent } from '@formily/element-plus/lib/__builtins__/shared'
+import _TableSelector from './TableSelector'
 
-/*export const TableSelector = transformComponent(_TableSelector, {
-  change: 'input',
-})*/
+export const TableSelector = transformComponent(_TableSelector, {
+  // change: 'input',
+})
 
 export default TableSelector
