33011c35e4b4c7ed7447b312ac9b2aed97610561
Feynman <xufei@tapdata.io>
2023-12-01 18:41:03 +0800

    refactor: update i18n

--- packages/dag/src/locale/lang/en.js
+++ packages/dag/src/locale/lang/en.js
@@ -651,6 +651,7 @@ export default {
   packages_dag_mixins_formscope_gaizduanshibi: 'This field is required!',
   packages_dag_default_js: 'Default JS',
   packages_dag_standardization_js: 'Standardized JS',
+  packages_dag_mixins_formscope_gaiziduanshibi: 'This field is required!',
   packages_dag_shared_mining_table_info_index_wajuebiaoxinxi: 'Mining table information',
   packages_dag_table_not_exist: 'Table not exist, will be automatically created',
   packages_dag_field_inference_list_anxishutiaozheng: 'Adjust by coefficient',
