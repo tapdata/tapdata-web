b921c95d60e7f9771bb65662b73c55e60833f0ab
Feynman <xufei@tapdata.io>
2023-12-12 15:49:28 +0800

    refactor(@tap/form): add select, update field-select

---
+++ packages/form/src/components/select/index.tsx
@@ -0,0 +1,73 @@
+import { connect, mapProps, mapReadPretty } from '@formily/vue'
+import { defineComponent, h } from 'vue'
+import { PreviewText } from '@formily/element-plus'
+
+import { ElSelect, ElOption } from 'element-plus'
+import { resolveComponent, transformComponent } from '@formily/element-plus/esm/__builtins__'
+
+export type SelectProps = typeof ElSelect & {
+  options?: Array<typeof ElOption>
+}
+
+const TransformElSelect = transformComponent<SelectProps>(ElSelect, {
+  change: 'update:modelValue',
+})
+
+const InnerSelect = connect(
+  TransformElSelect,
+  mapProps({ value: 'modelValue', readOnly: 'readonly' }),
+  mapReadPretty(PreviewText.Select),
+)
+
+const SelectOption = defineComponent({
+  name: 'FSelect',
+  props: ['options'],
+  setup(customProps, { attrs, slots }) {
+    return () => {
+      const options = customProps.options || []
+      const children =
+        options.length !== 0
+          ? {
+              default: () =>
+                options.map((option: any) => {
+                  if (typeof option === 'string') {
+                    return h(
+                      ElOption,
+                      { key: option, value: option, label: option },
+                      {
+                        default: () => [resolveComponent(slots?.option ?? option, { option })],
+                      },
+                    )
+                  } else {
+                    return h(
+                      ElOption,
+                      {
+                        key: option.value,
+                        ...option,
+                      },
+                      {
+                        default: () => [resolveComponent(slots?.option ?? option, { option })],
+                      },
+                    )
+                  }
+                }),
+            }
+          : slots
+      return h(
+        InnerSelect,
+        {
+          ...attrs,
+        },
+        children,
+      )
+    }
+  },
+})
+
+export const Select = connect(
+  SelectOption,
+  mapProps({ dataSource: 'options', loading: true }),
+  mapReadPretty(PreviewText.Select),
+)
+
+export default Select
