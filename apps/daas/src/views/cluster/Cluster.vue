832f5032d79248355cc96e398e8f8a79354f5a11
Feynman <xufei@tapdata.io>
2024-04-08 11:32:43 +0800

    refactor: set viewType default cluster

--- apps/daas/src/views/cluster/Cluster.vue
+++ apps/daas/src/views/cluster/Cluster.vue
@@ -654,7 +654,7 @@ export default {
         }
       ],
       bindWorkerMap: {},
-      viewType: 'component',
+      viewType: 'cluster',
       netStatDialog: {
         visible: false
       },
