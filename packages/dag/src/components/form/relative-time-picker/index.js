31e060ebc0e5a6373eb3ee3dc7c4ba93e975d646
Feynman <xufei@tapdata.io>
2024-04-29 14:52:22 +0800

    refactor: update RelativeTimePicker preview and tooltip

--- packages/dag/src/components/form/relative-time-picker/index.js
+++ packages/dag/src/components/form/relative-time-picker/index.js
@@ -1,6 +1,7 @@
-import { defineComponent, reactive, watch, ref } from '@vue/composition-api'
+import { defineComponent, reactive, watch, ref, computed } from '@vue/composition-api'
 import { observer } from '@formily/reactive-vue'
 import { useForm, useField } from '@tap/form'
+import { VIcon } from '@tap/component'
 import { taskApi } from '@tap/api'
 import { dayjs } from '@tap/business/src/shared/dayjs'
 import i18n from '@tap/i18n'
@@ -19,43 +20,48 @@ export const RelativeTimePicker = observer(
         unit: record.unit || 'DAY'
       })
       const currentDate = ref(new Date())
-      const loadDateRange = async () => {
-        const result = await taskApi.getTimeRange(state)
-      }
-      const loadCurrentEngineTime = () => {
-        const date = taskApi.getCurrentEngineTime()
+      // const loadDateRange = async () => {
+      //   const result = await taskApi.getTimeRange(state)
+      // }
+      const loadCurrentEngineTime = async () => {
+        const date = await taskApi.getCurrentEngineTime()
         currentDate.value = new Date(date)
       }
 
-      loadDateRange()
+      // loadDateRange()
       loadCurrentEngineTime()
-      const genDatePreview = () => {
+
+      const preview = computed(() => {
         if (state.form !== 'BEFORE') return ''
         let start = dayjs(currentDate.value)
         let end = dayjs(currentDate.value)
+        let year = currentDate.value.getFullYear()
         switch (state.unit) {
           case 'HOUR':
             start = end.subtract(state.number, 'hour') // 昨天
             break
           case 'DAY':
             end = end.subtract(1, 'day') // 昨天
-            start = end.subtract(state.number, 'day')
+            start = end.subtract(state.number - 1, 'day')
             break
           case 'WEEK':
             end = end.subtract(end.day() || 7, 'd') // 上周
-            start = end.subtract(6, 'day').subtract(state.number - 1, 'w') // 上周一
+            start = end.subtract(6, 'day').subtract(state.number - 1, 'week') // 上周一
             break
           case 'MONTH':
-            end = end.subtract(1, 'M').endOf('month') // 上个月
-            start = end.startOf('month')
+            end = end.subtract(1, 'month').endOf('month') // 上个月
+            start = end.startOf('month').subtract(state.number - 1, 'month')
             break
           case 'YEAR':
             end = dayjs().subtract(1, 'year').endOf('year')
-            start = end.startOf('year')
+            start = end.startOf('year').subtract(state.number - 1, 'year')
             break
         }
-        // const formatStr = 'LL'
-      }
+
+        const formatStr = start.year() !== year ? 'MMM D, YYYY' : 'MMM D'
+
+        return `${start.format(formatStr)} - ${end.format(formatStr)}`
+      })
 
       watch(state, val => {
         Object.assign(record, state)
@@ -80,6 +86,35 @@ export const RelativeTimePicker = observer(
         WEEK: { l: i18n.t('public_last_week'), c: i18n.t('public_this_week') },
         MONTH: { l: i18n.t('public_last_month'), c: i18n.t('public_this_month') },
         YEAR: { l: i18n.t('public_last_year'), c: i18n.t('public_this_year') }
+      }
+
+      const getCurrentTip = unit => {
+        let current = dayjs(currentDate.value)
+        let year = current.year()
+        let str, s, e, formatStr
+        switch (unit) {
+          case 'HOUR':
+            str = current.format('ddd, MMM D H')
+            break
+          case 'DAY':
+            str = current.format('ddd, MMM D')
+            break
+          case 'WEEK':
+            s = current.subtract((current.day() || 7) - 1, 'd') // 上周
+            e = s.add(6, 'd')
+            formatStr = s.year() !== year ? 'ddd, MMM D, YYYY' : 'ddd, MMM D'
+            str = `${s.format(formatStr)} - ${e.format(formatStr)}`
+            break
+          case 'MONTH':
+            str = `${current.startOf('month').format('ddd, MMM D')} - ${current.endOf('month').format('ddd, MMM D')}`
+            break
+          case 'YEAR':
+            str = `${current.startOf('month').format('ddd, MMM D, YYYY')} - ${current
+              .endOf('month')
+              .format('ddd, MMM D, YYYY')}`
+            break
+        }
+        return i18n.t('public_date_current_prefix') + str
       }
 
       // FIXME 升级到vue3后， 这里要用children的方式传递具名slot
@@ -142,20 +177,25 @@ export const RelativeTimePicker = observer(
                       ))}
                     </ElSelect>
                   </div>
+                  <div class="flex gap-2 align-center font-color-sslight mt-2">
+                    <VIcon size={16}>calendar</VIcon>
+                    <span class="lh-base">{preview.value}</span>
+                  </div>
                 </ElTabPane>
                 <ElTabPane label={i18n.t('public_date_current')} name="CURRENT">
                   <div class="flex flex-wrap gap-3">
                     {unitOptions.slice(1).map(option => (
-                      <ElTag
-                        class="clickable rounded-pill"
-                        type={state.unit === option.value ? '' : 'info'}
-                        size="medium"
-                        onClick={() => {
-                          state.unit = option.value
-                        }}
-                      >
-                        {option.label}
-                      </ElTag>
+                      <ElTooltip open-delay={500} placement="bottom" content={getCurrentTip(option.value)}>
+                        <ElButton
+                          class="rounded-pill m-0 min-w-0"
+                          type={state.unit === option.value ? 'primary' : ''}
+                          onClick={() => {
+                            state.unit = option.value
+                          }}
+                        >
+                          {option.label}
+                        </ElButton>
+                      </ElTooltip>
                     ))}
                   </div>
                 </ElTabPane>
