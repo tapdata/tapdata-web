93c8a93a157cead7cefe83cf09bbd9d5e0acfa6d
Feynman <xufei@tapdata.io>
2024-07-19 18:35:15 +0800

    refactor: update install oem

--- apps/daas/src/oem/index.js
+++ apps/daas/src/oem/index.js
@@ -1,11 +1,13 @@
 const requireContext = require.context('./', true, /\.js$/)
 
+const AllInstall = requireContext.keys().map(fileName => {
+  if (fileName === './index.js') return
+
+  return requireContext(fileName)
+})
+
 export function installOEM(router, i18n) {
-  requireContext.keys().forEach(fileName => {
-    if (fileName === './index.js') return
-
-    let Module = requireContext(fileName)
-
-    Module.install?.(router, i18n)
+  AllInstall.forEach(Module => {
+    Module?.install?.(router, i18n)
   })
 }
