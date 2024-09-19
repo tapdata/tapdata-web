e059667519fcb792b138236a04fc3249f0796f09
Feynman <xufei@tapdata.io>
2024-09-19 15:57:53 +0800

    fix: shared mining table can't keep selection

--- packages/business/src/views/shared-mining/Table.vue
+++ packages/business/src/views/shared-mining/Table.vue
@@ -44,6 +44,7 @@
     </div>
     <VTable
       :columns="columns"
+      row-key="id"
       :remoteMethod="remoteMethod"
       :page-options="{
         layout: 'total, prev, pager, next, jumper'
@@ -155,7 +156,8 @@ export default {
       ],
       columns: [
         {
-          type: 'selection'
+          type: 'selection',
+          reserveSelection: true
         },
         {
           label: i18n.t('packages_business_shared_mining_table_biaoming'),
@@ -170,7 +172,8 @@ export default {
         },
         {
           label: i18n.t('packages_business_shared_mining_table_leijiwajue'),
-          prop: 'allCount'
+          prop: 'allCount',
+          minWidth: 150
         },
         {
           label: i18n.t('packages_business_shared_mining_table_jinriwajue'),
@@ -265,7 +268,10 @@ export default {
         }
         return {
           total: total,
-          data: data.items || []
+          data: (data.items || []).map(item => {
+            item.id = `${item.connectionId}_${item.name}`
+            return item
+          })
         }
       })
     },
