9aa85adeff0321253f6154dc43fb2e63dca860a5
Feynman <xufei@tapdata.io>
2024-09-30 16:59:48 +0800

    refactor: add kafka-enhanced doc url map

--- packages/business/src/components/ConnectorDoc.vue
+++ packages/business/src/components/ConnectorDoc.vue
@@ -87,7 +87,8 @@ const pdkDocMap = {
   dummy: 'prerequisites/others/dummy',
   'http-receiver': 'prerequisites/others/http-receiver',
   greenplum: 'prerequisites/warehouses-and-lake/greenplum',
-  dws: 'prerequisites/warehouses-and-lake/gaussdb'
+  dws: 'prerequisites/warehouses-and-lake/gaussdb',
+  'kafka-enhanced': 'prerequisites/mq-and-middleware/kafka-enhanced'
 }
 
 // 维护一个DocMap还有一个NameDictionary的原因是，docMap从文档仓库直接复制过来，有些命名和pdkId不一致
