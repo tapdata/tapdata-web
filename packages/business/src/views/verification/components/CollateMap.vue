fc05cc123823c2c0bd5d8aaa7ee858d927228f70
Feynman <xufei@tapdata.io>
2024-12-05 01:21:57 +0800

    feat: add CollateMap component and integrate with ConditionBox for custom sorting options
    - Introduced a new CollateMap component for managing key-value pairs.
    - Updated ConditionBox to utilize CollateMap for custom sorting configurations.
    - Replaced MultiSelection with FieldSelectWrap for improved field selection handling.
    - Enhanced data processing in ConditionBox to support new field structure.
    - Added styling adjustments for better UI consistency.

---
+++ packages/business/src/views/verification/components/CollateMap.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="py-3 flex flex-column gap-3">
+    <div v-for="(item, i) in list" :key="item.key" class="flex gap-3">
+      <FieldSelect v-model="item.key" :options="sortFields" placeholder="请选择字段" @change="handleChange" />
+      <ElInput v-model="item.value" placeholder="请输入字符集" @change="handleChange" />
+      <IconButton class="flex-shrink-0" @click="handleDelete(item, i)"> delete </IconButton>
+    </div>
+    <ElButton class="w-100" style="border-style: dashed" @click="handleAdd"> 添加 </ElButton>
+  </div>
+</template>
+
+<script>
+import { defineComponent, computed, ref, del, set } from '@vue/composition-api'
+import { FieldSelect } from '@tap/form'
+import { IconButton } from '@tap/component'
+
+export default defineComponent({
+  name: 'CollateMap',
+  props: {
+    value: {
+      type: Object,
+      default: () => ({})
+    },
+    fields: {
+      type: Array,
+      default: () => []
+    },
+    sortColumn: {
+      type: String,
+      required: true
+    }
+  },
+
+  components: {
+    FieldSelect,
+    IconButton
+  },
+
+  setup(props, { emit }) {
+    const list = ref([])
+
+    if (props.value) {
+      list.value = Object.keys(props.value).map(key => ({
+        key,
+        value: props.value[key]
+      }))
+    }
+
+    if (list.value.length === 0) {
+      list.value.push({
+        key: '',
+        value: ''
+      })
+    }
+
+    const sortColumns = computed(() => {
+      return props.sortColumn ? props.sortColumn.split(',') : []
+    })
+
+    const sortFields = computed(() => {
+      return props.fields.filter(field => sortColumns.value.includes(field.value) && !(field.value in props.value))
+    })
+
+    const handleAdd = () => {
+      list.value.push({
+        key: '',
+        value: ''
+      })
+    }
+
+    const handleDelete = (item, index) => {
+      list.value.splice(index, 1)
+      item.key && del(props.value, item.key)
+    }
+
+    const handleChange = () => {
+      const result = list.value.reduce((acc, item) => {
+        item.key && set(acc, item.key, item.value)
+        return acc
+      }, {})
+
+      emit('input', result)
+    }
+
+    return {
+      list,
+      sortFields,
+      handleDelete,
+      handleAdd,
+      handleChange
+    }
+  }
+})
+</script>
--- packages/business/src/views/verification/components/ConditionBox.vue
+++ packages/business/src/views/verification/components/ConditionBox.vue
@@ -127,9 +127,16 @@
                   label-width="130"
                 />
               </div>
-              <div v-if="!isCountOrHash" class="setting-item mt-4">
-                <label class="item-label">{{ $t('packages_business_verification_indexField') }}: </label>
-                <MultiSelection
+              <template v-if="!isCountOrHash">
+                <div class="setting-item mt-4">
+                  <label class="item-label">{{ $t('packages_business_verification_indexField') }}: </label>
+                  <FieldSelectWrap
+                    v-model="item.source.sortColumn"
+                    :options="item.source.fields"
+                    :key="`item-source-sortColumn` + item.id"
+                    class="flex-1"
+                  ></FieldSelectWrap>
+                  <!-- <MultiSelection
                   v-model="item.source.sortColumn"
                   class="item-select"
                   :class="{ 'empty-data': !item.source.sortColumn }"
@@ -137,17 +144,59 @@
                   :id="'item-source-' + index"
                   :key="`item-source-sortColumn` + item.id"
                   @focus="handleFocus(item.source)"
-                ></MultiSelection>
-                <span class="item-icon"></span>
-                <MultiSelection
+                ></MultiSelection> -->
+                  <span class="item-icon"></span>
+                  <FieldSelectWrap
+                    v-model="item.target.sortColumn"
+                    :options="item.source.fields"
+                    :key="`item-target-sortColumn` + item.id"
+                    class="flex-1"
+                  ></FieldSelectWrap>
+                  <!-- <MultiSelection
                   v-model="item.target.sortColumn"
                   class="item-select"
                   :class="{ 'empty-data': !item.target.sortColumn }"
                   :options="item.target.fields"
                   :key="`item-target-sortColumn` + item.id"
                   @focus="handleFocus(item.target)"
-                ></MultiSelection>
-              </div>
+                ></MultiSelection> -->
+                </div>
+
+                <div class="setting-item mt-4">
+                  <label class="item-label">自定义排序: </label>
+                  <div class="flex-1">
+                    <ElSwitch v-model="item.source.enableCustomCollate" />
+
+                    <div v-if="item.source.enableCustomCollate">
+                      <CollateMap
+                        v-model="item.source.collateMap"
+                        :sort-column="item.source.sortColumn"
+                        :fields="item.source.fields"
+                      />
+                    </div>
+                  </div>
+                  <span class="item-icon"></span>
+                  <div class="flex-1">
+                    <ElSwitch v-model="item.target.enableCustomCollate" />
+                    <div v-if="item.target.enableCustomCollate">
+                      <CollateMap
+                        v-model="item.target.collateMap"
+                        :sort-column="item.target.sortColumn"
+                        :fields="item.target.fields"
+                      />
+                    </div>
+                  </div>
+                  <!-- <MultiSelection
+                  v-model="item.target.sortColumn"
+                  class="item-select"
+                  :class="{ 'empty-data': !item.target.sortColumn }"
+                  :options="item.target.fields"
+                  :key="`item-target-sortColumn` + item.id"
+                  @focus="handleFocus(item.target)"
+                ></MultiSelection> -->
+                </div>
+              </template>
+
               <div v-if="inspectMethod === 'field'" class="setting-item align-items-center mt-4">
                 <label class="item-label">{{ $t('packages_business_components_fieldbox_daijiaoyanmoxing') }}:</label>
                 <ElRadioGroup
@@ -258,7 +307,8 @@ import { inspectMethod as inspectMethodMap } from '../const'
 import MultiSelection from '../MultiSelection'
 import FieldDialog from './FieldDialog'
 import DocsDrawer from './DocsDrawer.vue'
-
+import FieldSelectWrap from './FieldSelectWrap.vue'
+import CollateMap from './CollateMap.vue'
 export default {
   name: 'ConditionBox',
 
@@ -276,7 +326,9 @@ export default {
     MultiSelection,
     FieldDialog,
     SchemaToForm,
-    HighlightCode
+    HighlightCode,
+    FieldSelectWrap,
+    CollateMap
   },
 
   props: {
@@ -347,23 +399,26 @@ export default {
               const data = await metadataInstancesApi.tapTables({
                 filter: JSON.stringify(params)
               })
-              fields = Object.values(data.items[0]?.nameFieldMap || {})
+              fields = Object.values(data.items[0]?.nameFieldMap || {}).map(t => {
+                return {
+                  id: t.id,
+                  label: t.fieldName || t.field_name,
+                  value: t.fieldName || t.field_name,
+                  field_name: t.fieldName || t.field_name,
+                  primary_key_position: t.primaryKey,
+                  data_type: t.dataType || t.data_type,
+                  primaryKey: t.primaryKey,
+                  unique: t.unique,
+                  type: item.data_type,
+                  tapType: JSON.stringify(t.tapType)
+                }
+              })
             }
-            const result = fields.map(t => {
-              return {
-                id: t.id,
-                label: t.fieldName || t.field_name,
-                value: t.fieldName || t.field_name,
-                field_name: t.fieldName || t.field_name,
-                primary_key_position: t.primaryKey,
-                data_type: t.dataType || t.data_type,
-                primaryKey: t.primaryKey,
-                unique: t.unique
-              }
-            })
+            const result = fields
             if (result.length) {
               item.fields = result
             }
+            console.log('loadTableFieldList', fields)
             return result
           } catch (e) {
             return []
