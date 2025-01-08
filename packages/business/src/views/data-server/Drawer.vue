d5d9431f059016c176ceba4da5efe3cfbf5d0bf9
Feynman <xufei@tapdata.io>
2025-01-08 15:11:08 +0800

    refactor: simplify debug tab layout in Drawer.vue

--- packages/business/src/views/data-server/Drawer.vue
+++ packages/business/src/views/data-server/Drawer.vue
@@ -472,11 +472,8 @@
 
         <!--  {{$t('packages_business_data_server_drawer_shilidaima2')}} -->
         <template v-if="tab === 'debug'">
-          <div class="flex position-relative mt-4 mb-3">
-            <div
-              class="position-absolute top-0 start-0 fs-7 fw-sub font-color-dark flex align-center"
-              style="line-height: 36px; height: 36px"
-            >
+          <div class="position-relative mt-4 mb-3">
+            <div class="fs-7 fw-sub font-color-dark flex align-center" style="line-height: 36px; height: 36px">
               <span class="data-server-panel__title my-0">
                 {{ $t('packages_business_data_server_drawer_shilidaima') }}
               </span>
