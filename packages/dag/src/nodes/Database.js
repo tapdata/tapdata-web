c131a1950a43e99423102a12e44a411cd1c2317d
Feynman <xufei@tapdata.io>
2025-03-19 11:05:18 +0800

    fix: Enhance visibility condition for syncIndex feature in Database node

--- packages/dag/src/nodes/Database.js
+++ packages/dag/src/nodes/Database.js
@@ -1083,7 +1083,8 @@ export class Database extends NodeType {
                         'x-reactions': {
                           fulfill: {
                             state: {
-                              visible: '{{hasFeature("syncIndex") && $settings.type !== "cdc"}}',
+                              visible:
+                                '{{hasFeature("syncIndex") && $settings.type !== "cdc" && $values.attrs.capabilities.filter(item => ["get_table_info_function", "create_index_function", "query_indexes_function"].includes(item.id)).length === 3}}',
                               description: `{{$self.value ? '${i18n.t('packages_dag_syncIndex_desc')}' : ''}}`
                             }
                           }
