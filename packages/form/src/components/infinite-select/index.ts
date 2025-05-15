084f5c988c6805775400858048af3e91e7484480
Feynman <xufei@tapdata.io>
2025-05-15 10:00:26 +0800

    refactor: update role management references and enhance Role.vue component
    - Changed the parent reference for 'roleList' in menu.js from 'roleList' to 'roles' for better clarity.
    - Updated routes.js to use dynamic import for the Role component, improving loading performance.
    - Refactored Role.vue to integrate PageContainer and Check components, enhancing layout and accessibility.
    - Improved the structure and styling of the role management UI for better user experience.

--- packages/form/src/components/infinite-select/index.ts
+++ packages/form/src/components/infinite-select/index.ts
@@ -1,7 +1,23 @@
+import { PreviewText } from '@formily/element-plus'
+import { transformComponent } from '@formily/element-plus/esm/__builtins__'
+import { connect, mapProps, mapReadPretty } from '@formily/vue'
 import InfiniteSelect from './InfiniteSelect.vue'
 
 const AsyncSelect = InfiniteSelect
 
-export { AsyncSelect, InfiniteSelect }
+const TransformAsyncSelect = transformComponent(AsyncSelect, {
+  change: 'update:modelValue',
+})
 
-export default InfiniteSelect
+const InnerAsyncSelect = connect(
+  TransformAsyncSelect,
+  mapProps({
+    value: 'modelValue',
+    readOnly: 'readonly',
+  }),
+  mapReadPretty(PreviewText.Input),
+)
+
+export { InnerAsyncSelect as AsyncSelect }
+
+export { InfiniteSelect }
