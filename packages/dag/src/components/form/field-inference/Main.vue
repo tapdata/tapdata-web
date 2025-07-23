40fc8958c38656003c1893a3808aed93c94812bd
Feynman <xufei@tapdata.io>
2025-07-23 17:41:46 +0800

    fix: remove dataType from fields in field inference component

--- packages/dag/src/components/form/field-inference/Main.vue
+++ packages/dag/src/components/form/field-inference/Main.vue
@@ -205,6 +205,11 @@ export default {
       this.fieldsLoading = false
 
       const { fields: newFields } = mapFieldsData(this.selected)
+
+      newFields.forEach((t) => {
+        delete t.dataType
+      })
+
       this.fieldOptions = newFields
       this.selected.fields = newFields
     },
