72e511aa8e182663491f67a660e1460eb12d5f95
Feynman <xufei@tapdata.io>
2025-11-11 11:35:27 +0800

    refactor: update schemaFormInstance to use computed property

--- packages/business/src/views/connections/DatabaseForm.vue
+++ packages/business/src/views/connections/DatabaseForm.vue
@@ -19,9 +19,9 @@ import {
   getProxyId,
   subscribeProxy,
 } from '@tap/api/src/core/proxy'
-
 import resize from '@tap/component/src/directives/resize'
 import SchemaToForm from '@tap/form/src/SchemaToForm.vue'
+
 import i18n from '@tap/i18n'
 import { checkConnectionName, submitForm, uuid } from '@tap/shared'
 import { isEmpty } from 'lodash-es'
@@ -123,7 +123,6 @@ export default {
         total: 0,
       },
       showAgentIpAlert: false,
-      schemaFormInstance: null,
     }
   },
   computed: {
@@ -139,6 +138,12 @@ export default {
           ? 'io'
           : 'net'
       }/prerequisites/allow-access-network`
+    },
+
+    schemaFormInstance: {
+      get() {
+        return this.$refs.schemaToForm?.form
+      },
     },
   },
   async created() {
@@ -160,8 +165,6 @@ export default {
     }
   },
   mounted() {
-    // this.schemaFormInstance = markRaw(this.$refs.schemaToForm?.form) // 获取表单的 form
-    this.schemaFormInstance = this.$refs.schemaToForm?.form // 获取表单的 form
     this.id = this.$route.params.id || ''
     const { fromPath } = this.$route.query
     if (fromPath) {
@@ -898,7 +901,7 @@ export default {
               properties: {
                 heartbeatEnable: {
                   type: 'boolean',
-                  default: true,
+                  default: false,
                   'x-component': 'Switch',
                 },
               },
@@ -1587,7 +1590,7 @@ export default {
     },
 
     getForm() {
-      return this.schemaFormInstance
+      return this.$refs.schemaToForm?.form
     },
 
     handleDebug() {
