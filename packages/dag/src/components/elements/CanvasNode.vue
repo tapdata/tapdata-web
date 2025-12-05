6c5101b5e71ca8799eab30043717320b6a612009
Feynman <xufei@tapdata.io>
2025-12-05 11:36:03 +0800

    feat(CanvasNode): refactor to Composition API, add border-transparent utility class

--- packages/dag/src/components/elements/CanvasNode.vue
+++ packages/dag/src/components/elements/CanvasNode.vue
@@ -1,117 +1,82 @@
-<script>
-import deviceSupportHelpers from '@tap/component/src/mixins/deviceSupportHelpers'
+<script setup lang="ts">
 import { OverflowTooltip } from '@tap/component/src/overflow-tooltip'
-import i18n from '@tap/i18n'
-import { mapGetters, mapMutations, mapState } from 'vuex'
+import { useI18n } from '@tap/i18n'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 import BaseNode from '../BaseNode.vue'
 import NodeSourceHandle from './NodeSourceHandle.vue'
 import NodeTargetHandle from './NodeTargetHandle.vue'
 import NodeToolbar from './NodeToolbar.vue'
 
-export default {
+defineOptions({
   name: 'DFNode',
-  components: {
-    OverflowTooltip,
-    BaseNode,
-    NodeSourceHandle,
-    NodeTargetHandle,
-    NodeToolbar,
-  },
-  mixins: [deviceSupportHelpers],
-  props: {
-    data: Object,
-    nodeId: {
-      type: String,
-      required: true,
-    },
-    jsPlumbIns: Object,
-    hideDisableAction: Boolean,
-    isSync: Boolean,
-  },
-  data() {
-    return {
-      id: this.$attrs.id,
-    }
-  },
-  computed: {
-    ...mapState('dataflow', ['canBeConnectedNodeIds']),
-    ...mapGetters('dataflow', [
-      'nodeById',
-      'isActionActive',
-      'isNodeActive',
-      'isNodeSelected',
-      'isMultiSelect',
-      'processorNodeTypes',
-      'hasNodeError',
-      'stateIsReadonly',
-      'activeType',
-    ]),
+})
 
-    ins() {
-      return this.data?.__Ctor || {}
-    },
+const { t } = useI18n()
 
-    allowTarget() {
-      if (typeof this.ins?.allowTarget === 'boolean') {
-        return this.ins.allowTarget
-      }
-      return true
-    },
-
-    wrapClass() {
-      if (this.canBeConnectedNodeIds.includes(this.nodeId))
-        return 'can-be-connected'
-      return ''
-    },
-
-    nodeClass() {
-      const list = []
-
-      if (this.isNodeActive(this.nodeId) && this.activeType === 'node')
-        list.push('active')
-      if (this.isNodeSelected(this.nodeId)) list.push('selected')
-      if (this.data.attrs.disabled) list.push('node--disabled')
-      if (this.data.disabled) list.push('node--disabled__main')
-
-      this.ins && list.push(`node--${this.ins.group}`)
-      return list
-    },
-
-    nodeStyle() {
-      const [left = 0, top = 0] = this.data.attrs?.position || []
-      return {
-        left: `${left}px`,
-        top: `${top}px`,
-      }
-    },
-
-    nodeErrorMsg() {
-      const res = this.hasNodeError(this.data.id)
-      if (res) {
-        return typeof res === 'string'
-          ? res
-          : i18n.t('packages_dag_components_dfnode_qingjianchajiedian')
-      }
-      return null
-    },
-  },
-  mounted() {},
-  methods: {
-    ...mapMutations('dataflow', [
-      'setActiveNode',
-      'addActiveAction',
-      'removeActiveAction',
-      'resetSelectedNodes',
-      'setNodeError',
-      'clearNodeError',
-    ]),
-  },
+interface Props {
+  data: Record<string, any>
+  nodeId: string
+  jsPlumbIns?: Record<string, any>
+  hideDisableAction?: boolean
+  isSync?: boolean
 }
+
+const props = defineProps<Props>()
+
+const store = useStore()
+
+// Vuex state
+const canBeConnectedNodeIds = computed(
+  () => store.state.dataflow.canBeConnectedNodeIds,
+)
+
+// Vuex getters
+const isNodeActive = computed(() => store.getters['dataflow/isNodeActive'])
+const isNodeSelected = computed(() => store.getters['dataflow/isNodeSelected'])
+const hasNodeError = computed(() => store.getters['dataflow/hasNodeError'])
+const activeType = computed(() => store.getters['dataflow/activeType'])
+
+// computed
+const ins = computed(() => props.data?.__Ctor || {})
+
+const wrapClass = computed(() => {
+  if (canBeConnectedNodeIds.value.includes(props.nodeId))
+    return 'can-be-connected'
+  return ''
+})
+
+const nodeClass = computed(() => {
+  const list: string[] = []
+
+  if (isNodeActive.value(props.nodeId) && activeType.value === 'node')
+    list.push('active')
+  if (isNodeSelected.value(props.nodeId)) list.push('selected')
+  if (props.data.attrs.disabled) list.push('node--disabled')
+  if (props.data.disabled) list.push('node--disabled__main')
+
+  ins.value && list.push(`node--${ins.value.group}`)
+  return list
+})
+
+const nodeErrorMsg = computed(() => {
+  const res = hasNodeError.value(props.data.id)
+  if (res) {
+    return typeof res === 'string'
+      ? res
+      : t('packages_dag_components_dfnode_qingjianchajiedian')
+  }
+  return null
+})
 </script>
 
 <template>
-  <div class="df-node-wrap canvas-node" :class="wrapClass" tabindex="1">
-    <BaseNode :node="data" :class="nodeClass" @click="mouseClick">
+  <div
+    class="df-node-wrap canvas-node border border-transparent"
+    :class="wrapClass"
+    tabindex="1"
+  >
+    <BaseNode :node="props.data" :class="nodeClass">
       <template #text="{ text }">
         <OverflowTooltip
           class="df-node-text"
@@ -122,81 +87,14 @@ export default {
         />
         <VIcon v-if="ins.beta" class="mr-1" size="32">beta</VIcon>
       </template>
-      <!-- <div v-if="!stateIsReadonly" class="df-node-options gap-4" @click.stop>
-        <div
-          v-if="isSync"
-          :disabled="data.disabled"
-          class="cursor-pointer"
-          :class="{
-            'opacity-50 cursor-not-allowed': data.disabled,
-          }"
-          @click.stop="!data.disabled && $emit('preview', nodeId, data)"
-        >
-          <VIcon size="20">action-play</VIcon>
-        </div>
-
-        <div
-          :disabled="data.disabled"
-          class="cursor-pointer"
-          :class="{
-            'opacity-50 cursor-not-allowed': data.disabled,
-          }"
-          :title="$t('packages_dag_components_dfnode_tianjiajiedian')"
-          @click.stop="
-            !data.disabled &&
-            $emit(
-              'show-node-popover',
-              'node',
-              data,
-              $event.currentTarget || $event.target,
-            )
-          "
-        >
-          <VIcon size="20">action-add</VIcon>
-        </div>
-
-        <template v-if="!hideDisableAction">
-          <div
-            v-if="data.disabled"
-            name="action-enable"
-            class="cursor-pointer"
-            :title="$t('packages_dag_btn_disable_node')"
-            @click.stop="$emit('enable', data)"
-          >
-            <VIcon size="20">action-enable</VIcon>
-          </div>
-          <div
-            v-else
-            name="action-disable"
-            class="cursor-pointer"
-            :title="$t('packages_dag_btn_disable_node')"
-            @click.stop="$emit('disable', data)"
-          >
-            <VIcon size="20">action-disable</VIcon>
-          </div>
-        </template>
-
-        <div
-          :disabled="data.disabled"
-          name="action-delete"
-          class="cursor-pointer"
-          :class="{
-            'opacity-50 cursor-not-allowed': data.disabled,
-          }"
-          :title="$t('packages_dag_components_dfnode_shanchujiedian')"
-          @click.stop="!data.disabled && $emit('delete', data.id)"
-        >
-          <VIcon size="20">action-delete</VIcon>
-        </div>
-      </div> -->
       <ElTooltip
-        v-if="hasNodeError(data.id)"
-        :content="nodeErrorMsg"
+        v-if="hasNodeError(props.data.id)"
+        :content="nodeErrorMsg ?? undefined"
         placement="top"
       >
         <VIcon class="mr-2" size="14" color="#FF7474">warning</VIcon>
       </ElTooltip>
-      <VIcon v-if="data.disabled" class="mr-2 color-warning" size="16"
+      <VIcon v-if="props.data.disabled" class="mr-2 color-warning" size="16"
         >disable</VIcon
       >
     </BaseNode>
