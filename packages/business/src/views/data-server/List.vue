bfc215247fdd9566ae6d4d40f4737e2a449a4494
Feynman <xufei@tapdata.io>
2026-04-09 10:58:12 +0800

    fix: correct button disabled state in List component to reflect user edit permissions

--- packages/business/src/views/data-server/List.vue
+++ packages/business/src/views/data-server/List.vue
@@ -765,7 +765,7 @@ defineExpose({
               class="ellipsis"
               type="primary"
               style="display: block; line-height: 20px"
-              :disabled="row.permissionActions?.includes('Edit')"
+              :disabled="!row.permissionActions?.includes('Edit')"
               @click.stop="showDrawer(row)"
             >
               {{ row.name }}
