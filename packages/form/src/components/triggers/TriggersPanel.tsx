f750db2052fcb349405e9160c70e02a2a9f4188f
Feynman <xufei@tapdata.io>
2026-04-10 11:58:22 +0800

    feat: add multiple and clearable options to TriggersPanel component and introduce localization support for multiple languages

--- packages/form/src/components/triggers/TriggersPanel.tsx
+++ packages/form/src/components/triggers/TriggersPanel.tsx
@@ -230,6 +230,8 @@ export const TriggersPanel = defineComponent({
             }
             disabled={props.disabled}
             class="w-full"
+            multiple
+            clearable
           >
             {OP_OPTIONS.map((op) => (
               <ElOption
