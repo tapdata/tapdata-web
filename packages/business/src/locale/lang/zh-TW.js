c5e6d731cfc19d4a6bd4052bb05db5d6e69de395
hardy <2545433047@qq.com>
2026-09-11 09:33:43 +0800

    style(TAP-12467): fix prettier formatting for CI

--- packages/business/src/locale/lang/zh-TW.js
+++ packages/business/src/locale/lang/zh-TW.js
@@ -726,10 +726,14 @@ export default {
     '當任一API Server的工作行程記憶體使用量超過告警閾值時',
   packages_business_setting_alarmnotification_api_server_worker_memory_usage_alter:
     '當任一API Server的工作行程記憶體使用量超過通知閾值時',
-  packages_business_api_server_p95_warn: '當任一API Server請求耗時P95超過告警閾值時',
-  packages_business_api_server_p99_warn: '當任一API Server請求耗時P99超過告警閾值時',
-  packages_business_api_server_error_rate_warn: '當任一API Server請求錯誤率超過告警閾值時',
-  packages_business_task_source_no_incremental_event: '當源端已開啟心跳表，連續 60 秒未接收到增量事件時',
+  packages_business_api_server_p95_warn:
+    '當任一API Server請求耗時P95超過告警閾值時',
+  packages_business_api_server_p99_warn:
+    '當任一API Server請求耗時P99超過告警閾值時',
+  packages_business_api_server_error_rate_warn:
+    '當任一API Server請求錯誤率超過告警閾值時',
+  packages_business_task_source_no_incremental_event:
+    '當源端已開啟心跳表，連續 60 秒未接收到增量事件時',
   packages_business_setting_alarmnotification_dangrenwuuP: 'Agent服务啓動時',
   packages_business_setting_alarmnotification_dangyinqinglixian: '當引擎停止時',
   packages_business_setting_alarmnotification_dangyinqinghuifu: '當引擎啓動時',
@@ -1579,6 +1583,8 @@ export default {
   public_connector_alpha_tip: '正在持續開發中，部分功能可能還在完善',
   packages_business_column_setting: '顯示設定',
   packages_business_column_reset: '重置',
-  packages_business_api_server_connection_pool_deficiency_warn: '當任一API Server 連接池資源緊張触发告警閾值時',
-  packages_business_api_server_connection_pool_idle_warn: '當任一API Server 連接池連接數低於告警閾值時',
+  packages_business_api_server_connection_pool_deficiency_warn:
+    '當任一API Server 連接池資源緊張触发告警閾值時',
+  packages_business_api_server_connection_pool_idle_warn:
+    '當任一API Server 連接池連接數低於告警閾值時',
 }
